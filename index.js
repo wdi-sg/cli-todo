f732a1a30eb97e1efe41079247af9730796330e7
ah5ive <ah5ive@gmail.com>
2018-10-01 18:57:17 +0800

    getting the item in the node index.js by showing items and adding items

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ console.log("works!!", process.argv[2]);
 const pg = require('pg');
 
 const configs = {
-    user: 'akira',
+    user: 'Haruspring',
     host: '127.0.0.1',
     database: 'todo',
     port: 5432,
@@ -15,7 +15,14 @@ let queryDoneCallback = (err, result) => {
     if (err) {
       console.log("query error", err.message);
     } else {
-      console.log("result", result.rows );
+
+        for (var i = 0; i < result.rows.length; i++){
+            let toDoItems = result.rows[i];
+            let myString = toDoItems.id + '[ ]' + toDoItems.name;
+            console.log("===", myString);
+        }
+
+      //console.log("result", result.rows );
     }
 };
 
@@ -25,11 +32,26 @@ let clientConnectionCallback = (err) => {
     console.log( "error", err.message );
   }
 
-  let text = "INSERT INTO todo (name) VALUES ($1) RETURNING id";
+  if (process.argv[2] === "show"){
 
-  const values = ["hello"];
+    let text = "SELECT * FROM items";
 
-  client.query(text, values, queryDoneCallback);
+    client.query(text, queryDoneCallback);
+
+  } else if (process.argv[2] === "add" ){
+
+  let text = "INSERT INTO items (name) VALUES ($1) RETURNING *";
+//"INSERT INTO todo (name) VALUES ($1) RETURNING id";
+// when added values into the item table the "*" let node return
+//the value add in the for looped. which is the myString.
+  const values = [process.argv[3]];
+
+    client.query(text, values, queryDoneCallback);
+    }
+
+
 };
 
 client.connect(clientConnectionCallback);
+
+//
