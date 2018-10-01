4af0629f52c68962cc27876c8d940cb15d8b522f
weiwenl <weiwenl.justin@gmail.com>
2018-10-01 21:57:52 +0800

    Add deliverables

--- index.js
+++ index.js
@@ -1,35 +1,71 @@
-console.log("works!!", process.argv[2]);
+console.log("works!!", process.argv[2], process.argv[3]);
 
+// require the module
 const pg = require('pg');
 
+// set all of the configuration in an object
 const configs = {
-    user: 'akira',
+    user: 'weiwenlee',
     host: '127.0.0.1',
     database: 'todo',
     port: 5432,
 };
 
+// create a new instance of the client
 const client = new pg.Client(configs);
 
-let queryDoneCallback = (err, result) => {
-    if (err) {
-      console.log("query error", err.message);
-    } else {
-      console.log("result", result.rows );
-    }
-};
-
-let clientConnectionCallback = (err) => {
-
+// connect to the client
+client.connect((err) => {
   if( err ){
     console.log( "error", err.message );
   }
 
-  let text = "INSERT INTO todo (name) VALUES ($1) RETURNING id";
+  // show list
+  if(process.argv[2] === 'show'){
+    // if connection is successful, call the query method
+    // if query is successful, log the result
 
-  const values = ["hello"];
+    // set up what to query
+    let text = 'SELECT * FROM items';
 
-  client.query(text, values, queryDoneCallback);
-};
+    client.query(text, (err, res) => {
+      if (err) {
+        console.log("query error", err.message);
+      }
 
-client.connect(clientConnectionCallback);
+      else {
+        for( let i=0; i<res.rows.length; i++ ){
+           let items = res.rows[i];
+           let str = `${items.id}. [done/not done] - ${items.name}`;
+           //console.log("result: ", res.rows[i]);
+           console.log(str);
+        }
+      }
+    });
+  }
+
+  // add stuff into list
+  else if(process.argv[2] === 'add'){
+
+    // if connection is successful, call the query method
+    // if query is successful, add the values
+
+    // set up what to query
+    // returning allows you to see what you inserted
+    // let text = "INSERT INTO items (id, name) VALUES ($1, $2) RETURNING (id, name)"
+    // const values = [process.argv[3], process.argv[4]];
+    let text = "INSERT INTO items  (name) VALUES ($1) RETURNING (id, name)"
+    const values = [process.argv[3]];
+    //?? Can we auto generate the id ??
+    client.query(text, values, (err, res) => {
+
+      if (err) {
+        console.log("query error", err.message);
+      }
+
+      else {
+        console.log(res.rows);
+      }
+    });
+  }
+});
---
+++ seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO items (id, name) VALUES('1', 'eat cookies');
+INSERT INTO items (id, name) VALUES('2', 'go for a swim');
+INSERT INTO items (id, name) VALUES('3', 'meet Alan');
+INSERT INTO items (id, name) VALUES('4', 'go shopping');
+INSERT INTO items (id, name) VALUES('5', 'book an air ticket');
