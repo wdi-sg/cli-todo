d10d0b17d0eaf00bce465114da8f2e2b6c321fac
lamesensei <16118606+lamesensei@users.noreply.github.com>
2018-10-01 22:49:34 +0800

    Add ASCII header

--- index.js
+++ index.js
@@ -9,12 +9,23 @@ const configs = {
 
 const client = new pg.Client(configs);
 
+const header = `                                 
+88888888888     d8888  .d8888b.  888    d8P   .d8888b.  
+    888        d88888 d88P  Y88b 888   d8P   d88P  Y88b 
+    888       d88P888 Y88b.      888  d8P    Y88b.      
+    888      d88P 888  "Y888b.   888d88K      "Y888b.   
+    888     d88P  888     "Y88b. 8888888b        "Y88b. 
+    888    d88P   888       "888 888  Y88b         "888 
+    888   d8888888888 Y88b  d88P 888   Y88b  Y88b  d88P 
+    888  d88P     888  "Y8888P"  888    Y88b  "Y8888P" 
+`;
+
 const displayTasks = () => {
   client.query('SELECT * FROM tasks ORDER BY id ASC', (qerr, res) => {
     if (qerr) {
       console.log(qerr.stack);
     } else {
-      console.log('Task List');
+      console.log(header);
       res.rows.forEach((element, i) => {
         if (element.done === true) console.log(`${(i += 1)}. [x] - ${element.name}`);
         else console.log(`${i + 1}. [ ] - ${element.name}`);
