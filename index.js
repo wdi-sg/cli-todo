c7b1b51396da36de18601e370352f5d7a817cb69
lamesensei <16118606+lamesensei@users.noreply.github.com>
2018-10-01 22:36:17 +0800

    Add delete & modified timestamp tracking

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ const displayTasks = () => {
     if (qerr) {
       console.log(qerr.stack);
     } else {
-      let i = 0;
-      res.rows.forEach((element) => {
+      console.log('Task List');
+      res.rows.forEach((element, i) => {
         if (element.done === true) console.log(`${(i += 1)}. [x] - ${element.name}`);
-        else console.log(`${(i += 1)}. [ ] - ${element.name}`);
+        else console.log(`${i + 1}. [ ] - ${element.name}`);
       });
     }
     client.end();
@@ -26,8 +26,8 @@ const displayTasks = () => {
 
 // CHECK FOR SHOW QUERY
 if (process.argv[2] === 'show') {
-  client.connect((cerr) => {
-    if (cerr) return console.error(cerr);
+  client.connect((conErr) => {
+    if (conErr) return console.error(conErr);
     // INIT SELECT QUERY TO DISPLAY ALL TASKS IN LIST
     return displayTasks();
   });
@@ -35,13 +35,13 @@ if (process.argv[2] === 'show') {
 
 // CHECK FOR ADD QUERY
 if (process.argv[2] === 'add' && process.argv[3] !== undefined) {
-  client.connect((cerr) => {
-    if (cerr) return console.error(cerr);
+  client.connect((conErr) => {
+    if (conErr) return console.error(conErr);
     const text = 'INSERT INTO tasks (name) VALUES ($1);';
     const value = [process.argv[3]];
     // INIT INSERT OF USER INPUT
-    client.query(text, value, (ierr, res) => {
-      if (ierr) console.log(ierr.stack);
+    client.query(text, value, (insErr, res) => {
+      if (insErr) console.log(insErr.stack);
       else displayTasks();
     });
     return null;
@@ -50,14 +50,37 @@ if (process.argv[2] === 'add' && process.argv[3] !== undefined) {
 
 // CHECK FOR MARK DONE
 if (process.argv[2] === 'done' && process.argv[3] !== undefined) {
-  client.connect((cerr) => {
-    if (cerr) return console.error(cerr);
-    const text = 'UPDATE tasks SET done = true WHERE id = ($1);';
+  client.connect((conErr) => {
+    if (conErr) return console.error(conErr);
+    let text = 'UPDATE tasks SET done = true WHERE id = ($1);';
+    let value = [process.argv[3]];
+    // INIT UPDATE OF USER INPUT
+    client.query(text, value, (updErr, res) => {
+      if (updErr) console.log(updErr.stack);
+      else {
+        text = 'UPDATE tasks SET updated_at = now() WHERE id = ($1)';
+        value = [process.argv[3]];
+        client.query(text, value, (modError, res) => {
+          if (modError) console.log(modError.stack);
+          displayTasks();
+        });
+      }
+    });
+    return null;
+  });
+}
+
+if (process.argv[2] === 'delete' && process.argv[3] !== undefined) {
+  client.connect((conErr) => {
+    if (conErr) return console.error(conErr);
+    const text = 'DELETE FROM tasks WHERE id = ($1);';
     const value = [process.argv[3]];
-    // INIT INSERT OF USER INPUT
-    client.query(text, value, (ierr, res) => {
-      if (ierr) console.log(ierr.stack);
-      else displayTasks();
+    // INIT UPDATE OF USER INPUT
+    client.query(text, value, (delError, res) => {
+      if (delError) console.log(delError.stack);
+      else {
+        displayTasks();
+      }
     });
     return null;
   });
--- table.sql
+++ table.sql
@@ -2,5 +2,6 @@ CREATE TABLE IF NOT EXISTS tasks (
     id SERIAL PRIMARY KEY,
     name TEXT,
     done BOOLEAN DEFAULT FALSE,
-    created_at TIMESTAMP DEFAULT now()
+    created_at TIMESTAMP DEFAULT now(),
+    updated_at TIMESTAMP
 );
