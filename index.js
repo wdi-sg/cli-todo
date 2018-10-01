a23bc84e25ff6a901d1e894fc8ec253a1a73e3ee
JonathanL1211 <Jonathanknight1211@gmail.com>
2018-10-01 20:51:59 +0800

    add: database todo with table items and ability to add items

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ console.log("works!!", process.argv[2]);
 const pg = require('pg');
 
 const configs = {
-    user: 'akira',
+    user: 'jonathanlau',
     host: '127.0.0.1',
     database: 'todo',
     port: 5432,
@@ -25,11 +25,24 @@ let clientConnectionCallback = (err) => {
     console.log( "error", err.message );
   }
 
-  let text = "INSERT INTO todo (name) VALUES ($1) RETURNING id";
 
-  const values = ["hello"];
+  if (process.argv[2] === 'show'){
+    let text = 'SELECT * FROM items';
+    client.query(text, queryDoneCallback);
+  }
+  else if (process.argv[2] === 'add'){
 
-  client.query(text, values, queryDoneCallback);
+    let text1 = 'INSERT INTO items (name) VALUES ($1) RETURNING id';
+    let values = [process.argv[3]];
+    console.log(process.argv[3]);
+    client.query(text1, values, queryDoneCallback);
+
+  }
+
+  // let text = "INSERT INTO todo (name) VALUES ($1) RETURNING id";
+
+  // const values = ["hello"];
+
 };
 
 client.connect(clientConnectionCallback);
