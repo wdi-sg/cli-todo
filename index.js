88799b9e18c26e59c8df32f04c4aa2ccbbcf1627
ChloeLiang <nliangxin@gmail.com>
2018-10-01 18:25:47 +0800

    Refactor

--- index.js
+++ index.js
@@ -9,38 +9,37 @@ const configs = {
 
 const client = new pg.Client(configs);
 
+const showTasks = () => {
+  const text = 'SELECT * FROM items';
+
+  client.query(text, (err, result) => {
+    if (err) {
+      console.log('query error', err.message);
+    } else {
+      result.rows.forEach((task, index) => {
+        console.log(`${index}. [ ] - ${task.name}`);
+      });
+    }
+  });
+};
+
+const addTask = () => {
+  const text = 'INSERT INTO items (name) VALUES ($1)';
+  const values = [process.argv[3]];
+
+  client.query(text, values, (err, result) => {
+    if (err) {
+      console.log('query error', err.message);
+    } else {
+      showTasks();
+    }
+  });
+
+};
+
 client.connect(err => {
   if (err) {
     console.log('error', err.message);
-  }
-
-  const showTasks = () => {
-    const text = 'SELECT * FROM items';
-
-    client.query(text, (err, result) => {
-      if (err) {
-        console.log('query error', err.message);
-      } else {
-        result.rows.forEach((task, index) => {
-          console.log(`${index}. [ ] - ${task.name}`);
-        });
-      }
-    });
-  };
-
-  const addTask = () => {
-    const text = 'INSERT INTO items (name) VALUES ($1)';
-    const values = [process.argv[3]];
-
-
-    client.query(text, values, (err, result) => {
-      if (err) {
-        console.log('query error', err.message);
-      } else {
-        showTasks();
-      }
-    });
-
   }
 
   if (process.argv[2] === 'add') {
@@ -52,5 +51,4 @@ client.connect(err => {
   if (process.argv[2] === 'show') {
     showTasks();
   }
-
 });
