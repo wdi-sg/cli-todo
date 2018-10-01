f235fe5f18d49ccdd3f495e972b37fdf7e818f4f
ChloeLiang <nliangxin@gmail.com>
2018-10-01 18:12:08 +0800

    Add and show tasks

--- index.js
+++ index.js
@@ -1,35 +1,56 @@
-console.log("works!!", process.argv[2]);
-
 const pg = require('pg');
 
 const configs = {
-    user: 'akira',
-    host: '127.0.0.1',
-    database: 'todo',
-    port: 5432,
+  user: 'liangxin',
+  host: '127.0.0.1',
+  database: 'todo',
+  port: 5432
 };
 
 const client = new pg.Client(configs);
 
-let queryDoneCallback = (err, result) => {
-    if (err) {
-      console.log("query error", err.message);
-    } else {
-      console.log("result", result.rows );
-    }
-};
-
-let clientConnectionCallback = (err) => {
-
-  if( err ){
-    console.log( "error", err.message );
+client.connect(err => {
+  if (err) {
+    console.log('error', err.message);
   }
 
-  let text = "INSERT INTO todo (name) VALUES ($1) RETURNING id";
+  const showTasks = () => {
+    const text = 'SELECT * FROM items';
 
-  const values = ["hello"];
+    client.query(text, (err, result) => {
+      if (err) {
+        console.log('query error', err.message);
+      } else {
+        result.rows.forEach((task, index) => {
+          console.log(`${index}. [ ] - ${task.name}`);
+        });
+      }
+    });
+  };
 
-  client.query(text, values, queryDoneCallback);
-};
+  const addTask = () => {
+    const text = 'INSERT INTO items (name) VALUES ($1)';
+    const values = [process.argv[3]];
 
-client.connect(clientConnectionCallback);
+
+    client.query(text, values, (err, result) => {
+      if (err) {
+        console.log('query error', err.message);
+      } else {
+        showTasks();
+      }
+    });
+
+  }
+
+  if (process.argv[2] === 'add') {
+    if (process.argv[3]) {
+      addTask();
+    }
+  }
+
+  if (process.argv[2] === 'show') {
+    showTasks();
+  }
+
+});
---
+++ tables.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS items (
+  id SERIAL PRIMARY KEY,
+  name text
+);
