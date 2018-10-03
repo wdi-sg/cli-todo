4b511a2e406aecb3b81e54778a1940049669d0db
hygro5d <faidhiamran@Faidhis-MacBook-Pro.local>
2018-10-03 08:08:45 +0800

    Assignment Todo

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-console.log("works!!", process.argv[2]);
+// console.log("works!!", process.argv[2]);
 
 const pg = require('pg');
 
 const configs = {
-    user: 'akira',
+    user: 'faidhiamran',
     host: '127.0.0.1',
     database: 'todo',
     port: 5432,
@@ -11,25 +11,60 @@ const configs = {
 
 const client = new pg.Client(configs);
 
-let queryDoneCallback = (err, result) => {
+
+let showTask = (err) => {
+  if (err) {
+    console.log("query error", err.message);
+  }
+  let queryText = "SELECT * FROM items";
+  client.query(queryText, (err, res) => {
     if (err) {
       console.log("query error", err.message);
     } else {
-      console.log("result", result.rows );
+      for (let i = 0; i < res.rows.length; i++) {
+        let done;
+        if (res.rows[i].done) {
+          done = "[x]";
+        }
+        else done = "[ ]";
+        console.log(`${res.rows[i].id}. ${done} - ${res.rows[i].name}`);
+      }
     }
+  })
+}
+
+
+
+
+let addTask = (err) => {
+  if (err) {
+    console.log("error", err.message);
+  }
+  let text = "INSERT INTO items (name, done) VALUES ($1, $2)";
+  const values = [process.argv[3], false];
+  client.query(text, values, showTask);
 };
 
-let clientConnectionCallback = (err) => {
 
-  if( err ){
+// let clientConnectionCallback = (err) => {
+
+let doneTask = (err) => {
+  if (err){
     console.log( "error", err.message );
   }
-
-  let text = "INSERT INTO todo (name) VALUES ($1) RETURNING id";
-
-  const values = ["hello"];
-
-  client.query(text, values, queryDoneCallback);
+  let text = "UPDATE items SET done = ($1) WHERE id = ($2)";
+  const values = [true, process.argv[3]];
+  client.query(text, values, showTask);
 };
 
-client.connect(clientConnectionCallback);
+// client.connect(clientConnectionCallback);
+
+if (process.argv[2] === "show") {
+  client.connect(showTask);
+}
+else if (process.argv[2] === "add") {
+  client.connect(addTask);
+}
+ else if (process.argv[2] === "done") {
+  client.connect(doneTask);
+}
