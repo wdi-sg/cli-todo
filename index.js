89aa8a2767775d0f5714a09c77c954c1d8446084
angrylobster <liewminshan@gmail.com>
2018-12-14 18:45:38 +0800

    Done with further parts except ASCII and 'dynamic command line'

--- index.js
+++ index.js
@@ -1,19 +1,64 @@
-console.log("works!!", process.argv[2]);
-
-var commandType = process.argv[2];
-
-console.log("Your command was: "+commandType);
-
+const command = process.argv[2];
+const argument = process.argv[3];
 const jsonfile = require('jsonfile');
-
 const file = 'data.json'
 
-jsonfile.readFile(file, (err, obj) => {
+readAndWrite(command);
 
-  console.log(obj);
-  obj["helloworld"] = "monkey";
+function readAndWrite(command){
+    jsonfile.readFile(file, (err, obj) => {
+        var todoList = obj.todoItems;
+        execute(command, todoList);
+        jsonfile.writeFile(file, obj, (err) => {
+            if (err !== null){
+                console.log(err);
+            } else {
+                console.log(getList(todoList));
+            }
+        });
+    });
+}
 
-  jsonfile.writeFile(file, obj, (err) => {
-    console.log(err)
-  });
-});
+function execute(command, todoList){
+    switch(command){
+        case "add":
+            let date = new Date();
+            let dateString = `(${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}, ${date.getUTCHours() + 8}:${date.getUTCMinutes()}h)`
+            let obj = {
+                "task": argument,
+                "done": false,
+                "created_at": dateString
+            }
+            todoList.push(obj);
+            break;
+        case "done":
+            if (todoList[argument - 1].done === false){
+                todoList[argument - 1].done = true;
+            } else {
+                todoList[argument - 1].done === false
+            }
+            break;
+        case "delete":
+            let deletedTask = todoList.splice(argument - 1, 1);
+            console.log("'" + deletedTask[0].task + "' was deleted.")
+            break;
+        default:
+            console.log("Invalid command!");
+            console.log(getList(todoList));
+            break;
+    }
+}
+
+function getList(todoList){
+    let list = "";
+    for(let obj in todoList){
+        let done = " ";
+        if (todoList[obj].done === true){
+            done = "X";
+        }
+        let todoItem = todoList[obj];
+        let dateCreated = todoItem.created_at;
+        list += parseInt(obj) + 1 + ". [" + done + "] - " + todoItem.task + " " + dateCreated + "\n";
+    }
+    return list;
+}
