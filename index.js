59ca2ccc6690338b29e037a61077300bec0cdbb2
ishakbhn <ishakbhn@gmail.com>
2018-12-16 02:32:36 +0800

    completed add item and show list

--- index.js
+++ index.js
@@ -1,19 +1,44 @@
-console.log("works!!", process.argv[2]);
-
-var commandType = process.argv[2];
-
-console.log("Your command was: "+commandType);
-
 const jsonfile = require('jsonfile');
 
-const file = 'data.json'
+const file = 'data.json';
 
-jsonfile.readFile(file, (err, obj) => {
+var add = () => {
+    jsonfile.readFile(file, (err, obj) => {
+        let length = obj.todoItem.length;
+        let addItem = process.argv[3];
+        // push add item
+        obj.todoItem.push({
+            num: (length + 1) + ".",
+            checkbox: "[ ]",
+            item: "- " + addItem
+        });
+            jsonfile.writeFile(file, obj, (err) => {
+                console.log(err);
+            });
+    });
+}
 
-  console.log(obj);
-  obj["helloworld"] = "monkey";
+var show = () => {
+    jsonfile.readFile(file, (err, obj) => {
+    let length = obj.todoItem.length;
+       for (let i =0; i < length; i ++){
+        let listNum = obj.todoItem[i].num;
+        let checkbox = obj.todoItem[i].checkbox;
+        let item = obj.todoItem[i].item;
+        console.log(listNum + checkbox + item);
+    }
+            jsonfile.writeFile(file, obj, (err) => {
+                console.log(err);
+            });
+    });
+}
 
-  jsonfile.writeFile(file, obj, (err) => {
-    console.log(err)
-  });
-});
+switch (process.argv[2]){
+    case "add":
+        add();
+        break;
+    case "show":
+        show();
+        break;
+}
+
