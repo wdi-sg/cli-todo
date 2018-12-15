0a6e5ae07cb6235e9372100d77bdf26d3d147bc2
jumpingrock <kennethyeongky@gmail.com>
2018-12-15 20:17:32 +0800

    first commit read and add data operational

--- index.js
+++ index.js
@@ -3,17 +3,29 @@ console.log("works!!", process.argv[2]);
 var commandType = process.argv[2];
 
 console.log("Your command was: "+commandType);
+var toDoItem = [];
+if(commandType == "add"){
+    toDoItem.push(process.argv[3]);
+    console.log(toDoItem);
+    obj.push(process.argv[3]);
+    jsonfile.writeFile(file, obj, (err) => {
+        console.log(err)
+    });
+}else if (commandType == ".showlist"){
+    jsonfile.readFile(file, (err, obj) => {
+    console.log(obj);
+    });
+}
 
 const jsonfile = require('jsonfile');
 
 const file = 'data.json'
 
 jsonfile.readFile(file, (err, obj) => {
-
   console.log(obj);
-  obj["helloworld"] = "monkey";
+  obj.push(process.argv[3]);
 
-  jsonfile.writeFile(file, obj, (err) => {
-    console.log(err)
-  });
+  // jsonfile.writeFile(file, obj, (err) => {
+  //   console.log(err)
+  // });
 });
