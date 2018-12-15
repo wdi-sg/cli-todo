4995b5942a1f1263204fe74234e0bf6d4e30c7b0
ishakbhn <ishakbhn@gmail.com>
2018-12-16 03:30:41 +0800

    completed the done section

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const file = 'data.json';
 
 var add = () => {
     jsonfile.readFile(file, (err, obj) => {
+        //todoItem length
+        //the 4th argument to be addItem
+        // push add item
         let length = obj.todoItem.length;
         let addItem = process.argv[3];
-        // push add item
         obj.todoItem.push({
             num: (length + 1) + ".",
             checkbox: "[ ]",
@@ -22,11 +24,28 @@ var show = () => {
     jsonfile.readFile(file, (err, obj) => {
     let length = obj.todoItem.length;
        for (let i =0; i < length; i ++){
+        //todoItemList num
+        //todoItemList checkbox
+        //todoItemList.item
+        //Display the keys for todoItems keys
         let listNum = obj.todoItem[i].num;
         let checkbox = obj.todoItem[i].checkbox;
         let item = obj.todoItem[i].item;
         console.log(listNum + checkbox + item);
     }
+            jsonfile.writeFile(file, obj, (err) => {
+                console.log(err);
+            });
+    });
+}
+
+var done = () => {
+    jsonfile.readFile(file, (err, obj) => {
+        let length = obj.todoItem.length;
+        let markDone = parseInt(process.argv[3]);
+        markDone --;
+        obj.todoItem[markDone].checkbox ="[x]";
+
             jsonfile.writeFile(file, obj, (err) => {
                 console.log(err);
             });
@@ -39,6 +58,9 @@ switch (process.argv[2]){
         break;
     case "show":
         show();
+        break;
+    case "done":
+        done();
         break;
 }
 
