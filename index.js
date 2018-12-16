d759c2eb7d9b96ac52cbf9883f17091e99fe7946
angrylobster <liewminshan@gmail.com>
2018-12-16 14:07:26 +0800

    Improved code to recognize erroneous command inputs

--- index.js
+++ index.js
@@ -7,58 +7,90 @@ readAndWrite(command);
 
 function readAndWrite(command){
     jsonfile.readFile(file, (err, obj) => {
-        var todoList = obj.todoItems;
-        execute(command, todoList);
+        var todoItems = obj.todoItems;
+
+        //If the user input "done" or "delete" as a command, but the array is empty or if the array has less items than the argument number, or if the argument was empty, do nothing. Otherwise, execute the command.
+        if (!((command === "done" || command === "delete") && (isEmptyArray(todoItems) || argument > todoItems.length))){
+            if (!argument){
+                console.log("You must key in an argument!");
+                return;
+            }
+            execute(command, todoItems);
+        }
         jsonfile.writeFile(file, obj, (err) => {
             if (err !== null){
                 console.log(err);
             } else {
-                console.log(getList(todoList));
+                console.log(getTodoListString(todoItems));
             }
         });
     });
 }
 
-function execute(command, todoList){
+function execute(command, todoItems){
     switch(command){
         case "add":
             let date = new Date();
-            let dateString = `(${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}, ${date.getUTCHours() + 8}:${date.getUTCMinutes()}h)`
             let obj = {
                 "task": argument,
                 "done": false,
-                "created_at": dateString
+                "dateUpdated": date,
+                "dateUpdatedString": getDateString(date)
             }
-            todoList.push(obj);
+            todoItems.push(obj);
+            console.log(`"${obj.task}" was added to the todo list.`)
             break;
         case "done":
-            if (todoList[argument - 1].done === false){
-                todoList[argument - 1].done = true;
+            let doneString = "";
+            todoItem = todoItems[argument - 1];
+
+            if (todoItem.done === false){
+                todoItem.done = true;
+                doneString = "done";
             } else {
-                todoList[argument - 1].done === false
+                todoItem.done = false;
+                doneString = "undone";
             }
+
+            let newDate = new Date();
+            todoItem.dateUpdatedString = getDateString(newDate);
+            todoItem.date = newDate;
+            console.log(`"${todoItem.task}" was marked as ${doneString}.`);
             break;
         case "delete":
-            let deletedTask = todoList.splice(argument - 1, 1);
-            console.log("'" + deletedTask[0].task + "' was deleted.")
+            let deletedTask = todoItems.splice(argument - 1, 1);
+            console.log(`"${deletedTask[0].task}" was deleted.`)
             break;
         default:
             console.log("Invalid command!");
-            console.log(getList(todoList));
             break;
     }
 }
 
-function getList(todoList){
-    let list = "";
-    for(let obj in todoList){
-        let done = " ";
-        if (todoList[obj].done === true){
-            done = "X";
-        }
-        let todoItem = todoList[obj];
-        let dateCreated = todoItem.created_at;
-        list += parseInt(obj) + 1 + ". [" + done + "] - " + todoItem.task + " " + dateCreated + "\n";
+function isEmptyArray(array){
+    return (!Array.isArray(array) || !array.length);
+}
+
+function getDateString(date){
+    return `(Last updated: ${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}, ${date.getUTCHours() + 8}:${date.getUTCMinutes()<10?'0'+date.getUTCMinutes():date.getUTCMinutes()}h)`;
+}
+
+function getTodoListString(todo){
+    if (!Array.isArray(todo) || todo.length === 0){
+        return "The todo list is empty."
     }
-    return list;
+    let todoListString = "";
+    for(let obj in todo){
+        let todoItem = todo[obj];
+        let doneString;
+
+        if (todoItem.done){
+            doneString = "X";
+        } else {
+            doneString = " ";
+        }
+
+        todoListString += parseInt(obj) + 1 + ". [" + doneString + "] - " + todoItem.task + " " + todoItem.dateUpdatedString + "\n";
+    }
+    return todoListString;
 }
