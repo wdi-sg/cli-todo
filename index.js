9e48c9ac1bffaa1b3820e046548be542bb740e8d
Your Name <you@example.com>
2018-12-18 14:43:23 +0800

    basics done

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
-console.log("works!!", process.argv[2]);
-
-var commandType = process.argv[2];
-
-console.log("Your command was: "+commandType);
+// console.log("works!!", process.argv[2]);
 
 const jsonfile = require('jsonfile');
 
 const file = 'data.json'
 
+
+
+var commandType = (process.argv[2]).toUpperCase();
+
+var task = process.argv[3];
+
+
+console.log("Your command was: "+commandType);
+
+
 jsonfile.readFile(file, (err, obj) => {
 
-  console.log(obj);
-  obj["helloworld"] = "monkey";
+    if (commandType == "ADD") {
+        // console.log(task);
+        // console.log(obj);
+        obj["todoItems"].push(task);
 
-  jsonfile.writeFile(file, obj, (err) => {
-    console.log(err)
-  });
+        jsonfile.writeFile(file, obj, (err) => {
+        console.log(err)
+      });
+
+    } else if (commandType == "SHOW") {
+
+        for (let i = 0; i < obj.todoItems.length; i++) {
+            console.log((i+1)+". [] - "+obj.todoItems[i]);
+        }
+    }
 });
