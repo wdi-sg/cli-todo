0f2e1a7be0194c8dc93bd085fe25361ae781e3f2
jumpingrock <kennethyeongky@gmail.com>
2019-03-15 08:55:38 +0800

    mvp

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const jsonfile = require('jsonfile');
+const file = 'data.json'
+
 console.log("works!!", process.argv[2]);
 
 var commandType = process.argv[2];
@@ -5,21 +8,23 @@ var commandType = process.argv[2];
 console.log("Your command was: "+commandType);
 var toDoItem = [];
 if(commandType == "add"){
-    toDoItem.push(process.argv[3]);
+    toDoItem.push("[] - "+process.argv[3]);
     console.log(toDoItem);
-    obj.push(process.argv[3]);
-    jsonfile.writeFile(file, obj, (err) => {
-        console.log(err)
-    });
+    jsonfile.readFile(file, (err, obj) => {
+        console.log(obj);
+        obj.push("[] - " + process.argv[3]);
+        jsonfile.writeFile(file, obj, (err) => {
+            console.log(err)
+        });
+      });
+    
 }else if (commandType == ".showlist"){
     jsonfile.readFile(file, (err, obj) => {
     console.log(obj);
     });
 }
 
-const jsonfile = require('jsonfile');
 
-const file = 'data.json'
 
 jsonfile.readFile(file, (err, obj) => {
   console.log(obj);
