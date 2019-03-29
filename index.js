c3da3358ee4f7e3084fd9565c948b488399ae495
moses <moseslee90@gmail.com>
2019-03-29 19:16:28 +0800

    adding mark as done function

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log("works!!", process.argv[2]);
+console.log("works!!");
 
 var commandType = process.argv[2];
 
@@ -8,12 +8,46 @@ const jsonfile = require('jsonfile');
 
 const file = 'data.json'
 
-jsonfile.readFile(file, (err, obj) => {
+if (process.argv[2] === "add") {
+  //add an item to the to do list
+  let todoString = process.argv[3];
+  jsonfile.readFile(file, (err, obj) => {
+    if (err) {
+      console.log(err);
+    }
+    const todolistLength = Object.keys(obj).length;
+    //json file must have an index as key, and text as the value
+    for (let i = 1; i < todolistLength + 2; i++) {
+      if (obj[i] === undefined) {
+        obj[i] = {value:todoString, done:false};
+        console.log(i + ". [ ] - " + todoString)
+      }
+    }
 
-  console.log(obj);
-  obj["helloworld"] = "monkey";
-
-  jsonfile.writeFile(file, obj, (err) => {
-    console.log(err)
+    jsonfile.writeFile(file, obj, (err) => {
+      console.log(err)
+    });
   });
-});
+}
+if (process.argv[2] === "show") {
+  jsonfile.readFile(file, (err, obj)=>{
+    if (err) {
+      console.log(err);
+    }
+    const todolistLength = Object.keys(obj).length;
+    let done = "not reading done";
+    for (let i = 1; i < todolistLength + 1; i++) {
+      console.log(obj[i].done);
+      
+      if (obj[i].done === true) {
+        done = "x";
+      } else {
+        done = " ";
+      }
+
+      console.log(i + ". [" + done + "] - " + obj[i].value);
+
+
+    }
+  })
+}
