27b3d5d34af0036dd2343da05daddaedca449660
chuasweechin <sampiochua@gmail.com>
2019-03-29 21:10:17 +0800

    complete deliverable and further 1, 2, 3 and 4

--- index.js
+++ index.js
@@ -1,19 +1,114 @@
-console.log("works!!", process.argv[2]);
-
-var commandType = process.argv[2];
-
-console.log("Your command was: "+commandType);
-
 const jsonfile = require('jsonfile');
+const file = 'data.json';
 
-const file = 'data.json'
+var addZero = function(i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+}
 
-jsonfile.readFile(file, (err, obj) => {
+var showItems = function () {
+    jsonfile.readFile(file, (err, stuff) => {
+        stuff.todoItems.forEach(function(toDoItem) {
+            if (toDoItem.deleted === "false") {
+                if (toDoItem.done == "false") {
+                    console.log(`${ toDoItem.id }. [ ] - ${ toDoItem.item }`);
+                }
+                else if (toDoItem.done == "true") {
+                    console.log(`${ toDoItem.id }. [x] - ${ toDoItem.item }`);
+                }
+            }
+        });
+    });
+}
 
-  console.log(obj);
-  obj["helloworld"] = "monkey";
+var addItem = function (toDoItem) {
+    jsonfile.readFile(file, (err, stuff) => {
+        let temp = {};
+        let date = new Date();
+        let time = addZero(date.getHours()) + ":" + addZero(date.getMinutes()) + ":"
+                    + addZero(date.getSeconds());
+        let id =  stuff.todoItems.length + 1;
 
-  jsonfile.writeFile(file, obj, (err) => {
-    console.log(err)
-  });
-});
+        temp.id = id;
+        temp.item = toDoItem;
+        temp.done = "false";
+        temp.created_at = time;
+        temp.updated_at = "";
+        temp.deleted = "false";
+
+        stuff.todoItems.push(temp);
+
+        jsonfile.writeFile(file, stuff, (err) => {
+            if (err !== null) {
+                console.log(err)
+            }
+        });
+    });
+}
+
+var deleteItem = function (id) {
+    jsonfile.readFile(file, (err, stuff) => {
+        let date = new Date();
+        let time = addZero(date.getHours()) + ":" + addZero(date.getMinutes()) + ":"
+                    + addZero(date.getSeconds());
+
+        stuff.todoItems.forEach(function(item) {
+            if (item.id === Number(id)) {
+                item.deleted = "true";
+                item.updated_at = time;
+            }
+        });
+
+        jsonfile.writeFile(file, stuff, (err) => {
+            if (err !== null) {
+                console.log(err)
+            }
+        });
+    });
+}
+
+var markItemAsDone = function (id) {
+    jsonfile.readFile(file, (err, stuff) => {
+        let date = new Date();
+        let time = addZero(date.getHours()) + ":" + addZero(date.getMinutes()) + ":"
+                    + addZero(date.getSeconds());
+
+        stuff.todoItems.forEach(function(item) {
+            if (item.id === Number(id)) {
+                item.done = "true";
+                item.updated_at = time;
+            }
+        });
+
+        jsonfile.writeFile(file, stuff, (err) => {
+            if (err !== null) {
+                console.log(err)
+            }
+        });
+    });
+}
+
+const userCommandType = process.argv[2];
+const userInput = process.argv[3];
+
+if (userCommandType === "show") {
+    showItems();
+
+} else if (userCommandType === "add") {
+    addItem(userInput);
+    console.log("Item added!")
+
+} else if (userCommandType === "done"){
+    markItemAsDone(userInput);
+    console.log("Item marked as done!")
+
+} else if (userCommandType === "delete"){
+    deleteItem(userInput);
+    console.log("Item deleted!")
+
+} else {
+    console.log("Invalid Command Type! Please try again");
+
+}
