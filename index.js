c06967386f0dc6e84393f147fabf61868dc67856
Chang <ainurlindale@gmail.com>
2019-03-29 21:51:35 +0800

    part 1 done

--- index.js
+++ index.js
@@ -1,19 +1,42 @@
-console.log("works!!", process.argv[2]);
+// console.log("works!!", process.argv[2]);
 
-var commandType = process.argv[2];
+// var commandType = process.argv[2];
 
-console.log("Your command was: "+commandType);
+// console.log("Your command was: "+commandType);
+
+// const jsonfile = require('jsonfile');
+
+// const file = 'data.json'
+
+// jsonfile.readFile(file, (err, obj) => {
+
+//   console.log(obj);
+//   obj["helloworld"] = "monkey";
+
+//   jsonfile.writeFile(file, obj, (err) => {
+//     console.log(err)
+//   });
+
+
+console.log(process.argv);
 
 const jsonfile = require('jsonfile');
 
-const file = 'data.json'
+const file = 'data.json';
 
-jsonfile.readFile(file, (err, obj) => {
+const obj = {
+    1: '[ ] - eat bak kut teh',
+    2: '[ ] - go shopping',
+    3: '[ ] - feed dog',
+    4: '[ ] - swim practice',
+    5: '[ ] - code app',
+    6: '[ ] - meet gabriel',
+};
 
-  console.log(obj);
-  obj["helloworld"] = "monkey";
+if (process.argv[2] === 'write') {
+    console.log("THIS IS WORKING!")
+}
 
-  jsonfile.writeFile(file, obj, (err) => {
-    console.log(err)
-  });
+jsonfile.writeFile(file, obj, (err) => {
+  console.log(err)
 });
