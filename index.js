3a0c5524e32f69b6961a80282485c27230364cdc
Chang <ainurlindale@gmail.com>
2019-03-30 16:44:13 +0800

    basic stuff done

--- index.js
+++ index.js
@@ -1,42 +1,26 @@
-// console.log("works!!", process.argv[2]);
-
-// var commandType = process.argv[2];
-
-// console.log("Your command was: "+commandType);
-
-// const jsonfile = require('jsonfile');
-
-// const file = 'data.json'
-
-// jsonfile.readFile(file, (err, obj) => {
-
-//   console.log(obj);
-//   obj["helloworld"] = "monkey";
-
-//   jsonfile.writeFile(file, obj, (err) => {
-//     console.log(err)
-//   });
 
 
 console.log(process.argv);
 
 const jsonfile = require('jsonfile');
-
 const file = 'data.json';
+const fs = require('fs');
 
-const obj = {
-    1: '[ ] - eat bak kut teh',
-    2: '[ ] - go shopping',
-    3: '[ ] - feed dog',
-    4: '[ ] - swim practice',
-    5: '[ ] - code app',
-    6: '[ ] - meet gabriel',
-};
-
-if (process.argv[2] === 'write') {
-    console.log("THIS IS WORKING!")
+if (process.argv[2] === 'show') {
+  jsonfile.readFile(file, (err, obj) => {
+    console.log(obj);
+    });
 }
 
-jsonfile.writeFile(file, obj, (err) => {
-  console.log(err)
+if (process.argv[2] === 'write') {
+
+  jsonfile.readFile(file, (err, obj) => {
+
+  console.log(obj);
+  obj[process.argv[3]] = process.argv[4];
+
+  jsonfile.writeFile(file, obj, (err) => {
+    console.log(err)
+  });
 });
+}
