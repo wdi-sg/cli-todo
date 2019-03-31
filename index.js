ebf2f3ce9e15023fc74412b0a669cd83436c279b
seeo <aragonlotr5@gmail.com>
2019-03-31 14:15:47 +0800

    created another conditional statement that checks if user input 'show'. If user did, then index.js will simply show user what was written in the JSON file. Else, if 'add' keyword was used, the key-value pair will get written inside JSON file. Else, terminal show read out instructions on how to interact with index.js

--- index.js
+++ index.js
@@ -8,24 +8,34 @@ const file = 'data.json'
 //
 // }
 
-jsonfile.readFile(file, (err, obj) => {
-  let number = process.argv[3];
-  let item = process.argv[4];
-  console.log("Reading file now");
-  // console.log(obj);
-  obj.toDoItems[0][number] = item; // after reading, index.js saves the read file into obj; then goes into the
-  //'toDoItems' property name, first element of array, and starts to populate this array with userinput1 = process.argv[3]  and userinput2 = process.argv[4].
-  //process.argv[2] is implicitly an 'add' word that user ought to type out in the terminal
-  console.log("Done reading file");
+if (process.argv[2] == "add"){ //so instead of being implicit, I now code the read/write JSONfile function to only execute when process.argv[2] is explicitly named "add"
+  jsonfile.readFile(file, (err, obj) => {
+    let number = process.argv[3];
+    let item = process.argv[4];
+    console.log("***Reading file now***");
+    // console.log(obj);
+    obj.toDoItems[0][number] = item; // after reading, index.js saves the read file into obj; then goes into the
+    //'toDoItems' property name, first element of array, and starts to populate this array with userinput1 = process.argv[3]  and userinput2 = process.argv[4].
+    //process.argv[2] is implicitly an 'add' word that user ought to type out in the terminal
+    console.log("***Done reading file***");
 
-
-  jsonfile.writeFile(file, obj, { spaces: 2 }, (err) => { //used the spaces option so that JSON file contents' structure are always preserved. 
-    console.log("Writing file now");
+  jsonfile.writeFile(file, obj, { spaces: 2 }, (err) => { //used the spaces option so that JSON file contents' structure are always preserved.
+    console.log("***Writing file now***");
     console.log(err);
     console.log(obj);
-    console.log("Done writing file")
+    console.log("***Done writing file***");
+    });
   });
-});
+}else if((process.argv[2] == "show")){
+  jsonfile.readFile(file, (err, obj) => {
+    console.log("***Reading file now***");
+    console.log(obj);
+    console.log("***Done reading file***");
+  });
+}else{
+  console.log("Dear user,please tell me what items you want to add to the 'to do list' by using keyword 'add', space '[# sign]', space [item_name] in quotes. Or did you want to see the 'to do list'? If so then just say 'show'.");
+}
+
 
 
 // jsonfile.readFile(file, (err, obj) => {
