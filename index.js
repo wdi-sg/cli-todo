463b348e7b7d9cf771da0d1c11549883b2afca28
claucanchin <claucanchin@gmail.com>
2019-03-31 14:34:07 +0800

    completed basic deliverables

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
-console.log("works!!", process.argv[2]);
-
-var commandType = process.argv[2];
-
-console.log("Your command was: "+commandType);
-
 const jsonfile = require('jsonfile');
+const file = 'data.json';
 
-const file = 'data.json'
 
-jsonfile.readFile(file, (err, obj) => {
+if (process.argv[2] === "show") {
+    jsonfile.readFile(file, (err, obj) => {
+        if (err) console.error(err);
 
-  console.log(obj);
-  obj["helloworld"] = "monkey";
+        for (var i=0; i<obj.todoItems.length; i++) {
+            console.log((i + 1) + ". [ ] - " + obj.todoItems[i]);
+        }
+    });
+}
 
-  jsonfile.writeFile(file, obj, (err) => {
-    console.log(err)
-  });
-});
+if (process.argv[2] === "add") {
+    jsonfile.readFile(file, (err, obj) => {
+        if (err) console.error(err);
+
+        obj.todoItems.push(process.argv[3]);
+        for (var i=0; i<obj.todoItems.length; i++) {
+            console.log((i + 1) + ". [ ] - " + obj.todoItems[i]);
+        }
+        jsonfile.writeFile(file, obj, (err) => {
+            if (err) console.log(err);
+        });
+    });
+}
