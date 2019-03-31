b275f8f97e8e00cfa22e3515c2d80fa1507f438b
seeo <aragonlotr5@gmail.com>
2019-03-31 22:08:16 +0800

    added ability in index.js to write and save the 'created_at' and 'updated_at' values for each toDoItem element Next step to figure out how to permanently delete an element in the array...

--- index.js
+++ index.js
@@ -3,66 +3,82 @@ const jsonfile = require('jsonfile');
 // const chores = require('./chores');
 
 const file = 'data.json'
-
-// var add = (x,y) => {
-//
+//let check = process.argv[5];//included whether task is done, if task is not done,no need to type this parameter in terminal, if done, just type anything, it will work and input a cross
+// if(check){
+//   obj.toDoItems[0][number] = "["+"x"+"] - "+item;
+// }else{
+//   obj.toDoItems[0][number] = "["+" "+"] - "+item;
 // }
+
+let today = new Date();  //source: https://stackoverflow.com/questions/1531093/how-do-i-get-the-current-date-in-javascript
+let dd = String(today.getDate()).padStart(2, '0');
+let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+let yyyy = today.getFullYear();
+
+today = dd + '/' + mm + '/' + yyyy;
 
 if (process.argv[2] == "add"){ //so instead of being implicit, I now code the read/write JSONfile function to only execute when process.argv[2] is explicitly named "add"
   jsonfile.readFile(file, (err, obj) => {
     let number = process.argv[3];
     let item = process.argv[4];
-    console.log("***Reading file now***");
-    // console.log(obj);
-    obj.toDoItems[0][number] = item; // after reading, index.js saves the read file into obj; then goes into the
-    //'toDoItems' property name, first element of array, and starts to populate this array with userinput1 = process.argv[3]  and userinput2 = process.argv[4].
-    //process.argv[2] is implicitly an 'add' word that user ought to type out in the terminal
+
+    if(err){
+      console.log("***Error found in reading file***");
+    }
+      console.log("***Reading file now***");
+
+      obj.toDoItems[number-1][number+"."] = item;
+      obj.toDoItems[number-1]["created_at"] = today;
+
     console.log("***Done reading file***");
 
   jsonfile.writeFile(file, obj, { spaces: 2 }, (err) => { //used the spaces option so that JSON file contents' structure are always preserved.
     console.log("***Writing file now***");
-    console.log(err);
-    console.log(obj);
+    //console.log(err);
+    if(err){
+      console.log("***Error found in writing file***");
+    }
+    console.log(obj.toDoItems);
+    console.log("***Done writing file***");
+    });
+  });
+}else if(process.argv[2] == "done"){
+  jsonfile.readFile(file, (err, obj) => {
+    let number = process.argv[3];
+    if(err){
+      console.log("***Error found in reading file***");
+    }
+    console.log("***Reading file now***");
+    obj.toDoItems[number-1]["completion"] = "done";
+    obj.toDoItems[number-1]["updated_at"] = today;
+
+    console.log("***Done reading file***");
+
+  jsonfile.writeFile(file, obj, { spaces: 2 }, (err) => {
+    console.log("***Writing file now***");
+    if(err){
+      console.log("***Error found in writing file***");
+    }
+    console.log(obj.toDoItems);
     console.log("***Done writing file***");
     });
   });
 }else if((process.argv[2] == "show")){
   jsonfile.readFile(file, (err, obj) => {
     console.log("***Reading file now***");
-    console.log(obj);
+    if(err){
+      console.log("***Error found in reading file***");
+    }
+    console.log(obj.toDoItems);
     console.log("***Done reading file***");
   });
+
 }else{
-  console.log("Dear user,please tell me what items you want to add to the 'to do list' by using keyword 'add', space '[# sign]', space [item_name] in quotes. Or did you want to see the 'to do list'? If so then just say 'show'.");
+  console.log("***Dear user,please tell me what items you want to add to the 'to do list' by using keyword 'add', space '[#]', space [item_name] in quotes.\
+  Or did you want to see the 'to do list'?\
+  If so then just say 'show'.***");
 }
 
-
-
-// jsonfile.readFile(file, (err, obj) => {
-//   console.log("Reading file now");
-//   function add () {
-//
-//     number = process.argv[3];
-//     doneOrNot = process.argv[4]+ " - ";
-//     item = process.argv[5];
-//
-//     if (doneOrNot == "done") {
-//         doneOrNot = "[x]"
-//     } else if (doneOrNot == "undone") {
-//         doneOrNot = "[ ]"
-//     }
-//
-//         obj[number] = (doneOrNot + item);
-//          console.log("Done reading file");
-// }
-//
-//   jsonfile.writeFile(file, obj, { spaces: 2 }, (err) => {
-//     console.log("Writing file now");
-//     console.log(err)
-//     console.log(obj);
-//     console.log("Done writing file")
-//   });
-// });
 
 
 
