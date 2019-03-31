28eb252f2c6c10883e8147e01155fb861c5fba09
claucanchin <claucanchin@gmail.com>
2019-03-31 17:21:28 +0800

    edited - show only line that was added

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
 const jsonfile = require('jsonfile');
-const file = 'data.json';
+const file = "data.json";
 
-
+//see the list
 if (process.argv[2] === "show") {
     jsonfile.readFile(file, (err, obj) => {
-        if (err) console.error(err);
+        if (err) console.log(err);
 
-        for (var i=0; i<obj.todoItems.length; i++) {
-            console.log((i + 1) + ". [ ] - " + obj.todoItems[i]);
+        if (obj.todoItems.length === 0) {
+            console.log("Error: List is empty");
+        } else {
+            for (i=0; i<obj.todoItems.length; i++) {
+                console.log((i+1) + ". [ ] - " + obj.todoItems[i]);
+            }
         }
     });
 }
 
+//add to the list
 if (process.argv[2] === "add") {
     jsonfile.readFile(file, (err, obj) => {
-        if (err) console.error(err);
+        if (err) console.log(err);
 
         if (process.argv[3] === undefined) {
-            console.log("Error: add task after 'add'");
+            console.log("Error: Enter \"task\" after \"add\"");
         } else {
             obj.todoItems.push(process.argv[3]);
-            for (var i=0; i<obj.todoItems.length; i++) {
-                console.log((i + 1) + ". [ ] - " + obj.todoItems[i]);
-            }
+            console.log((obj.todoItems.length) + ". [ ] - " + process.argv[3]);
+
             jsonfile.writeFile(file, obj, (err) => {
                 if (err) console.log(err);
             });
