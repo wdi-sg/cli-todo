f3d7116ece553a70f72b825d887069431bbac1a3
seeo <aragonlotr5@gmail.com>
2019-03-31 12:39:00 +0800

    created index.js file that allows user to add property name and property value pair as process.argv[3] and process.argv[4], respectively. process.argv[2] is implicitly empty so that it can take in pseudo-commmands like 'add' and in future 'delete'. but first, will need to build out a function in index.js that allows script to understand what should be done to JSON data file when user types 'delete' in terminal.

--- index.js
+++ index.js
@@ -1,19 +1,77 @@
-console.log("works!!", process.argv[2]);
-
-var commandType = process.argv[2];
-
-console.log("Your command was: "+commandType);
 
 const jsonfile = require('jsonfile');
+// const chores = require('./chores');
 
 const file = 'data.json'
 
+// var add = (x,y) => {
+//
+// }
+
 jsonfile.readFile(file, (err, obj) => {
 
-  console.log(obj);
-  obj["helloworld"] = "monkey";
+  console.log("Reading file now");
+  // console.log(obj);
+  obj.toDoItems[0][process.argv[3]] = process.argv[4];
+  console.log("Done reading file");
 
-  jsonfile.writeFile(file, obj, (err) => {
-    console.log(err)
+
+  jsonfile.writeFile(file, obj, { spaces: 2 }, (err) => {
+    console.log("Writing file now");
+    console.log(err);
+    console.log(obj);
+    console.log("Done writing file")
   });
 });
+
+
+// jsonfile.readFile(file, (err, obj) => {
+//   console.log("Reading file now");
+//   function add () {
+//
+//     number = process.argv[3];
+//     doneOrNot = process.argv[4]+ " - ";
+//     item = process.argv[5];
+//
+//     if (doneOrNot == "done") {
+//         doneOrNot = "[x]"
+//     } else if (doneOrNot == "undone") {
+//         doneOrNot = "[ ]"
+//     }
+//
+//         obj[number] = (doneOrNot + item);
+//          console.log("Done reading file");
+// }
+//
+//   jsonfile.writeFile(file, obj, { spaces: 2 }, (err) => {
+//     console.log("Writing file now");
+//     console.log(err)
+//     console.log(obj);
+//     console.log("Done writing file")
+//   });
+// });
+
+
+
+//Prompt: User, please enter number as the first input and 'to do item' as the second input.
+// 1. "eat bak ku teh"
+
+// console.log("works!!", process.argv[2]);
+//
+// var commandType = process.argv[2];
+//
+// console.log("Your command was: "+commandType);
+//
+// const jsonfile = require('jsonfile');
+//
+// const file = 'data.json'
+//
+// jsonfile.readFile(file, (err, obj) => {
+//
+//   console.log(obj);
+//   obj["helloworld"] = "monkey";
+//
+//   jsonfile.writeFile(file, obj, (err) => {
+//     console.log(err)
+//   });
+// });
