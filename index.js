4b74178c813f402b6b35fe7263e5348f86ccc717
Chang <ainurlindale@gmail.com>
2019-03-31 13:57:12 +0800

    can delete items

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ console.log(process.argv);
 
 const jsonfile = require('jsonfile');
 const file = 'data.json';
-const fs = require('fs');
+const json = require('./data.json');
+
 
 if (process.argv[2] === 'show') {
   jsonfile.readFile(file, (err, obj) => {
@@ -22,5 +23,22 @@ if (process.argv[2] === 'write') {
   jsonfile.writeFile(file, obj, (err) => {
     console.log(err)
   });
+});
+}
+
+if (process.argv[2] === 'delete') {
+
+    let key = process.argv[3];
+    delete json[key];
+    console.log(json);
+
+    jsonfile.readFile(file, (err, obj) => {
+
+    console.log(obj);
+
+  jsonfile.writeFile(file, json, (err) => {
+    console.log(err)
+  });
+
 });
 }
