e02cf5325c3ed1568228bb4c94a5c0dee3082210
valenlyn <valenlynchua@gmail.com>
2019-04-01 08:41:40 +0800

    Added timestamp (created at)

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ console.log("Command: ", process.argv[2]);
 
 var commandType = process.argv[2];
 
-// console.log("Your command was: "+commandType);
+// console.log(" "+commandType);
 
 const jsonfile = require('jsonfile');
 
@@ -14,12 +14,14 @@ const obj = {
 
 const art = "  _            _       \n | |_ ___   __| | ___  \n | __/ _ \\ / _` |/ _ \\ \n | || (_) | (_| | (_) | \n  \\__\\___/ \\__,_|\\___/ \n";
 
+const artTwo = "           *-*,\n       ,*\\/|`| \\\n       \\'  | |'| *,\n        \\ `| | |/ )\n         | |'| , /\n         |'| |, /\n       __|_|_|_|_\n      [___________]\n       |         |\n       |  VAL'S  |\n       |  TODO   |\n       |  LIST   |\n       |_________|";
+
 jsonfile.readFile(file, (err, obj) => {
 
       let array = obj["todoItems"];
 
       var show = function(){
-            console.log(art);
+            console.log(artTwo);
              for (var i = 0; i < array.length; i++) {
                   console.log(i+1 + array[i]);
              }
@@ -27,8 +29,10 @@ jsonfile.readFile(file, (err, obj) => {
 
         if (process.argv[2] === "add") {
 
+            var createdAt = new Date();
+
             // not adding list index no. here so that it's easier to print list later when items are deleted later
-           array.push(". [ ] - "+ process.argv[3]);
+           array.push(". [ ] - "+ process.argv[3] + " "+ createdAt);
 
            console.log((array.length) +". [ ] - "+ process.argv[3]);
 
