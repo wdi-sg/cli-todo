57a7ef8430166c5a130bcc86c1ee20156043f8ec
kach92 <kenny.ang9206@gmail.com>
2019-07-12 18:06:20 +0800

    done with first part

--- index.js
+++ index.js
@@ -1,19 +1,65 @@
-console.log("works!!", process.argv[2]);
+// console.log("works!!", process.argv[2]);
 
-var commandType = process.argv[2];
 
-console.log("Your command was: "+commandType);
+
+// console.log("Your command was: "+commandType);
+
 
 const jsonfile = require('jsonfile');
-
 const file = 'data.json'
 
-jsonfile.readFile(file, (err, obj) => {
+var commandType = process.argv[2];
+var input1 = process.argv[3];
 
-  console.log(obj);
-  obj["helloworld"] = "monkey";
+const addItem = function(newItem) {
+    jsonfile.readFile(file, (err, obj) => {
 
-  jsonfile.writeFile(file, obj, (err) => {
-    console.log(err)
-  });
-});
+        if (err) {
+            console.log(err)
+        } else {
+            obj.todoItems.push(`${obj.counter}. [ ] - ${newItem}`)
+            obj.counter++;
+
+            jsonfile.writeFile(file, obj, (err) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log("Writing Done");
+                }
+
+            });
+        }
+
+    });
+}
+
+const showToDoList = function() {
+    jsonfile.readFile(file, (err, obj) => {
+        if (err) {
+            console.log(err);
+        } else {
+            let str = "";
+            for (let i = 0; i < obj.todoItems.length; i++) {
+                str += obj.todoItems[i] + "\n";
+            }
+            console.log(str);
+        }
+    })
+}
+
+const showDescription = function() {
+    console.log("Later");
+}
+
+if (commandType === undefined) {
+    showDescription();
+} else {
+    switch (commandType) {
+        case "add":
+            addItem(input1);
+            break;
+        case "show":
+            showToDoList();
+
+    }
+}
