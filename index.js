0afff6e78857c7343a6c720909311c3dc3747674
kach92 <kenny.ang9206@gmail.com>
2019-07-12 19:37:22 +0800

    left last further

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-// console.log("works!!", process.argv[2]);
-
-
-
-// console.log("Your command was: "+commandType);
-
-
 const jsonfile = require('jsonfile');
 const file = 'data.json'
 
+const figlet = require('figlet');
+
 var commandType = process.argv[2];
 var input1 = process.argv[3];
+
+const createAscii = function(str) {
+    figlet(str, function(err, data) {
+        if (err) {
+            console.log('Something went wrong...');
+            console.dir(err);
+            return;
+        }else{
+            console.log(data)
+        }
+
+    });
+}
+
 
 const addItem = function(newItem) {
     jsonfile.readFile(file, (err, obj) => {
@@ -17,7 +26,9 @@ const addItem = function(newItem) {
         if (err) {
             console.log(err)
         } else {
-            obj.todoItems.push(`${obj.counter}. [ ] - ${newItem}`)
+            let created_at = new Date();
+
+            obj.todoItems.push(`${obj.counter}. [ ] - ${newItem}\t\tCreated At: ${created_at}`);
             obj.counter++;
 
             jsonfile.writeFile(file, obj, (err) => {
@@ -47,8 +58,63 @@ const showToDoList = function() {
     })
 }
 
+const markAsDone = function(itemNum) {
+    jsonfile.readFile(file, (err, obj) => {
+
+        if (err) {
+            console.log(err)
+        } else {
+            let parsedItemNum = parseInt(itemNum);
+            let updated_at = new Date();
+            let tempArr = obj.todoItems[parsedItemNum - 1].split("");
+            tempArr[4] = "x";
+            obj.todoItems[parsedItemNum - 1] = tempArr.join("");
+            obj.todoItems[parsedItemNum - 1] += `\tUpdated at: ${updated_at}`;
+
+
+            jsonfile.writeFile(file, obj, (err) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log("Mark Done");
+                }
+
+            });
+        }
+
+    });
+}
+
+const deleteItem = function(itemNum) {
+    jsonfile.readFile(file, (err, obj) => {
+
+        if (err) {
+            console.log(err)
+        } else {
+            let parsedItemNum = parseInt(itemNum);
+            obj.todoItems.splice(parsedItemNum - 1, 1);
+            for (let i = parsedItemNum - 1; i < obj.todoItems.length; i++) {
+                let str = obj.todoItems[i].split("");
+                str[0] = parseInt(str[0]) - 1;
+                obj.todoItems[i] = str.join("");
+            }
+            obj.counter--;
+
+            jsonfile.writeFile(file, obj, (err) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log("Delete Done Sir");
+                }
+
+            });
+        }
+
+    });
+}
+
 const showDescription = function() {
-    console.log("Later");
+    createAscii("Welcome");
 }
 
 if (commandType === undefined) {
@@ -60,6 +126,15 @@ if (commandType === undefined) {
             break;
         case "show":
             showToDoList();
-
+            break;
+        case "done":
+            markAsDone(input1);
+            break;
+        case "delete":
+            deleteItem(input1);
+            break;
+        default:
+            createAscii("ERROR!!!")
+            break;
     }
 }
