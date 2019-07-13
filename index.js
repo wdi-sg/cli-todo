deee150e3d00e55b2514bc1919e0bbe892a96220
Jonathan <jonathanbek93@gmail.com>
2019-07-13 22:02:43 +0800

    change list items to objects, added date

--- index.js
+++ index.js
@@ -12,22 +12,39 @@ const file = 'data.json'
 var notDone = "[ ]";
 var done = "[x]";
 let item = "";
+var listItem = {};
+
+var year = 0;
+var month = 0;
+var day = 0;
+
 
 const add = function(obj){
     let counter = obj.todoItems.length + 1;
     item = `${counter}. ${notDone} - ${task}`;
 }
 
-if (commandType === "add"){
+const getDate = function(){
+    let dateEverything = new Date();
+    year = dateEverything.getFullYear();
+    month = dateEverything.getMonth();
+    day = dateEverything.getDate();
+}
 
+
+if (commandType === "add"){
   jsonfile.readFile(file, (err, obj) => {
     if(obj.todoItems){
         add(obj);
-        obj.todoItems.push(item);
-        console.log("added: "+item);
+        getDate();
+        let displayDate = `${year}-${month}-${day}`
+        listItem.task = item;
+        listItem.dateAdded = displayDate;
+        obj.todoItems.push(listItem);
+        console.log(`added: ${listItem.task} at date: ${listItem.dateAdded}`);
     } else {
         obj.todoItems = [];
-        console.log("no array found. created array!");
+        console.log("initialised. Please input again! >_<");
     }
 
     jsonfile.writeFile(file, obj, (err) => {
@@ -44,15 +61,15 @@ if (commandType === "add"){
         let doneItemIndex = process.argv[3] - 1;
         console.log("doneitemindex: "+doneItemIndex)
 
-        let oldItem = obj.todoItems[doneItemIndex]
+        let oldItem = obj.todoItems[doneItemIndex].task
         console.log('oldItem : '+oldItem)
 
         let newItem = oldItem.replace("[ ]", ["[x]"])
-        console.log('newitem : '+newItem)
+        console.log('newItem : '+newItem)
 
         // oldItem = newItem;
 
-        obj.todoItems[doneItemIndex] = newItem;
+        obj.todoItems[doneItemIndex].task = newItem;
 
     jsonfile.writeFile(file, obj, (err) => {
     console.log(err)
