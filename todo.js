3b91be025c01c09e1057bd4eb1d14356a2bc8672
Yi Xin Ang <angyixin@gmail.com>
2019-07-13 14:53:52 +0800

    able to delete and reassign index.

--- todo.js
+++ todo.js
@@ -20,21 +20,21 @@ var userInput  = process.argv[3];
 // });
 
 // --------------------------------------------------------------------------------------------------
-// add function
+// Add a task
 // --------------------------------------------------------------------------------------------------
 
 var add = function (newItem) {
     jsonfile.readFile(file, (err, data) => {
-        let arr = {};
+        let item = {};
 
-        arr.id        = data.todoItems.length + 1;
-        arr.task      = newItem;
-        arr.completed = false;
+        item.id        = data.todoItems.length + 1;
+        item.task      = newItem;
+        item.done = false;
         // arr.deleted = "false";
         // arr.created_at = getCurrentDateAndTime();
         // arr.updated_at = "";
 
-        data.todoItems.push(arr);
+        data.todoItems.push(item);
 
         jsonfile.writeFile(file, data, (err) => {
             if (err) {
@@ -45,7 +45,7 @@ var add = function (newItem) {
 }
 
 // --------------------------------------------------------------------------------------------------
-// show function
+// Show the task list
 // --------------------------------------------------------------------------------------------------
 var show = function () {
   jsonfile.readFile(file, (err, data) => {
@@ -55,7 +55,7 @@ var show = function () {
       console.log('There is no tasks in the todo list, please add some tasks');
     } else {
       for (var i = 0; i < data.todoItems.length; i++){
-        if (data.todoItems[i].completed  === true){
+        if (data.todoItems[i].done  === true){
           console.log(data.todoItems[i].id + ". "+ "[x] " + data.todoItems[i].task);
         } else {
           console.log(data.todoItems[i].id + ". "+ "[ ] " + data.todoItems[i].task);
@@ -68,19 +68,19 @@ var show = function () {
 }
 
 // --------------------------------------------------------------------------------------------------
-// markDone function
+// Mark a task done
 // --------------------------------------------------------------------------------------------------
 
-var check = function (index) {
+var markDone = function (index) {
     jsonfile.readFile(file, (err, data) => {
         var j = index - 1;
-        if (data.todoItems[j].completed === false){
-          data.todoItems[j].completed = true;
+
+        // this if/else statement can uncheck
+        if (data.todoItems[j].done === false){
+          data.todoItems[j].done = true;
         } else {
-          data.todoItems[j].completed = false;
+          data.todoItems[j].done = false;
         }
-
-
 
         jsonfile.writeFile(file, data, (err) => {
             if (err) {
@@ -90,6 +90,29 @@ var check = function (index) {
     });
 }
 
+// --------------------------------------------------------------------------------------------------
+// Delete a task
+// --------------------------------------------------------------------------------------------------
+
+var deleteTask = function (index) {
+    jsonfile.readFile(file, (err, data) => {
+
+        //deletes the tasks at the j position, 1 item
+        var j = index - 1;
+        data.todoItems.splice(j,1);
+
+        //after deleting, reassigns index
+        for (var i=0; i< data.todoItems.length; i++){
+          data.todoItems[i].id = i+1;
+        }
+
+        jsonfile.writeFile(file, data, (err) => {
+            if (err) {
+                console.log(err)
+            }
+        });
+    });
+}
 
 // --------------------------------------------------------------------------------------------------
 // cli flow, show instructions or execute commands depending on userInput
@@ -98,7 +121,7 @@ const instructions = function() {
     console.log("Welcome to the todo app, here is how you use it");
     console.log("Type  node todo.js show            -  to show the to do list");
     console.log("Type  node todo.js add 'new task'  -  to add a new task");
-    console.log("Type  node todo.js check 1         -  to mark task 1 done or not done");
+    console.log("Type  node todo.js done 1          -  to mark task 1 done or not done");
     console.log("Type  node todo.js delete 1        -  to delete task 1");
 }
 
@@ -106,22 +129,35 @@ if (commandType === "show") {
     show();
 }
 else if (commandType === "add") {
-    add(userInput);
-    setTimeout(function(){
-      show();
-    },100);
+    if(userInput === undefined){
+      console.log(`Please indicate a task name, type node todo.js add 'new task'`);
+    } else {
+      add(userInput);
+      setTimeout(function(){
+        show();
+      },100);
+    }
 }
-else if (commandType === "check") {
-    check(userInput);
+else if (commandType === "done") {
+
+  if (userInput === undefined){
+    console.log(`Please indicate a task number`);
+  } else {
+    markDone(userInput);
     setTimeout(function(){
       show();
     },100);
+  }
 }
 else if (commandType === "delete") {
-    // delele(userInput);
+  if (userInput === undefined){
+    console.log(`Please indicate a task number`);
+  } else {
+    deleteTask(userInput);
     setTimeout(function(){
       show();
-    },100);
+    },500);
+  }
 }
 else {
     //default
