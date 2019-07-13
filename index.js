285ac9ebf320314809369be822833cd4683f5ec6
gitwenlei <ngwenlei@gmail.com>
2019-07-13 16:33:06 +0800

    Push new user entered input to todoItems array list

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-console.log("works!!", process.argv[2]);
+
+console.log("works!!" + process.argv[2]);
 
 var commandType = process.argv[2];
 
-console.log("Your command was: "+commandType);
+console.log("Your command was: " + commandType);
 
 const jsonfile = require('jsonfile');
-
 const file = 'data.json'
 
+// var str = '{"todoItems":[]}' ;
+
 jsonfile.readFile(file, (err, obj) => {
+    console.log("current list: ", obj);
 
-  console.log(obj);
-  obj["helloworld"] = "monkey";
+    // Add new item to array
+    obj["todoItems"].push(commandType);
 
-  jsonfile.writeFile(file, obj, (err) => {
-    console.log(err)
-  });
+    jsonfile.writeFile(file, obj, (err) => {
+        console.log(err)
+    });
+    console.log("updated list: ", obj);
 });
