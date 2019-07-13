9a08f7d99468cf017bc694e05d6d313d10e1ce25
gitwenlei <ngwenlei@gmail.com>
2019-07-13 17:44:26 +0800

    Upated wit add to the list feature

--- index.js
+++ index.js
@@ -2,22 +2,25 @@
 console.log("works!!" + process.argv[2]);
 
 var commandType = process.argv[2];
+var item = process.argv[3];
 
-console.log("Your command was: " + commandType);
+console.log("Your command was: " + " " + commandType + " " + item);
 
 const jsonfile = require('jsonfile');
 const file = 'data.json'
 
-// var str = '{"todoItems":[]}' ;
 
 jsonfile.readFile(file, (err, obj) => {
     console.log("current list: ", obj);
 
-    // Add new item to array
-    obj["todoItems"].push(commandType);
+    // Check if user input command is 'add'
+    if (commandType == 'add') {
+        // Add new item to array list
+        obj["todoItems"].push(item);
 
-    jsonfile.writeFile(file, obj, (err) => {
-        console.log(err)
-    });
-    console.log("updated list: ", obj);
+        jsonfile.writeFile(file, obj, (err) => {
+            console.log(err)
+        });
+            console.log("updated list: ", obj);
+    }
 });
