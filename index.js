0b5f63b4d06d0ac1f5a1e4ee808580144c1d7b06
gitwenlei <ngwenlei@gmail.com>
2019-07-14 01:04:09 +0800

    Update user input variables, showList function

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 console.log("works!!" + process.argv[2]);
 
 var commandType = process.argv[2];
-var item = process.argv[3];
+var value = process.argv[3];
 
-console.log("Your command was: " + " " + commandType + " " + item);
+console.log("Your command was: " + commandType + " " + value);
 
 const jsonfile = require('jsonfile');
 const file = 'data.json';
@@ -16,33 +16,55 @@ var addToList = function(task) {
     jsonfile.readFile(file, (err, obj) => {
         console.log("current list: ", obj);
         // Add new item to array list
-        obj["todoItems"].push(item);
+        obj["todoItems"].push(task);
 
         jsonfile.writeFile(file, obj, (err) => {
             console.log(err);
         });
-        console.log("updated list: ", obj);
+            console.log("updated list: ", obj);
     });
 };
 
 
 // how all items on list from json file
 var showList = function() {
+    let arr = [];
 
     jsonfile.readFile(file, (err, obj) => {
-        console.log("latest list: " , obj);
+        for (let i=0; i < obj.todoItems.length; i++) {
+            arr.push((i+1) + '. [ ] - ' + obj.todoItems[i]);
+        }
+        console.log(arr);
+    });
+};
+
+
+// if item = 4, then put an 'x' on item 4 to mark as done
+var markAsDone = function(index) {
+    let arr = [];
+
+    jsonfile.readFile(file, (err, obj) => {
 
         for (let i=0; i < obj.todoItems.length; i++) {
-            console.log((i+1) + '. [ ] - ' + obj.todoItems[i]);
+            let tmpIndex = index - 1;
+
+            if (tmpIndex !== i) {
+                arr.push((i+1) + '. [ ] - ' + obj.todoItems[i]);
+            } else {
+                arr.push((i+1) + '. [x] - ' + obj.todoItems[i]);
+            }
         }
+        console.log(arr);
     });
 };
 
 
 // Check if user input command is 'add'
-if (commandType == 'add') {
-    addToList(item);
+if (commandType === 'add') {
+    addToList(value);
 // Check if user input command is 'show'
-} else if (commandType == 'show') {
+} else if (commandType === 'show' && value === undefined) {
     showList();
+} else if (commandType ==='done') {
+    markAsDone(value);
 }
