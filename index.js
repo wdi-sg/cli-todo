81c1d9b7d6b69a723bcf0d8ae5786b7e0e5b6da1
emijay <hilmi.johari@yahoo.com.sg>
2019-07-13 22:07:43 +0800

    Added code for when a user marks an item as "done"

--- index.js
+++ index.js
@@ -27,12 +27,12 @@ const readDataFile = () => {
         } else {
 
             if (obj["todoItems"] === undefined || obj["todoItems"].length == 0) {
-                console.log('There is nothing on the list yet')
+                console.log('There is nothing on the list.\n Would you like to add something to the list?')
 
             } else if (obj["todoItems"].length > 0) {
 
                 for (let i = 0; i < obj["todoItems"].length; i ++) {
-                    console.log((i + 1) + ". [ ] - " + obj["todoItems"][i])
+                    console.log((i + 1) + ". " + obj["todoItems"][i])
                 }
             }
         }
@@ -49,7 +49,34 @@ const addNewItem = (inputValue) => {
         if (err) {
             console.log('there is an error!');
         } else {
-            obj["todoItems"].push(inputValue);
+            obj["todoItems"].push("[ ] - " + inputValue);
+
+            jsonfile.writeFile(file, obj, (err) => {
+                if (err) {
+                    console.log('there is an error!')
+                } else {
+                    console.log(obj);
+                }
+            });
+        };
+    });
+}
+
+//----- Function for marking item as done -----//
+
+const markDone = (numOnList) => {
+
+    jsonfile.readFile(file, (err, obj) => {
+
+        if (err) {
+            console.log('there is an error!');
+        } else {
+
+            let itemInArray = obj["todoItems"][numOnList - 1];
+
+            itemInArray = itemInArray.substring(0,1) + "x" + itemInArray.substring(2);
+
+            obj["todoItems"][numOnList - 1] = itemInArray;
 
             jsonfile.writeFile(file, obj, (err) => {
                 if (err) {
@@ -67,11 +94,13 @@ const checkCommandType = (userInput) => {
     let commandType = process.argv[2];
 
     if (commandType === undefined ) {
-        console.log('Please choose one of the following options: \n show - See to-do list \n add - Add something to to-do list')
+        console.log('Please choose one of the following options:\n show - See to-do list\n add - Add something to to-do list\n done - Mark an item on list as done')
     } else if (commandType === "show") {
         readDataFile();
     } else if (commandType === "add") {
-        addNewItem(userInput)
+        addNewItem(userInput);
+    } else if (commandType === "done") {
+        markDone(userInput);
     }
 }
 
