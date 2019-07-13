90d0639fc327f32e13d7733ab0464b9b6eb028fd
shirleytwl <shirtanwl@gmail.com>
2019-07-13 15:55:20 +0800

    base done

--- index.js
+++ index.js
@@ -1,19 +1,69 @@
-console.log("works!!", process.argv[2]);
-
-var commandType = process.argv[2];
-
-console.log("Your command was: "+commandType);
-
+//include jsonfile node module
 const jsonfile = require('jsonfile');
+//file path of json file
+const file = 'data.json';
 
-const file = 'data.json'
+//store user command
+const userCommand = process.argv[2];
 
-jsonfile.readFile(file, (err, obj) => {
+// add item function
+const addItem = () => {
+  //store user input
+  let userInput = "";
+  for(let i=0;i<process.argv.length-3;i++){
+    userInput += process.argv[i+3]+" ";
+  }
+  userInput = userInput.substring(0, userInput.length-1);
 
-  console.log(obj);
-  obj["helloworld"] = "monkey";
-
-  jsonfile.writeFile(file, obj, (err) => {
-    console.log(err)
+  //read json file and then, write into file
+  jsonfile.readFile(file,(err,obj) => {
+  	if (!err) {
+	    const todoList = obj.todoItems;
+	    const newItem = {
+		    item: userInput,
+		    check: "[ ]"
+	    };
+	    todoList.push(newItem);
+	    jsonfile.writeFile(file, obj, (err) => {
+		    if (!err) {
+			    console.log(`'${userInput}' have been added to the todo list!`);
+		    }
+	    });
+    }
   });
-});
+};
+
+// show items function
+const showItem = () => {
+  jsonfile.readFile(file,(err,obj) => {
+  	if (!err){
+	    const todoList = obj.todoItems;
+	    for(let i=0;i<todoList.length;i++){
+	      console.log(`${i+1}. ${todoList[i].check} – ${todoList[i].item}`);
+	    }
+    }
+  })
+};
+
+// show instructions
+const showInstructions = () => {
+  console.log("To use any of the following commands, enter the command key:");
+  console.log("1. Add new item – add [item name]");
+  console.log("2. Show items – show");
+};
+
+// if user did not enter a command
+if (!userCommand) {
+  showInstructions();
+}
+else {
+  // check what command the user have entered
+  switch (userCommand) {
+    case "add" :
+      addItem();
+      break;
+    case "show" :
+      showItem();
+      break;
+  }
+}
