b557d0e6995d3eca5cdd2f8a5bf2c94be7fe686d
gitwenlei <ngwenlei@gmail.com>
2019-07-13 17:59:52 +0800

    Added addToList function

--- index.js
+++ index.js
@@ -9,20 +9,22 @@ console.log("Your command was: " + " " + commandType + " " + item);
 const jsonfile = require('jsonfile');
 const file = 'data.json';
 
+// Add task to list
+var addToList = function(task) {
 
-jsonfile.readFile(file, (err, obj) => {
-
-    console.log("current list: ", obj);
-
-    // Check if user input command is 'add'
-    if (commandType == 'add') {
+    jsonfile.readFile(file, (err, obj) => {
+        console.log("current list: ", obj);
         // Add new item to array list
         obj["todoItems"].push(item);
 
         jsonfile.writeFile(file, obj, (err) => {
-            // console.log(err)
+            console.log(err);
         });
-
         console.log("updated list: ", obj);
-    }
-});
+    });
+};
+
+        // Check if user input command is 'add'
+if (commandType == 'add') {
+    addToList(item);
+}
