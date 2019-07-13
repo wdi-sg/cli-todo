4d1a52f95b68ac38c0b8063e098aee5acc296fd1
Jonathan <jonathanbek93@gmail.com>
2019-07-13 16:29:38 +0800

    completed add, show, doneX

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 console.log("works!!", process.argv[2]);
 
 var commandType = process.argv[2];
+var task = process.argv[3];
 
 console.log("Your command was: "+commandType);
 
@@ -8,12 +9,87 @@ const jsonfile = require('jsonfile');
 
 const file = 'data.json'
 
-jsonfile.readFile(file, (err, obj) => {
+var notDone = "[ ]";
+var done = "[x]";
+let item = "";
 
+const add = function(obj){
+    let counter = obj.todoItems.length + 1;
+    item = `${counter}. ${notDone} - ${task}`;
+}
 
-  obj["helloworld"] = "yo";
-  console.log(obj);
-  jsonfile.writeFile(file, obj, (err) => {
+if (commandType === "add"){
+
+  jsonfile.readFile(file, (err, obj) => {
+    if(obj.todoItems){
+        add(obj);
+        obj.todoItems.push(item);
+        console.log("added: "+item);
+    } else {
+        obj.todoItems = [];
+        console.log("no array found. created array!");
+    }
+
+    jsonfile.writeFile(file, obj, (err) => {
     console.log(err)
-  });
+    });
+
+    });
+} else if (commandType === "show"){
+    jsonfile.readFile(file, (err, obj) => {
+    console.log(obj.todoItems);
 });
+} else if (commandType === "done"){
+    jsonfile.readFile(file, (err, obj) => {
+        let doneItemIndex = process.argv[3] - 1;
+        console.log("doneitemindex: "+doneItemIndex)
+
+        let oldItem = obj.todoItems[doneItemIndex]
+        console.log('oldItem : '+oldItem)
+
+        let newItem = oldItem.replace("[ ]", ["[x]"])
+        console.log('newitem : '+newItem)
+
+        // oldItem = newItem;
+
+        obj.todoItems[doneItemIndex] = newItem;
+
+    jsonfile.writeFile(file, obj, (err) => {
+    console.log(err)
+    });
+    });
+
+}
+
+//item: list number, taskstatus, task
+//goal, add and show
+// 1. [ ] - eat bak kut teh
+// 2. [ ] - go shopping
+// 3. [ ] - feed dog
+// 4. [ ] - swim practice
+// 5. [ ] - code app
+// 6. [ ] - meet gabriel
+
+
+// original
+// jsonfile.readFile(file, (err, obj) => {
+
+
+//   obj["helloworld"] = "yo";
+//   console.log(obj);
+//   jsonfile.writeFile(file, obj, (err) => {
+//     console.log(err)
+//   });
+// });
+
+
+
+
+    // if(obj.todoItems){
+    //     add(obj);
+
+    //     obj.todoItems.push(item);
+    //     console.log("added: "+item);
+    // } else {
+    //     obj.todoItems = [];
+    // }
