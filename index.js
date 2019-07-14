91597df2aaa892526cccec40c17d2e180e4ea431
wilfredloh <lohwilfred19@gmail.com>
2019-07-14 15:51:41 +0800

    added ASCII and some user validation

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 console.log("node.js running!");
+////////////////////////////////////////////////////////////////
+//                      GLOBAL VARIABLES                     //
+////////////////////////////////////////////////////////////////
 
-//           GLOBAL VARIABLES
 const jsonfile = require('jsonfile');
+const figlet = require('figlet');
 const file = 'data.json'
 
 let input1 = process.argv[2];
 let input2 = process.argv[3];
 let parsedInt2 = parseInt(input2);
-let date = new Date();
+let date = new Date;
+let fixedDate = `${date.getDate()}.${date.getMonth()+1}.${date.getFullYear()} at ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 
-//          SHOWING / ADDING / CHECKING / DELETING - TODOS
+////////////////////////////////////////////////////////////////
+//      SHOWING / ADDING / CHECKING / DELETING - TODOS        //
+////////////////////////////////////////////////////////////////
+
 let showTodos = (obj) => {
 
     let todoCheck = obj["todoCheck"];
@@ -34,26 +41,30 @@ let addTodos = (obj) => {
     if (input2) {
         obj["userInput"].push(input2);
         obj["todoCheck"].push(". [ ] - ");
-        obj["createdOn"].push(`--- created on ${date}`);
+        obj["createdOn"].push(`--- created on ${fixedDate}`);
         showTodos(obj);
     } else {
         console.log('Please add an input!');
     }
-
 }
 
 let checkTodo = (obj) => {
-    // WHY CAN'T I PLACE IT IN HERE?
+    // WHY CAN'T I SET THE VARIABLE BELOW?
     // let selectedTodo = obj["todoCheck"][parsedInt2-1];
-    let todoCheck = obj["todoCheck"];
-    if (todoCheck[parsedInt2-1].includes('x')) {
-        todoCheck[parsedInt2-1] = '. [ ] - ';
-    } else {
-        todoCheck[parsedInt2-1] = '. [x] - ';
-    }
+    // console.log('selectedTodo: ' + selectedTodo);
 
-    obj["createdOn"][parsedInt2-1] = `--- updated on ${date}`
-    showTodos(obj);
+    let todoCheck = obj["todoCheck"];
+    if (parsedInt2 > todoCheck.length || parsedInt2 <= 0 || isNaN(parsedInt2)){
+        console.log('Type a valid number!');
+    } else {
+        if (todoCheck[parsedInt2-1].includes('x')) {
+            todoCheck[parsedInt2-1] = '. [ ] - ';
+        } else {
+            todoCheck[parsedInt2-1] = '. [x] - ';
+        }
+        obj["createdOn"][parsedInt2-1] = `--- updated on ${fixedDate}`
+        showTodos(obj);
+    }
 }
 
 let deleteTodo = (obj) => {
@@ -62,36 +73,60 @@ let deleteTodo = (obj) => {
     let userInput = obj["userInput"];
     let createdOn = obj["createdOn"];
 
-    if (input2 === 'all') {
-        obj["todoCheck"] = [];
-        obj["userInput"] = [];
-        obj["createdOn"] = [];
-        console.log('No todos left!');
+    if (parsedInt2 > userInput.length || parsedInt2 <= 0 || isNaN(parsedInt2)){
+        console.log('Type a valid number!');
     } else {
-        todoCheck.splice((parsedInt2-1), 1);
-        userInput.splice((parsedInt2-1), 1);
-        createdOn.splice((parsedInt2-1), 1);
-        showTodos(obj);
+        if (input2 === 'all') {
+            obj["todoCheck"] = [];
+            obj["userInput"] = [];
+            obj["createdOn"] = [];
+            console.log('Deleted all todos!');
+        } else {
+            todoCheck.splice((parsedInt2-1), 1);
+            userInput.splice((parsedInt2-1), 1);
+            createdOn.splice((parsedInt2-1), 1);
+            showTodos(obj);
+        }
     }
 }
 
 let showOptions = () => {
-    console.log(`What would you like to do? \n\n1. show : shows all todos \n2. add [new todo] : adds a todo \n3. check [todo #] : checks/unchecks a todo \n4. delete [todo #] : deletes selected todo`);
+    console.log(`What would you like to do? \n\n1. show : shows all todos \n2. add [new todo] : adds a todo \n3. check [todo #] : checks/unchecks a todo \n4. delete [todo #] : deletes selected todo\n\n* fonts : check available figlet fonts`);
 }
 
 jsonfile.readFile(file, (err, obj) => {
 
-    obj["todoItems"] = [];
-    if (input1 === 'show') {
-        showTodos(obj);
-    } else if (input1 === 'add') {
-        addTodos(obj);
-    } else if (input1 === 'check') {
-        checkTodo(obj);
-    } else if (input1 === 'delete') {
-        deleteTodo(obj);
+    if (err) {
+        console.log(err);
     } else {
-        showOptions();
+        obj["todoItems"] = [];
+        switch(input1)
+        {
+            case 'show':
+                showFig('Current Todos');
+                showTodos(obj);
+                break;
+            case 'add':
+                showFig('Add');
+                addTodos(obj);
+                break;
+            case 'check':
+                showFig('Check');
+                checkTodo(obj);
+                break;
+            case 'delete':
+                showFig('Delete');
+                deleteTodo(obj);
+                break;
+            case 'fonts':
+                showFig('Fonts');
+                checkFigFonts();
+                break;
+            default:
+                showFig('The Todolist!');
+                showOptions();
+                break;
+        }
     }
 
     jsonfile.writeFile(file, obj, (err) => {
@@ -101,11 +136,34 @@ jsonfile.readFile(file, (err, obj) => {
     });
 });
 
-console.log(`
-  __             .___       .__  .__          __
-_/  |_  ____   __| _/____   |  | |__| _______/  |_
-    __ /  _   / __ |/  _    |  | |  |/  ___/    __
- |  | (  <_> ) /_/ (  <_> ) |  |_|  | ___    |  |
- |__|   ____/ ____ | ____/  |____/__/____>   |__|
+////////////////////////////////////////////////////////////////
+//                       FIGLET SECTION                       //
+////////////////////////////////////////////////////////////////
 
- `)
+// THIS IS USING SYNC FONT FUNCTIONS
+
+let showFig = (feature, font = generateRandomFont()) => {
+    console.log(figlet.textSync( feature , {
+        font: font,
+        horizontalLayout: 'default',
+        verticalLayout: 'default'
+    }));
+}
+
+let generateRandomFont = () => {
+    let randomNum = Math.floor(Math.random()*287);
+    let font = figlet.fontsSync()[randomNum];
+    return font;
+}
+
+// THIS IS USING ASYNC FONT FUNCTION
+let checkFigFonts = () => {
+    figlet.fonts(function(err, fonts) {
+        if (err) {
+            console.log('something went wrong...');
+            console.log(err);
+            return;
+        }
+        console.dir(fonts.length);
+    });
+}
