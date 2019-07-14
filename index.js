6acd472300093aa615d95c8712dab0726ebec165
MalcolmLow83 <malcomlow@gmail.com>
2019-07-14 21:59:19 +0800

    completion of delete and insert logic

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ var input3 = process.argv[3];
 console.log("input3: "+ input3);
 // console.log("typeof input3: "+ typeof input3);
 
+var date = new Date();
+// console.log("date: " + date);
+// console.log("typeof date: " + typeof date);
+
 // ==============================================
 
 // when typed "create", create itemToDo array
@@ -56,7 +60,9 @@ else if (input2 === "add") {
             obj.toDoItems.push({
                 "itemNumber" : (obj.toDoItems.length + 1).toString(),
                 "itemStatus" : "[ ]",
-                "itemString" : input3
+                "itemString" : input3,
+                "action" : "created",
+                "actionDate" : date
             })
 
             jsonfile.writeFile(file,obj, (err) => {
@@ -76,13 +82,14 @@ else if (input2 === "add") {
 
 // when typed "show", show all items added previously
 else if (input2 === "show") {
+    console.log("show operation");
     jsonfile.readFile(file, (err, obj) => {
         if (err) {
             console.log(err)
         }
         else {
             for (let i = 0; i < obj.toDoItems.length; i++) {
-                console.log(obj.toDoItems[i].itemNumber + ". " + obj.toDoItems[i].itemStatus + " - " + obj.toDoItems[i].itemString);
+                console.log(obj.toDoItems[i].itemNumber + ". " + obj.toDoItems[i].itemStatus + " - " + obj.toDoItems[i].itemString + " , " + obj.toDoItems[i].action + " at " +obj.toDoItems[i].actionDate);
             }
         }
     })
@@ -92,6 +99,7 @@ else if (input2 === "show") {
 
 // when type done and "#", change the status from [ ] to [x]
 else if (input2 === "done") {
+    console.log("done operation");
     jsonfile.readFile(file, (err, obj) => {
         if (err) {
             console.log(err)
@@ -100,6 +108,8 @@ else if (input2 === "done") {
             for (let i = 0; i < obj.toDoItems.length; i++) {
                 if (obj.toDoItems[i].itemNumber === input3) {
                     obj.toDoItems[i].itemStatus = "[x]";
+                    obj.toDoItems[i].action = "updated";
+                    obj.toDoItems[i].actionDate = date;
 
                     jsonfile.writeFile(file, obj, (err) => {
                         if (err) {
@@ -117,15 +127,71 @@ else if (input2 === "done") {
 
 }
 
-
-
-;
-
 // ==============================================
 
+// when type delete and "#", change the status and string to null
+else if (input2 === "delete") {
+    console.log("delete operation");
+    jsonfile.readFile(file, (err, obj) => {
+        if (err) {
+            console.log(err)
+        }
+        else {
+            for (let i = 0; i < obj.toDoItems.length; i++){
+                if (obj.toDoItems[i].itemNumber === input3) {
+                    obj.toDoItems[i].itemStatus = null;
+                    obj.toDoItems[i].itemString = null;
+                    obj.toDoItems[i].action = "deleted";
+                    obj.toDoItems[i].actionDate = date;
 
+                    jsonfile.writeFile(file, obj, (err) => {
+                        if (err) {
+                            console.log(err)
+                        }
+                        else {
+                            console.log("delete " + input3 + " completed")
+                        }
+                    })
+                }
+            }
+        }
+    })
+}
 
+// ==============================================
+else if (input2 === "insert") {
+    console.log("insert operation");
+    jsonfile.readFile(file, (err, obj) => {
+        if (err) {
+            console.log(err)
+        }
+        else {
+            for (i = 0; i < obj.toDoItems.length; i++ ) {
+                if (obj.toDoItems[i].itemStatus !== null){
+                    console.log("no deleted item, use add instead");
+                }
+                else if (obj.toDoItems[i].itemStatus === null) {
+                    obj.toDoItems[i].itemStatus = "[ ]";
+                    obj.toDoItems[i].itemString = input3;
+                    obj.toDoItems[i].action = "inserted";
+                    obj.toDoItems[i].actionDate = date;
 
+                    jsonfile.writeFile(file, obj, (err) => {
+                        if (err) {
+                            console.log(err)
+                        }
+                        else {
+                            console.log("insert " + input3 + " completed")
+                        }
+                    })
+                }
+
+            }
+        }
+    })
+}
+
+;
 
 // ==============================================
 //testing read logic
