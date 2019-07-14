810bc03087e44154a4e32401c0c7bbc65eecc13e
MalcolmLow83 <malcomlow@gmail.com>
2019-07-14 17:21:18 +0800

    completion of "done" logic

--- index.js
+++ index.js
@@ -5,22 +5,21 @@ const jsonfile = require('jsonfile');
 
 const file = 'data.json';
 
-
 //user input add
 var input2 = process.argv[2];
 console.log("input2: "+ input2);
+// console.log("typeof input2: "+ typeof input2);
 
 //user input toDo
 var input3 = process.argv[3];
 console.log("input3: "+ input3);
-
-// var indexNo;
-// console.log("indexNo: " + indexNo);
+// console.log("typeof input3: "+ typeof input3);
 
 // ==============================================
 
 // when typed "create", create itemToDo array
 if (input2 === "create"){
+    console.log("create operation");
     jsonfile.readFile(file, (err, obj) => {
         if (err) {
             console.log(err);
@@ -46,6 +45,7 @@ if (input2 === "create"){
 
 // when typed "add", push item to the array
 else if (input2 === "add") {
+    console.log("add operation");
     jsonfile.readFile(file, (err, obj) => {
         if (err) {
             console.log(err)
@@ -54,7 +54,7 @@ else if (input2 === "add") {
 
             // create number, status and string keys
             obj.toDoItems.push({
-                "itemNumber" : obj.toDoItems.length + 1,
+                "itemNumber" : (obj.toDoItems.length + 1).toString(),
                 "itemStatus" : "[ ]",
                 "itemString" : input3
             })
@@ -64,7 +64,7 @@ else if (input2 === "add") {
                     console.log(err)
                 }
                 else {
-                    console.log("add " + input3 + " sucessful");
+                    console.log("add " + input3 + " completed");
                 }
             })
 
@@ -88,6 +88,37 @@ else if (input2 === "show") {
     })
 }
 
+// ==============================================
+
+// when type done and "#", change the status from [ ] to [x]
+else if (input2 === "done") {
+    jsonfile.readFile(file, (err, obj) => {
+        if (err) {
+            console.log(err)
+        }
+        else {
+            for (let i = 0; i < obj.toDoItems.length; i++) {
+                if (obj.toDoItems[i].itemNumber === input3) {
+                    obj.toDoItems[i].itemStatus = "[x]";
+
+                    jsonfile.writeFile(file, obj, (err) => {
+                        if (err) {
+                            console.log(err)
+                        }
+                        else {
+                            console.log("done " + input3 + " completed")
+                        }
+                    })
+                }
+            }
+
+        }
+    })
+
+}
+
+
+
 ;
 
 // ==============================================
@@ -109,7 +140,7 @@ if (input2 === "read") {
             console.log("obj.toDoItems[0]: " + obj.toDoItems[0]);
 
             console.log("obj.toDoItems[0].itemsNumber: " + obj.toDoItems[0].itemNumber);
-            console.log("tyoeof: " + typeof obj.toDoItems[0].itemNumber);
+            console.log("typeof: " + typeof obj.toDoItems[0].itemNumber);
 
             console.log("obj.toDoItems[0].status: " + obj.toDoItems[0].status);
             console.log("obj.toDoItems[0].taskString: " + obj.toDoItems[0].taskString);
