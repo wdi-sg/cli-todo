c464d9f758ee14991388f47efe41059692f2847e
Janet <cashhpvn@yahoo.com>
2019-07-14 08:50:28 +0800

    done the basic

--- index.js
+++ index.js
@@ -1,19 +1,66 @@
-console.log("works!!", process.argv[2]);
-
-var commandType = process.argv[2];
-
-console.log("Your command was: "+commandType);
-
 const jsonfile = require('jsonfile');
+var commandType = process.argv[2];
+var item = process.argv[3];
+// console.log(commandType);
+const file = 'data.json';
 
-const file = 'data.json'
+var storeData= function(item){
+	//get the data
+		jsonfile.readFile(file,(err, arr)=>{
+			if (err){
+				console.log("There's an error.");
+				}
+	//change the data			
+			arr.push({todoItems:item,done:false,});//must have comma after "false" or or console log return undefined.
 
-jsonfile.readFile(file, (err, obj) => {
+	//write to data.json
 
-  console.log(obj);
-  obj["helloworld"] = "monkey";
+			jsonfile.writeFile(file,arr, (err)=>{
+				if(err){console.log("Hmm");}
+				});
+			});
+		}
 
-  jsonfile.writeFile(file, obj, (err) => {
-    console.log(err)
-  });
-});
+// storeData(item);
+var showData = function(){
+	//get the data
+	jsonfile.readFile(file,(err, arr)=>{
+		if (err){
+			console.log("There's an error.");
+		} else if( arr.length > 0){
+			//show the data
+			//use some formating for nicer looking text
+
+			arr.forEach(function(todoItems,index){
+                console.log(index+1+"."," ["+(todoItems.done ? "X" : " ")+"] ",todoItems.todoItems);
+			});
+		}
+	});
+}
+
+var done =function(){
+	var item = process.argv[3];
+	//get the data
+	jsonfile.readFile(file,(err, arr)=>{
+		if (err){
+			console.log("There's an error.");
+		}
+		
+	arr[item -1].done = !arr[item].done;//toggle the value : true and false
+
+	jsonfile.writeFile(file,arr, (err)=>{
+		if(err){console.log("Hmm");}
+		});
+	});	
+}
+switch(commandType){
+	case "add":
+			storeData(item);
+			break;
+	case "show":
+			showData();
+			break;
+	case "done":
+			done();
+			break;
+}
