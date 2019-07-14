56e4e25639c79681fb6abf8d2460a79dcc89ab71
aliciawongg <w.alicia@gmail.com>
2019-07-15 00:31:31 +0800

    add functions to create task list, add tasks and show tasks

--- index.js
+++ index.js
@@ -1,19 +1,42 @@
-console.log("works!!", process.argv[2]);
-
-var commandType = process.argv[2];
-
-console.log("Your command was: "+commandType);
+console.log("your command: " + process.argv[2]);
 
 const jsonfile = require('jsonfile');
-
 const file = 'data.json'
 
+let commandType = process.argv[2];
+let input1 = process.argv[3];
+let task;
+
+//create list and add 1st task
+var createList = function(task, obj) {
+    obj['todo'] = [];
+    obj['todo'].push(task);
+};
+
+//add 2nd task onwards
+var addTask = function(task, obj) {
+    obj['todo'].push(task);
+};
+
+//show task list
+var showList = function(obj) {
+    console.log(obj);
+};
+
 jsonfile.readFile(file, (err, obj) => {
+    if(commandType === 'add' && Object.keys(obj).length === 0) {
+        createList(input1, obj);
+        console.log(obj);
+    }
+    else if (commandType === 'add' && Object.keys(obj).length !== 0) {
+       addTask(input1, obj);
+       console.log(obj);
+    }
+    else if (commandType === 'show') {
+        showList(obj);
+    }
 
-  console.log(obj);
-  obj["helloworld"] = "monkey";
-
-  jsonfile.writeFile(file, obj, (err) => {
-    console.log(err)
-  });
+    jsonfile.writeFile(file, obj, (err) => {
+        console.log(err)
+        });
 });
