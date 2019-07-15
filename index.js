65ab52930167d6dd682ac0bc1ab8b5a26a93f8d6
w0bbbles <wobblebeat@gmail.com>
2019-07-15 16:47:48 +0800

    Final for now

--- index.js
+++ index.js
@@ -1,19 +1,63 @@
-console.log("works!!", process.argv[2]);
+// console.log("works!!", process.argv[2]);
 
-var commandType = process.argv[2];
+// var commandType = process.argv[2];
 
-console.log("Your command was: "+commandType);
+// console.log("Your command was: "+commandType);
+
+// const jsonfile = require('jsonfile');
+
+// const file = 'data.json'
+
+// jsonfile.readFile(file, (err, obj) => {
+
+//   console.log(obj);
+//   obj["helloworld"] = "monkey";
+
+//   jsonfile.writeFile(file, obj, (err) => {
+//     console.log(err)
+//   });
+// });
+console.log("jello")
 
 const jsonfile = require('jsonfile');
-
 const file = 'data.json'
 
-jsonfile.readFile(file, (err, obj) => {
+var commandType = process.argv[2];
+var input1 = process.argv[3];
 
-  console.log(obj);
-  obj["helloworld"] = "monkey";
+var addItem = function(newItem) {
+    jsonfile.readFile(file, (err, obj) => {
+    if (err) {
+            console.log(err)
+        } else {
+            let created_at = new Date();
 
-  jsonfile.writeFile(file, obj, (err) => {
-    console.log(err)
-  });
-});
+            obj.todoItems.push(`${obj.counter}. [ ] - ${newItem}\t\tCreated At: ${created_at}`);
+            obj.counter++;
+
+            jsonfile.writeFile(file, obj, (err) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log("Writing Done");
+                }
+
+            });
+        }
+
+    });
+};
+
+const showToDoList = function() {
+    jsonfile.readFile(file, (err, obj) => {
+        if (err) {
+            console.log(err);
+        } else {
+            let str = "";
+            for (let i = 0; i < obj.todoItems.length; i++) {
+                str += obj.todoItems[i] + "\n";
+            }
+            console.log(str);
+        }
+    })
+};
