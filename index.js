5a0fe43ab1fb03db5c9f6b6cbaa53cb08284bd68
sowyuen <sowyuenl@gmail.com>
2019-07-15 09:14:11 +0800

    done

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
-console.log("works!!", process.argv[2]);
-
+console.log("works!!");
 var commandType = process.argv[2];
-
-console.log("Your command was: "+commandType);
+var userInput = process.argv[3];
+console.log("Your command was: " + commandType);
 
 const jsonfile = require('jsonfile');
 
 const file = 'data.json'
 
+// to add to-do-list
 jsonfile.readFile(file, (err, obj) => {
+    if(process.argv[2] === 'add'){
+        obj["toDoList"] =[];
+        console.log(`To Do List added!`);
+    }
+    else if(process.argv[2] === "show"){
+        for(var j=1;j<obj.toDoList.length;j++){
+            console.log(`To Do List:\n ${j}. [ ]- ${obj.toDoList[j]}`);
+        }
+    }
 
-  console.log(obj);
-  obj["helloworld"] = "monkey";
+    for(var i=3; i<process.argv.length; i++){
+        obj.toDoList.push(process.argv[i]);
+    }
 
-  jsonfile.writeFile(file, obj, (err) => {
-    console.log(err)
-  });
+    jsonfile.writeFile(file, obj, (err) => {
+        console.log(err)
+    });
 });
