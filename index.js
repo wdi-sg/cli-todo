6a986573c2939c40919a5a1d198f69dae2475d0d
aliciawongg <w.alicia@gmail.com>
2019-08-18 19:15:05 +0800

    amend codes. show, add task and mark done can work now.

--- index.js
+++ index.js
@@ -5,38 +5,49 @@ const file = 'data.json'
 
 let commandType = process.argv[2];
 let input1 = process.argv[3];
-let task;
+let task = {status: '. [ ] -', description: input1};
+let i;
 
-//create list and add 1st task
-var createList = function(task, obj) {
-    obj['todo'] = [];
-    obj['todo'].push(task);
-};
-
-//add 2nd task onwards
+//add task onwards
 var addTask = function(task, obj) {
     obj['todo'].push(task);
+    i = obj['todo'].indexOf(task);
+    console.log( (i+1) + obj['todo'][i].status + ' ' + obj['todo'][i].description);
 };
 
 //show task list
 var showList = function(obj) {
-    console.log(obj);
+    taskArr = obj['todo'];
+    //console.log(taskArr);
+    for (i=0; i<taskArr.length; i++) {
+        console.log( (i+1) + obj['todo'][i].status + ' ' + obj['todo'][i].description);
+    }
+};
+
+var markDone = function(task, obj) {
+    j = input1 - 1;
+    taskArr = obj['todo'];
+    //console.log(taskArr);
+    for (i=0; i<taskArr.length; i++) {
+        if (i === j) {
+            obj['todo'][i].status = '. [X] -';
+        }
+        console.log( (i+1) + obj['todo'][i].status + ' ' + obj['todo'][i].description);
+    }
 };
 
 jsonfile.readFile(file, (err, obj) => {
-    if(commandType === 'add' && Object.keys(obj).length === 0) {
-        createList(input1, obj);
-        console.log(obj);
-    }
-    else if (commandType === 'add' && Object.keys(obj).length !== 0) {
-       addTask(input1, obj);
-       console.log(obj);
+    if (commandType === 'add') {
+       addTask(task, obj);
     }
     else if (commandType === 'show') {
         showList(obj);
     }
+    else if (commandType === 'done') {
+        markDone(task,obj);
 
+    }
     jsonfile.writeFile(file, obj, (err) => {
-        console.log(err)
+        //console.log(err)
         });
 });
