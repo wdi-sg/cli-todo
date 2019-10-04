c8ce0814e6e055d2f9f6662e9f0f74a408b23982
lhjd <hongjinlau@gmail.com>
2019-10-04 23:52:45 +0800

    Log date and time created

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-console.log("---To Do List---");
+const jsonfile = require('jsonfile');
+const moment = require('moment');
 
 const command = process.argv[2];
 const newTask = process.argv[3];
-// console.log("Your command was: "+input);
-
-const jsonfile = require('jsonfile');
 
 const file = 'data.json';
 
@@ -12,23 +10,30 @@ const listItem = (item) => {
     return `${item.index}. [${item.done ? 'X' : ' ' }] - ${item.task} - created: ${item.created} - updated: ${item.updated}`;
 }
 
+console.log("---To Do List---");
 jsonfile.readFile(file, (err, obj) => {
 
     switch (command) {
         case "add":
             {
-                if(!newTask) {
+                if (!newTask) {
                     console.log("Please specify new task to be added.");
                     return;
                 }
 
-                let lastIndex = obj.toDoItems[obj.toDoItems.length-1].index;
+                let lastIndex;
+
+                if (!obj.toDoItems.length) {
+                    lastIndex = 0;
+                } else {
+                    lastIndex = obj.toDoItems[obj.toDoItems.length - 1].index;
+                }
 
                 let newItem = {
                     "index": lastIndex + 1,
                     "done": false,
                     "task": newTask,
-                    "created": "04 Oct 2019",
+                    "created": moment().format("LLLL"),
                     "updated": "04 Oct 2019"
                 };
 
@@ -43,25 +48,22 @@ jsonfile.readFile(file, (err, obj) => {
                 });
             }
             break;
-        case "list":
+        case "show":
             {
-                // console.log("listing items");
                 obj.toDoItems.forEach(item => console.log(listItem(item)));
                 break;
             }
         case "help":
             {
-                console.log("show help");
+                console.log("add - add new task");
+                console.log("show - show tasks");
                 break;
             }
         default:
             {
-                console.log("show default help");
+                console.log("add - add new task");
+                console.log("show - show tasks");
                 break;
             }
     }
-
-    // console.log(obj);
-    // obj["helloworld"] = "monkey";
-
 });
