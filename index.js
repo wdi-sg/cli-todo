b178ee48047433b847280704bfa63acdb3b360c4
lhjd <hongjinlau@gmail.com>
2019-10-05 00:29:46 +0800

    Add delete function

--- index.js
+++ index.js
@@ -17,35 +17,36 @@ jsonfile.readFile(file, (err, obj) => {
         case "add":
             {
                 if (!input) {
-                    console.log("Please specify new task to be added.");
+                    console.log("*** Please specify new task to be added. ***");
                     return;
-                }
-
-                let lastIndex;
-
-                if (!obj.toDoItems.length) {
-                    lastIndex = 0;
                 } else {
-                    lastIndex = obj.toDoItems[obj.toDoItems.length - 1].index;
-                }
 
-                let newItem = {
-                    "index": lastIndex + 1,
-                    "done": false,
-                    "task": input,
-                    "created": moment().format("LLLL"),
-                    "updated": "-"
-                };
+                    let lastIndex;
 
-                obj.toDoItems.push(newItem);
+                    if (!obj.toDoItems.length) {
+                        lastIndex = 0;
+                    } else {
+                        lastIndex = obj.toDoItems[obj.toDoItems.length - 1].index;
+                    }
 
-                jsonfile.writeFile(file, obj, (err) => {
-                    if (err) {
-                        console.log("Error writing file");
-                        console.log(err);
+                    let newItem = {
+                        "index": lastIndex + 1,
+                        "done": false,
+                        "task": input,
+                        "created": moment().format("LLLL"),
+                        "updated": "-"
                     };
-                    obj.toDoItems.forEach(item => console.log(listItem(item)));
-                });
+
+                    obj.toDoItems.push(newItem);
+
+                    jsonfile.writeFile(file, obj, (err) => {
+                        if (err) {
+                            console.log("*** Error writing file. ***");
+                            console.log(err);
+                        };
+                        obj.toDoItems.forEach(item => console.log(listItem(item)));
+                    });
+                }
             }
             break;
         case "show":
@@ -56,19 +57,54 @@ jsonfile.readFile(file, (err, obj) => {
         case "done":
             {
                 if (!obj.toDoItems.length) {
-                    console.log("Please add a todo item first.");
+                    console.log("*** To do list is empty, please add a task ***");
                     return;
                 } else {
-                let doneItem = obj.toDoItems[input - 1];
+                    if (!input) {
+                        console.log("*** Please specify the task to be marked done. ***");
+                        obj.toDoItems.forEach(item => console.log(listItem(item)));
+                        return;
+                    }
 
-                doneItem.done = !doneItem.done;
-                doneItem.updated = moment().format("LLLL");
 
-                obj.toDoItems[input-1] = doneItem;
+                    let doneItem = obj.toDoItems[input - 1];
+
+                    doneItem.done = !doneItem.done;
+                    doneItem.updated = moment().format("LLLL");
+
+                    obj.toDoItems[input - 1] = doneItem;
 
                     jsonfile.writeFile(file, obj, (err) => {
                         if (err) {
                             console.log("Error writing file");
+                            console.log(err);
+                        };
+                        obj.toDoItems.forEach(item => console.log(listItem(item)));
+                    });
+                }
+                break;
+            }
+        case "delete":
+            {
+                if (!obj.toDoItems.length) {
+                    console.log("*** To do list is empty, please add a task. ***");
+                    return;
+                } else {
+                    if (!input) {
+                        console.log("*** Please specify the task to be deleted. ***");
+                        obj.toDoItems.forEach(item => console.log(listItem(item)));
+                        return;
+                    }
+
+                    let deleteItem = obj.toDoItems[input - 1];
+
+                    obj.toDoItems.splice(input - 1, 1);
+
+                    obj.toDoItems.forEach((item, index) => { item.index = index + 1 });
+
+                    jsonfile.writeFile(file, obj, (err) => {
+                        if (err) {
+                            console.log("*** Error writing file. ***");
                             console.log(err);
                         };
                         obj.toDoItems.forEach(item => console.log(listItem(item)));
