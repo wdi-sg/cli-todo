6ddde36c7a64244b9ce2eeb1d02c9fe4569263c1
SYNG <simyenng@gmail.com>
2019-10-05 00:46:40 +0800

    Complete show command

--- index.js
+++ index.js
@@ -10,21 +10,23 @@ const file = 'data.json'
 
 jsonfile.readFile(file, (err, obj) => {
 
-  //confirm current array
-  console.log(obj);
-
-  //if commandType is 'add'
   let todo = "";
+
   if (commandType === 'add') {
     for ( let i=3; i<process.argv.length; i++ ) {
         todo += process.argv[i] + " ";
     }
     console.log(todo);
     obj["todoItems"].push(todo);
+    jsonfile.writeFile(file, obj, (err) => {
+        console.log(err)
+    });
+    console.log(obj);
+
+  } else if (commandType === "show") {
+        for ( let j=0; j<obj["todoItems"].length; j++ ) {
+            console.log(`${j+1}. [ ] - ${obj["todoItems"][j]}`)
+        }
   }
 
-
-  jsonfile.writeFile(file, obj, (err) => {
-    console.log(err)
-  });
 });
