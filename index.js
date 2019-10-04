6029fa6c0c8b08b4528987ddb2bac5be470faf06
SYNG <simyenng@gmail.com>
2019-10-05 01:06:57 +0800

    Complete done command

--- index.js
+++ index.js
@@ -12,21 +12,35 @@ jsonfile.readFile(file, (err, obj) => {
 
   let todo = "";
 
-  if (commandType === 'add') {
-    for ( let i=3; i<process.argv.length; i++ ) {
-        todo += process.argv[i] + " ";
-    }
-    console.log(todo);
-    obj["todoItems"].push(todo);
-    jsonfile.writeFile(file, obj, (err) => {
-        console.log(err)
-    });
-    console.log(obj);
+  switch (commandType) {
 
-  } else if (commandType === "show") {
+    case 'add':
+        for ( let i=3; i<process.argv.length; i++ ) {
+            todo += process.argv[i] + " ";
+        }
+        console.log(todo);
+        obj["todoItems"].push(todo);
+        jsonfile.writeFile(file, obj, (err) => {
+            console.log(err)
+        });
+        console.log(obj);
+        break;
+
+    case 'show':
         for ( let j=0; j<obj["todoItems"].length; j++ ) {
             console.log(`${j+1}. [ ] - ${obj["todoItems"][j]}`)
         }
-  }
+        break;
 
+    case 'done':
+        for ( let k=0; k<obj["todoItems"].length; k++ ) {
+            if ( k === parseInt(process.argv[3])-1 ) {
+                console.log(`${k+1}. [x] - ${obj["todoItems"][k]}`)
+            } else {
+                console.log(`${k+1}. [ ] - ${obj["todoItems"][k]}`)
+            }
+        }
+        break;
+
+  }
 });
