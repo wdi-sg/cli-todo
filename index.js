488db38df752391dd1b6cf9bf288fc616523abce
Nathanael Tan <nathanael.tan92@gmail.com>
2019-10-05 11:58:17 +0800

    Done deliverable, and can remove items. with remove command

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-console.log("works!!", process.argv[2]);
 
-var commandType = process.argv[2];
+
+let commandType = process.argv[2];
+let toDoItem = process.argv[3];
+let parsedItem = parseInt(toDoItem)
+
+
 
 console.log("Your command was: "+commandType);
 
@@ -8,12 +12,56 @@ const jsonfile = require('jsonfile');
 
 const file = 'data.json'
 
+
+let addInput = (obj) => {
+  let toDoList = obj["toDoItems"];
+  let checkBox = "[]";
+
+  
+  toDoList.push(toDoItem)
+
+  for(let i = 0; i<toDoList.length; i++) {
+    if(i === toDoList.length - 1) {
+     
+      toDoList[i] = (i + 1) + ". " + checkBox + " - " +toDoItem;
+    }
+
+  }
+  return toDoList;
+}
+
+let removeItem= (obj) => {
+  let toDoList = obj["toDoItems"];
+ 
+  for(let i = 0; i<toDoList.length; i++) {
+    if(parsedItem === i) {
+      toDoList.splice(i - 1, 1)
+    }
+  }
+  return toDoList;
+}
+
+let checkItem = (obj) => {
+  
+}
+
+
 jsonfile.readFile(file, (err, obj) => {
+ 
+  
 
-  console.log(obj);
-  obj["helloworld"] = "monkey";
+if(commandType === "add") {
+  addInput(obj)
+} else if(commandType === "show") {
+  console.log(obj)
+} else if(commandType === "remove") {
+  removeItem(obj)
+}
 
+    
   jsonfile.writeFile(file, obj, (err) => {
     console.log(err)
   });
 });
+
+// console.log( process.argv);
