fccfc71841753f6019888463ebd0df53f62f21b5
“woofborn” <wolf.born@gmail.com>
2019-10-05 13:51:06 +0800

    Add, show and done functionalities online. The "done" items do not stay done if you show.

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 console.log("works!!", process.argv[2]);
 
-var commandType = process.argv[2];
+const commandType = process.argv[2];
+var listCheck;
+
+
+
 
 console.log("Your command was: "+commandType);
 
@@ -10,10 +14,34 @@ const file = 'data.json'
 
 jsonfile.readFile(file, (err, obj) => {
 
-  console.log(obj);
-  obj["helloworld"] = "monkey";
+    if (commandType === "add"){
+        const toDo = process.argv[3]
 
-  jsonfile.writeFile(file, obj, (err) => {
-    console.log(err)
-  });
+        obj["todoItems"].push(toDo)
+        let index = obj["todoItems"].indexOf(toDo)
+        console.log(`${index+1}. [ ] - ${toDo}`)
+    } else if (commandType === "show"){
+
+        for (i=1; i<=obj["todoItems"].length; i++){
+            console.log(`${i}. [ ] - ${obj["todoItems"][i-1]}`)
+        }
+    } else if (commandType === "done"){
+        const todoNum = process.argv[3]
+        let listCheck;
+
+        for (i=1; i<=obj["todoItems"].length;i++){
+
+             if (i === parseInt(todoNum)){
+                listCheck = "[x]"
+            } else {
+                listCheck = "[ ]"
+            }
+            console.log(`${i}. ${listCheck} - ${obj["todoItems"][i-1]}`)
+        }
+
+    }
+
+      jsonfile.writeFile(file, obj, (err) => {
+        console.log(err)
+      });
 });
