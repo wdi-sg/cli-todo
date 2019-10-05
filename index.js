3e4381436c72b7d8fd95a31a92b8aff8561dfec6
“woofborn” <wolf.born@gmail.com>
2019-10-05 16:55:28 +0800

    Created array of objects to store whether item complete or not (so "done" item remains checked off.)

--- index.js
+++ index.js
@@ -3,41 +3,52 @@ console.log("works!!", process.argv[2]);
 const commandType = process.argv[2];
 var listCheck;
 
-
-
-
 console.log("Your command was: "+commandType);
 
 const jsonfile = require('jsonfile');
 
 const file = 'data.json'
+const command = "commands"
+
+class todoItem {
+    constructor(complete, item){
+
+        this.complete = complete;
+        this.item = item
+    }
+}
+
+
 
 jsonfile.readFile(file, (err, obj) => {
 
     if (commandType === "add"){
-        const toDo = process.argv[3]
 
-        obj["todoItems"].push(toDo)
-        let index = obj["todoItems"].indexOf(toDo)
-        console.log(`${index+1}. [ ] - ${toDo}`)
+
+        newTodo = new todoItem (`[ ]`, process.argv[3])
+        obj["todoList"].push(newTodo)
+        newTodo.number = `${obj["todoList"].indexOf(newTodo)+1}`
+
+        console.log(`${newTodo["number"]}. ${newTodo["complete"]} - ${newTodo["item"]}`)
+        console.log(obj)
+        console.log(newTodo)
+
     } else if (commandType === "show"){
 
-        for (i=1; i<=obj["todoItems"].length; i++){
-            console.log(`${i}. [ ] - ${obj["todoItems"][i-1]}`)
+        for (i=0; i<obj["todoList"].length; i++){
+            console.log(`${obj["todoList"][i]["number"]}. ${obj["todoList"][i]["complete"]} - ${obj["todoList"][i]["item"]}`)
         }
     } else if (commandType === "done"){
         const todoNum = process.argv[3]
-        let listCheck;
-
-        for (i=1; i<=obj["todoItems"].length;i++){
-
-             if (i === parseInt(todoNum)){
-                listCheck = "[x]"
-            } else {
-                listCheck = "[ ]"
-            }
-            console.log(`${i}. ${listCheck} - ${obj["todoItems"][i-1]}`)
+        const index = todoNum - 1
+        obj["todoList"][index]["complete"] = "[x]"
+         for (i=0; i<obj["todoList"].length; i++){
+            console.log(`${obj["todoList"][i]["number"]}. ${obj["todoList"][i]["complete"]} - ${obj["todoList"][i]["item"]}`)
         }
+
+
+
+
 
     }
 
