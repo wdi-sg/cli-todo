42bbbc8082d532c46389304386b2c70f21d41765
tansiewling-hotmail <prudence_tan@hotmail.com>
2019-10-05 17:52:15 +0800

    Added mark as done but not in correct display format

--- todo.js
+++ todo.js
@@ -4,8 +4,11 @@ const jsonfile = require('jsonfile');
 // Create data.json file with empty object syntax
 const file = 'data.json';
 
-// Create constant variable to indicate display format
-const bulletPoint = ". [ ] - ";
+// Create constant variable to indicate if task is completed
+const taskIncomplete = ". [ ] - ";
+const taskComplete = ". [x] - ";
+
+// Create
 
 // Read the third argument from command line to add into todoItems array
 jsonfile.readFile(file, (err, obj) => {
@@ -24,12 +27,22 @@ jsonfile.readFile(file, (err, obj) => {
             // e.g. 1. [ ] - eat bak kut teh
         for(let i = 1; i < toDoObject.length; i++) {
 
-            console.log(i + bulletPoint + toDoObject[i]);
+            console.log(i + taskIncomplete + toDoObject[i]);
         }
+
+    } else if(process.argv[2] === "done") {
+
+        // Check the next input for the item to be marked as done
+        let taskNo = parseInt(process.argv[3]);
+
+        // Pass the taskNo to the array and mark as complete
+        console.log(taskNo + taskComplete + toDoObject[taskNo]);
+
+        console.log(toDoObject);
 
     }
 
     jsonfile.writeFile(file, obj, (err) => {
-        console.log(err)
+        console.log("Error: " + err);
     });
 });
