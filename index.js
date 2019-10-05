edd6f584ff4cc8c249d8e4b4f7d33df90cd3098a
SYNG <simyenng@gmail.com>
2019-10-05 15:16:24 +0800

    Updated commands to update file

--- index.js
+++ index.js
@@ -10,16 +10,19 @@ const file = 'data.json'
 
 jsonfile.readFile(file, (err, obj) => {
 
-  let todo = "";
-
   switch (commandType) {
 
     case 'add':
-        for ( let i=3; i<process.argv.length; i++ ) {
-            todo += process.argv[i] + " ";
-        }
-        console.log(todo);
+        //add brackets
+        let todo = "[ ] -";
         obj["todoItems"].push(todo);
+        //add item
+        let item = "";
+        for ( let i=3; i<process.argv.length; i++ ) {
+            item += " " + process.argv[i];
+        }
+        console.log(item);
+        obj["todoItems"].push(item);
         jsonfile.writeFile(file, obj, (err) => {
             console.log(err)
         });
@@ -27,17 +30,30 @@ jsonfile.readFile(file, (err, obj) => {
         break;
 
     case 'show':
-        for ( let j=0; j<obj["todoItems"].length; j++ ) {
-            console.log(`${j+1}. [ ] - ${obj["todoItems"][j]}`)
+        let listNumShow=1;
+        for ( let j=1; j<obj["todoItems"].length; j++ ) {
+            if ( !!(j % 2) ) {  //output only odd index
+                console.log(`${listNumShow}. ${obj["todoItems"][j-1]} ${obj["todoItems"][j]}`)
+                listNumShow++;
+            }
         }
         break;
 
     case 'done':
-        for ( let k=0; k<obj["todoItems"].length; k++ ) {
-            if ( k === parseInt(process.argv[3])-1 ) {
-                console.log(`${k+1}. [x] - ${obj["todoItems"][k]}`)
-            } else {
-                console.log(`${k+1}. [ ] - ${obj["todoItems"][k]}`)
+        let checkItem = parseInt(process.argv[3]) + (parseInt(process.argv[3]) - 2);
+        obj["todoItems"][checkItem] = "[x] -";
+
+        //call add to update array
+        jsonfile.writeFile(file, obj, (err) => {
+            console.log(err)
+        });
+
+        //call show to list again
+        let listNumDone=1;
+        for ( let k=1; k<obj["todoItems"].length; k++ ) {
+            if ( !!(k % 2) ) {  //output only odd index
+                console.log(`${listNumDone}. ${obj["todoItems"][k-1]} ${obj["todoItems"][k]}`)
+                listNumDone++;
             }
         }
         break;
