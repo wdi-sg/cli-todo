5594971df171a5f76ce8d9c8ee3fbe5a70b5501d
Nathanael Tan <nathanael.tan92@gmail.com>
2019-10-05 14:26:44 +0800

    Check box is done. And remove item bug fixed for last item

--- index.js
+++ index.js
@@ -1,66 +1,72 @@
-
-
 let commandType = process.argv[2];
 let toDoItem = process.argv[3];
-let parsedItem = parseInt(toDoItem)
+let parsedItem = parseInt(toDoItem);
 
+console.log("Your command was: " + commandType);
+// Global Variables
+const jsonfile = require("jsonfile");
+const file = "data.json";
+let checkBox = "[]";
 
+// Functions for adding removing, checking
 
-console.log("Your command was: "+commandType);
-
-const jsonfile = require('jsonfile');
-
-const file = 'data.json'
-
-
-let addInput = (obj) => {
+let addInput = obj => {
   let toDoList = obj["toDoItems"];
-  let checkBox = "[]";
 
-  
-  toDoList.push(toDoItem)
+  toDoList.push(toDoItem);
 
-  for(let i = 0; i<toDoList.length; i++) {
-    if(i === toDoList.length - 1) {
-     
-      toDoList[i] = (i + 1) + ". " + checkBox + " - " +toDoItem;
-    }
-
-  }
-  return toDoList;
-}
-
-let removeItem= (obj) => {
-  let toDoList = obj["toDoItems"];
- 
-  for(let i = 0; i<toDoList.length; i++) {
-    if(parsedItem === i) {
-      toDoList.splice(i - 1, 1)
+  for (let i = 0; i < toDoList.length; i++) {
+    if (i === toDoList.length - 1) {
+      toDoList[i] = i + 1 + ". " + checkBox + " - " + toDoItem;
     }
   }
   return toDoList;
-}
+};
 
-let checkItem = (obj) => {
+let removeItem = obj => {
+  let toDoList = obj["toDoItems"];
+
+  for (let i = 0; i < toDoList.length; i++) {
+    if (parsedItem === i) {
+      toDoList.splice(i - 1, 1);
+    } else if(parsedItem === toDoList.length){
+      toDoList.pop()
+    }
+  }
+  return toDoList;
+};
+
+// Check the box if the command is done, 
+let checkItem = obj => {
+  let toDoList = obj["toDoItems"];
+
+  for (let i = 0; i < toDoList.length; i++) {
+    if (parsedItem === i) {
+      console.log(toDoList.length)
+      toDoList[i - 1] = toDoList[i - 1].replace('[]', '[X]');
+    } else {
+      toDoList[toDoList.length - 1] = toDoList[toDoList.length - 1].replace('[]', '[X]');
+    }
   
-}
+  }
+  return toDoList;
+};
 
+// Read and Write Function
 
 jsonfile.readFile(file, (err, obj) => {
- 
-  
+  if (commandType === "add") {
+    addInput(obj);
+  } else if (commandType === "show") {
+    console.log(obj);
+  } else if (commandType === "remove") {
+    removeItem(obj);
+  } else if (commandType === "done") {
+    checkItem(obj);
+  }
 
-if(commandType === "add") {
-  addInput(obj)
-} else if(commandType === "show") {
-  console.log(obj)
-} else if(commandType === "remove") {
-  removeItem(obj)
-}
-
-    
-  jsonfile.writeFile(file, obj, (err) => {
-    console.log(err)
+  jsonfile.writeFile(file, obj, err => {
+    console.log(err);
   });
 });
 
