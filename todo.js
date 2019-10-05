a87d9d18780d6ee54249509a1f9dfb22c1e77650
kevinngth <kevin.ng.th@gmail.com>
2019-10-05 12:29:46 +0800

    added done toggle. done with further 2

--- todo.js
+++ todo.js
@@ -2,24 +2,45 @@ let commandType = process.argv[2];
 
 let listItem = process.argv[3];
 
-console.log(`Your command was: ${commandType} ${listItem}`);
+console.log(`Your command was: ${commandType} `);
 
 const jsonfile = require('jsonfile');
 
 const file = 'data.json'
 
+class Item {
+    constructor(name) {
+        this._name = name;
+        this._done = false;
+    }
+}
+
 jsonfile.readFile(file, (err, obj) => {
 
   switch (commandType) {
+// if command is to add
     case 'add':
-        obj['todoItems'].push(listItem);
+// push new object to array
+        obj['todoItems'].push(new Item(listItem));
         console.log('your wish is my command');
         break;
+// if command is to show
     case 'show':
+// iterate every item in the list
         for (let i=0; i<obj['todoItems'].length; i++) {
-            let formatOutput = `${i+1}. [ ] - ${obj["todoItems"][i]}`
+// destructure the object
+            let {_name, _done} = obj["todoItems"][i];
+// if else for checking if the list item is done
+            let doneOrNot = _done ? 'X': ' ';
+// format the output in a proper list form
+            let formatOutput = `${i+1}. [${doneOrNot}] - ${_name}`
             console.log(formatOutput);
         };
+        break;
+// if command is done
+    case 'done':
+        let selected = obj["todoItems"][parseInt(listItem)-1];
+        selected._done = !selected._done;
         break;
     default:
         console.log('invalid command');
