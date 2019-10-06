d2efaba5d6475557acb3f489d9b96234688575c2
jasminesis <jasmine-aips@hotmail.com>
2019-10-06 16:36:21 +0800

    can mark as done

--- index.js
+++ index.js
@@ -1,20 +1,51 @@
-console.log("works!!", process.argv[2]);
-
 var commandType = process.argv[2];
 
-console.log("Your command was: "+commandType);
+var arg = process.argv[3];
+
+console.log("Your command was: " + commandType);
 
 const jsonfile = require('jsonfile');
 
-const file = 'data.json'
+const file = 'data.json';
+
 
 jsonfile.readFile(file, (err, obj) => {
 
-  console.log(obj);
-  obj["helloworld"] = "monkey";
+    // clear obj
+    // obj["todoItems"] = [];
 
-  jsonfile.writeFile(file, obj, (err) => {
-    console.log(err)
-  });
+    // format:   1. [] - eat bah kut teh
+    if (commandType === "add") {
+        let count = obj["todoItems"].length
+        obj["todoItems"].push(`${count + 1}. [ ] - ${arg}`);
+        console.log(`You have added ${arg} to the to-do list!`)
+        jsonfile.writeFile(file, obj, (err) => {
+          if (err) {
+            console.log(err);
+            return;
+          }
+        });
+    } else if (commandType === "show") {
+        console.log(obj['todoItems'].join("\n"));
+    } else if (commandType === "done") {
+        let index = arg - 1;
+        console.log(obj["todoItems"][index])
+
+        let crossOut = obj["todoItems"].splice(index, 1);
+        console.log(`crossOut is ${crossOut}`);
+
+        let taskDone = crossOut[0].replace("[ ]", "[x]");
+        console.log(`taskDone is ${taskDone}`);
+
+        obj["todoItems"].splice(index, 0, taskDone);
+        console.log(obj["todoItems"]);
+        jsonfile.writeFile(file, obj, (err) => {
+          if (err) {
+            console.log(err);
+            return;
+          }
+        });
+
+
+    }
 });
-// trying out commits
