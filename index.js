3f6c7db7320b3be7f9a66d823b2aba1dacf5d795
Maria Dimitrijevic <maria.dimitrijevikj@gmail.com>
2019-10-06 14:55:04 +0800

    add item and track item id

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 console.log("works!!", process.argv[2]);
 
 var commandType = process.argv[2];
+let task = process.argv[3];
 
 console.log("Your command was: "+commandType);
 
@@ -11,9 +12,15 @@ const file = 'data.json'
 jsonfile.readFile(file, (err, obj) => {
 
   console.log(obj);
-  obj["helloworld"] = "monkey";
+  // obj["helloworld"] = "monkey";
+  add(obj, task);
 
   jsonfile.writeFile(file, obj, (err) => {
     console.log(err)
   });
 });
+
+add = (obj, task) => {
+  let id = obj.todoItems.length + 1;
+  obj.todoItems.push({ "id" : id, "task": task});
+};
