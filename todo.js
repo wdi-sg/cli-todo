97454a533dc09185dd1b26c6b632f61ed8325296
tansiewling-hotmail <prudence_tan@hotmail.com>
2019-10-06 23:04:57 +0800

    Added created_at and updated_at timestamp

--- todo.js
+++ todo.js
@@ -5,10 +5,8 @@ const jsonfile = require('jsonfile');
 const file = 'data.json';
 
 // Create constant variable to indicate if task is completed
-const taskIncomplete = ". [ ] - ";
-const taskComplete = ". [x] - ";
-
-// Create
+/*const taskIncomplete = ". [ ] - ";
+const taskComplete = ". [x] - ";*/
 
 // Read the third argument from command line to add into todoItems array
 jsonfile.readFile(file, (err, obj) => {
@@ -16,29 +14,52 @@ jsonfile.readFile(file, (err, obj) => {
     // Declare a variable to for obj.todoItems
     let toDoObject = obj.todoItems;
 
+    // Check the last item number
+    let currentLastItemNo = toDoObject.length;
+    console.log("Current last item no. is " + currentLastItemNo);
+
+    let nextItemNo = currentLastItemNo + 1;
+    console.log("Next item no. is " + nextItemNo);
+
+    let created_at = new Date();
+    let updated_at = new Date();
+
     if(process.argv[2] === "add") {
 
-        toDoObject.push(process.argv[3]);
-        console.log(process.argv[3] + " has been added to the list.");
+        let record = {
+            "taskNum": nextItemNo,
+            "taskDescription": process.argv[3],
+            "status": "[ ]",
+            "created_at": created_at,
+            "updated_at": updated_at
+        };
+
+        toDoObject.push(record);
+
+        console.log(record.taskNum + ". " + record.status + " - " + record.taskDescription);
 
     } else if(process.argv[2] === "show") {
 
         // Display list as per expected format
             // e.g. 1. [ ] - eat bak kut teh
-        for(let i = 1; i < toDoObject.length; i++) {
+        for(let i = 0; i < toDoObject.length; i++) {
 
-            console.log(i + taskIncomplete + toDoObject[i]);
+            console.log(toDoObject[i].taskNum + ". " + toDoObject[i].status + " - " + toDoObject[i].taskDescription);
         }
 
     } else if(process.argv[2] === "done") {
 
-        // Check the next input for the item to be marked as done
-        let taskNo = parseInt(process.argv[3]);
+        // Check the item number to be marked as done
+        let taskNo = parseInt(process.argv[3]) - 1;
 
-        // Pass the taskNo to the array and mark as complete
-        console.log(taskNo + taskComplete + toDoObject[taskNo]);
+        // Get the record
+        let record = toDoObject[taskNo];
 
-        console.log(toDoObject);
+        // Update the record status to complete
+        record.status = "[x]";
+        record.updated_at = updated_at;
+
+        console.log(record);
 
     }
 
