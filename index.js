5e2734d92bdedc7c526fe9f437e84544c234211b
DeeTrax <norris.wong@hotmail.com>
2019-10-07 10:47:27 +0800

    homework not completed

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-console.log("works!!", process.argv[2]);
+// console.log("works!!", process.argv[2]);
 
-var commandType = process.argv[2];
+// var commandType = process.argv[2];
 
-console.log("Your command was: "+commandType);
+// console.log("Your command was: "+commandType);
+
+var itemsType = process.argv[2];
+var value = process.argv[3];
+console.log("working!!")
+
+if (itemsType === "add") {
+	
+}
+let totalItems = itemsArray.length-1;
+let selectedItems = process.argv[itemNumber];
+let chosenItemNumber = function() {
+
+}
+console.log("appplee")
 
 const jsonfile = require('jsonfile');
 
@@ -11,9 +25,10 @@ const file = 'data.json'
 jsonfile.readFile(file, (err, obj) => {
 
   console.log(obj);
-  obj["helloworld"] = "monkey";
+  // obj["helloworld"] = "monkey";
 
   jsonfile.writeFile(file, obj, (err) => {
     console.log(err)
   });
 });
+console.log(file);
