45b9c127eb610aecb3eff0efa7af0b78ff192696
EugeneTan9 <tyx.eugene@gmail.com>
2019-10-07 11:28:48 +0800

    redoing in progress

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-console.log("works!!", process.argv[2]);
+// console.log("works!!", process.argv[2]);
 
-var commandType = process.argv[2];
+// var commandType = process.argv[2];
 
-console.log("Your command was: "+commandType);
+let value = process.argv[2];
+let item = process.argv[3];
+
+console.log("working");
 
 const jsonfile = require('jsonfile');
 
@@ -10,10 +13,16 @@ const file = 'data.json'
 
 jsonfile.readFile(file, (err, obj) => {
 
-  console.log(obj);
-  obj["helloworld"] = "monkey";
+  // console.log(obj);
+  // obj["helloworld"] = "monkey";
+
+
 
   jsonfile.writeFile(file, obj, (err) => {
     console.log(err)
+
+    if(value === add) {
+
+    }
   });
 });
