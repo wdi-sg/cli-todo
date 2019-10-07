89dad6684e0ed5076df927abd02f125fa96a66bb
Nathanael Tan <nathanael.tan92@gmail.com>
2019-10-07 08:36:17 +0800

    functionality: add, checkdone, remove, clear, date/time

--- index.js
+++ index.js
@@ -3,69 +3,81 @@ let toDoItem = process.argv[3];
 let parsedItem = parseInt(toDoItem);
 
 console.log("Your command was: " + commandType);
+
 // Global Variables
 const jsonfile = require("jsonfile");
-const dateFormat = require('dateFormat')
-let now = new Date()
+const dateFormat = require("dateFormat");
+let now = new Date();
 const file = "data.json";
 let checkBox = "[]";
 
 // Functions for adding removing, checking
 
+// Add a Todo Item
 let addInput = obj => {
   let toDoList = obj["toDoItems"];
 
   toDoList.push(toDoItem);
- 
-
 
   for (let i = 0; i < toDoList.length; i++) {
     if (i === toDoList.length - 1) {
-      toDoList[i] = i + 1 + ". " + checkBox + " - " + toDoItem + " |||| Created On:  " + dateFormat(now, "dd/mmm/yyyy, h:MM TT") ;
-      
+      toDoList[i] =
+        i +
+        1 +
+        ". " +
+        checkBox +
+        " - " +
+        toDoItem +
+        " |||| Created On:  " +
+        dateFormat(now, "dd/mmm/yyyy, h:MM TT");
     }
   }
   return toDoList;
 };
 
+// Permenantly removes an item
 let removeItem = obj => {
   let toDoList = obj["toDoItems"];
 
   for (let i = 0; i < toDoList.length; i++) {
     if (parsedItem === i) {
       toDoList.splice(i - 1, 1);
-    } else if(parsedItem === toDoList.length){
-      toDoList.pop()
+    } else if (parsedItem === toDoList.length) {
+      toDoList.pop();
     }
   }
   return toDoList;
 };
 
-// Check the box if the command is done, 
+// Check the box if the command is done,
 let checkItem = obj => {
   let toDoList = obj["toDoItems"];
 
   for (let i = 0; i < toDoList.length; i++) {
     if (parsedItem === i) {
-      console.log(toDoList.length)
-      toDoList[i - 1] = toDoList[i - 1].replace('[]', '[X]');
-      toDoList[i - 1] = toDoList[i - 1].replace('Created', 'Finished');
-      
-    } else if(parsedItem === toDoList.length){
-      toDoList[toDoList.length - 1] = toDoList[toDoList.length - 1].replace('Created', 'Finished');
-     
+      console.log(toDoList.length);
+      toDoList[i - 1] = toDoList[i - 1].replace("[]", "[X]");
+      toDoList[i - 1] = toDoList[i - 1].replace("Created", "Finished");
+    } else if (parsedItem === toDoList.length) {
+      toDoList[toDoList.length - 1] = toDoList[toDoList.length - 1].replace(
+        "Created",
+        "Finished"
+      );
+      toDoList[toDoList.length - 1] = toDoList[toDoList.length - 1].replace(
+        "[]",
+        "[X]"
+      );
     }
-  
   }
   return toDoList;
 };
-
-let clearList = (obj) => {
+// Clear the entire list
+let clearList = obj => {
   let toDoList = obj["toDoItems"];
   toDoList.length = 0;
 
-  return toDoList
-}
+  return toDoList;
+};
 
 // Read and Write Function
 
@@ -78,13 +90,11 @@ jsonfile.readFile(file, (err, obj) => {
     removeItem(obj);
   } else if (commandType === "done") {
     checkItem(obj);
-  } else if(commandType === "clear") {
-    clearList(obj)
+  } else if (commandType === "clear") {
+    clearList(obj);
   }
 
   jsonfile.writeFile(file, obj, err => {
     console.log(err);
   });
 });
-
-
