d34366c3a61ecf56886fcf03b83e357518617798
“kellylimmm” <kellylim.06@gmail.com>
2019-10-07 08:56:59 +0800

    homework

--- index.js
+++ index.js
@@ -1,19 +1,46 @@
-console.log("works!!", process.argv[2]);
-
 var commandType = process.argv[2];
 
-console.log("Your command was: "+commandType);
+console.log("Your command is: "+commandType);
+
 
 const jsonfile = require('jsonfile');
 
 const file = 'data.json'
 
 jsonfile.readFile(file, (err, obj) => {
+    if (commandType === "add"){
+        var commandText = process.argv[3];
+        console.log("Your command text is: " + commandText);
+        if (!obj["todoList"]){
+            obj["todoList"] = [];
+        };
 
-  console.log(obj);
-  obj["helloworld"] = "monkey";
+        obj["todoList"].push(commandText);
+        var objectList = obj["todoList"];
 
-  jsonfile.writeFile(file, obj, (err) => {
-    console.log(err)
-  });
+
+    } else if (commandType === "show"){
+        for (var i = 0; i < 6; i++){
+            var objectList = obj["todoList"];
+            console.log( (i + 1) + "." + "[ ]" + " - " + objectList[i]);
+        }
+    }
+
+    if (commandType === "done"){
+        for (var i = 0; i < 6; i++){
+            var objectList = obj["todoList"];
+            var commandText = process.argv[3];
+            if (i == commandText){
+               console.log( (i + 1) + "." + "[ x ]" + " - " +objectList[i]);
+            } else {
+                console.log( (i + 1) + "." + "[ ]" + " - " +objectList[i]);
+            }
+        }
+
+
+    }
+
+    jsonfile.writeFile(file, obj, (err) => {
+        console.log(err)
+    });
 });
