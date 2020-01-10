dd506b3ed8a1eda1924a8f8811ba89e86b437b04
Jess <jessicatanhc@gmail.com>
2020-01-10 17:41:19 +0800

    Modify display task

--- todo.js
+++ todo.js
@@ -2,12 +2,10 @@ const jsonfile = require('jsonfile');
 
 const file = 'data.json';
 var commandType = process.argv[2];
-var  task = process.argv[3];
+var task = process.argv[3];
 
 var addToList = (task)=>{
  jsonfile.readFile(file, (err, obj) => {
-
-  console.log(obj);
   var taskObj = {
     "task":task,
     "completeTask":"[ ]"
@@ -16,16 +14,21 @@ var addToList = (task)=>{
   jsonfile.writeFile(file, obj, (err) => {
     console.log(err);
   });
-   displayTask(obj["todoItems"]);
 });
 }
 
-var displayTask = (tasks) => {
+var show = () => {
+   jsonfile.readFile(file, (err, obj) => {
+    var tasks = obj["todoItems"];
     for(var i = 0; i <tasks.length; i++){
         console.log((i+1)+'. '+tasks[i]["completeTask"] +' - ' + tasks[i]["task"]);
     }
+});
 }
 
 if(commandType === "add" && task!== null){
     addToList(task);
+    show();
+}else if(commandType === "show"){
+    show();
 }
