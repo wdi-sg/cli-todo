f1bfbe3f271f2605a306a0b65edd9fd35f019044
Stuart Myers <stuart.myers@gmail.com>
2020-01-10 16:31:11 +0800

    Can add items to list

--- index.js
+++ index.js
@@ -1,19 +1,65 @@
 console.log("works!!", process.argv[2]);
 
-var commandType = process.argv[2];
+const commandType = process.argv[2];
+const commandArg = process.argv[3];
 
-console.log("Your command was: "+commandType);
+console.log("Your command was: " + commandType);
 
 const jsonfile = require('jsonfile');
 
 const file = 'data.json'
 
-jsonfile.readFile(file, (err, obj) => {
+var toDoItems = [];
 
-  console.log(obj);
-  obj["helloworld"] = "monkey";
+const whatCommand = () => {
+    switch (commandType) {
+        case 'add':
+            addToDoListItem();
+            break;
+        case 'show':
+            break;
+        case 'display':
+            break;
+        default:
+            console.log('Not a valid command');
+            break;
+    }
+}
 
-  jsonfile.writeFile(file, obj, (err) => {
-    console.log(err)
-  });
-});
+const onFileRead = (err, obj) => {
+
+    if (err) {
+        console.log('There is an error:');
+        console.log(err);
+        return;
+    }
+
+    if (!obj.toDoItems) {
+        obj.toDoItems = [];
+    }
+    toDoItems = obj.toDoItems;
+
+    whatCommand();
+    console.log('Items in list:');
+    listToDoListItems();
+
+    jsonfile.writeFile(file, obj, (err) => {
+        if (err) {
+          console.log('Error writing file:');
+          console.log(err);
+        }
+    });
+};
+
+const addToDoListItem = () => {
+    console.log('adding' + commandArg);
+    toDoItems.push(commandArg);
+}
+
+const listToDoListItems = () => {
+    for (let i = 0; i < toDoItems.length; i++) {
+        console.log(toDoItems[i]);
+    }
+}
+
+jsonfile.readFile(file, onFileRead);
