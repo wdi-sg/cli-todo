17b391a6cac9f61accdd6da27802a03d67154164
Robert Kolsek <kolsek.income@gmail.com>
2020-01-10 16:39:57 +0800

    MVP done

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-console.log("works!!", process.argv[2]);
-
-var commandType = process.argv[2];
-
-console.log("Your command was: "+commandType);
-
 const jsonfile = require('jsonfile');
 
 const file = 'data.json'
@@ -11,7 +5,19 @@ const file = 'data.json'
 jsonfile.readFile(file, (err, obj) => {
 
   console.log(obj);
-  obj["helloworld"] = "monkey";
+  if (!obj.todoItems){
+    obj.todoItems = []
+  }
+
+  if (process.argv[2] === "add"){
+    obj.todoItems.push(`${obj.todoItems.length+1}. [ ] - ${process.argv[3]}`)
+    console.log(obj.todoItems)
+  }
+  // if (process.argv[2] === "done"){
+  //   let element = obj.todoItems[process.argv[3]-1]
+  //   element = `${parseInt(process.argv[3])}. [X] - ${element}`
+  //   console.log(obj.todoItems)
+  // }
 
   jsonfile.writeFile(file, obj, (err) => {
     console.log(err)
