19046f689ef5114f4aaeb712288db047f9383963
eunicelok <eunicelp511@gmail.com>
2020-01-10 17:51:49 +0800

    Manage to show list in the command line

--- index.js
+++ index.js
@@ -13,13 +13,17 @@ jsonfile.readFile(file, (err, obj) => {
     if (process.argv[2] === "add") {
         obj["todoItems"].push(process.argv[3])
     }
-  console.log(obj);
+    // console.log("1. [ ] - " + obj["todoItems"][0]);
+    if (process.argv[2] === "show") {
+    for (let i = 0; i < obj["todoItems"].length; i++) {
+    console.log(i + 1 + ". [ ] - " + obj["todoItems"][i]);
+    }
+}
 
   jsonfile.writeFile(file, obj, (err) => {
-    console.log(err)
+    // console.log(err)
   });
 });
-
 
 
 //Create a commandline todo list app that you can use from your terminal and that will keep track of things you need to do.
