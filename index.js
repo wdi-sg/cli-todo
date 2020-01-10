51fdd0aa9c595bd004c5114a3c4ccf5b26fef412
Robert Kolsek <kolsek.income@gmail.com>
2020-01-10 23:45:43 +0800

    Further three "node index.js done 4"

--- index.js
+++ index.js
@@ -7,9 +7,15 @@ jsonfile.readFile(file, (err, obj) => {
   if (!obj.todoItems){
     obj.todoItems = []
   }
-
   if (!obj.tempStorage){
     obj.tempStorage = []
+  }
+
+  let display = () => {
+    //loop through todoItems for display
+    for (let i = 0; i < obj.todoItems.length; i++){
+      console.log(`${i+1}. ${obj.todoItems[i]}`)
+    }
   }
 
 
@@ -19,25 +25,26 @@ jsonfile.readFile(file, (err, obj) => {
     //push input and date into temp storage
     obj.tempStorage.push({input: process.argv[3], created: `created at: ${date.toString()}`})
     //push todo with string text into todoItems
-    obj.todoItems.push(`${obj.todoItems.length+1}. [ ] - ${process.argv[3]} - ${obj.tempStorage[obj.todoItems.length].created}`)
+    obj.todoItems.push(`[ ] - ${process.argv[3]} - ${obj.tempStorage[obj.todoItems.length].created}`)
 
-    //loop through todoItems for display
-    for (let i = 0; i < obj.todoItems.length; i++){
-      console.log(`${obj.todoItems[i]}`)
-    }
+    display()
   }
 
   if (process.argv[2] === "done"){
     let arrayItem = process.argv[3]-1
     //set todoItem string to X
-    obj.todoItems[arrayItem] = `${process.argv[3]}. [X] - ${obj.tempStorage[arrayItem].input} - ${obj.tempStorage[arrayItem].created}`
-    //loop and print all todoItems
-    for (let i = 0; i < obj.todoItems.length; i++){
-      console.log(`${obj.todoItems[i]}`)
-    }
+    obj.todoItems[arrayItem] = `[X] - ${obj.tempStorage[arrayItem].input} - ${obj.tempStorage[arrayItem].created}`
+    
+    display()
   }
 
-  //WORK ON ^^^^ step
+  if (process.argv[2] === "remove"){
+    let arrayItem = process.argv[3]-1
+    obj.todoItems.splice(arrayItem, 1)
+    obj.tempStorage.splice(arrayItem, 1)
+
+    display()
+  }
 
   jsonfile.writeFile(file, obj, (err) => {
     console.log(err)
