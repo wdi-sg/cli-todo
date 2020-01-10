f63acb5b98bcd9c6615e7e1df4c8f0049a0870da
Stuart Myers <stuart.myers@gmail.com>
2020-01-10 18:02:59 +0800

    Beautify and tidy up

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
+const jsonfile = require('jsonfile');
+
 console.log("works!!", process.argv[2]);
 
 const commandType = process.argv[2];
 const commandArg = process.argv[3];
 
-console.log("Your command was: " + commandType);
-
-const jsonfile = require('jsonfile');
-
 const file = 'data.json'
 
 var toDoItems = [];
 
+// Make To Do items a class, so all objects can be the same.
 class ToDoListItem {
     constructor(item, isDone = false) {
         this.item = item;
         this.isDone = isDone;
         this.dateCreated = new Date();
-        this.dateUpdated = new Date();
+        this.dateUpdated = new Date(); // Update this each time the object is changed.
     }
 }
 
+// Parse argument to see what command to do. By default just give an error and list the to-do items.
 const whatCommand = () => {
     switch (commandType) {
         case 'add':
@@ -41,6 +41,7 @@ const whatCommand = () => {
     }
 }
 
+// What to do when the file is read.
 const onFileRead = (err, obj) => {
 
     if (err) {
@@ -49,15 +50,20 @@ const onFileRead = (err, obj) => {
         return;
     }
 
+    // If there is no to-do items list, create an empty list.
     if (!obj.toDoItems) {
         obj.toDoItems = [];
     }
+
     toDoItems = obj.toDoItems;
 
+    // Do the user command.
     whatCommand();
+    // Always list the items:
     console.log('Items in list:');
     listToDoListItems();
 
+    // Write everything back to the json.
     jsonfile.writeFile(file, obj, (err) => {
         if (err) {
             console.log('Error writing file:');
@@ -66,15 +72,22 @@ const onFileRead = (err, obj) => {
     });
 };
 
+
 const addToDoListItem = (inputArg) => {
     console.log('adding' + inputArg);
     let itemToPush = new ToDoListItem(inputArg, false)
     toDoItems.push(itemToPush);
 }
 
+// Returns true if input number is outside of range of to-do list items.
+const invalidInput = (inputNo) => {
+    return (inputNo <= 0 || inputNo > toDoItems.length || !inputNo);
+}
+
+// Mark a To-Do list item as completed.
 const markedDone = (inputNo) => {
     inputNo = parseInt(inputNo);
-    if (inputNo <= 0 || inputNo > toDoItems.length || !inputNo) {
+    if (invalidInput(inputNo)) {
         console.log('Error please put in a valid number');
         return;
     }
@@ -83,6 +96,7 @@ const markedDone = (inputNo) => {
     toDoItems[indexNo].dateUpdated = new Date();
 }
 
+// For loop to list all items in the to do list, with date created and date updated.
 const listToDoListItems = () => {
     for (let i = 0; i < toDoItems.length; i++) {
         let markedAsDone = toDoItems[i].isDone ? "X" : " ";
@@ -92,14 +106,16 @@ const listToDoListItems = () => {
     }
 }
 
+// Permanently remove an item from the list.
 const deleteItem = (inputNo) => {
-  inputNo = parseInt(inputNo);
-  if (inputNo <= 0 || inputNo > toDoItems.length || !inputNo) {
-    console.log('Error please put in a valid number');
-    return;
-}
-  let indexNo = inputNo - 1;
-  toDoItems.splice(indexNo, 1);
+    inputNo = parseInt(inputNo);
+    if (invalidInput(inputNo)) {
+        console.log('Error please put in a valid number');
+        return;
+    }
+    let indexNo = inputNo - 1;
+    toDoItems.splice(indexNo, 1);
 }
 
+// The program runs by reading the json file.
 jsonfile.readFile(file, onFileRead);
