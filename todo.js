bb0b18d1880540454f51da4a5b8f7484221ae600
Jess <jessicatanhc@gmail.com>
2020-01-10 16:44:24 +0800

    get values from terminal

--- todo.js
+++ todo.js
@@ -1,6 +1,8 @@
 const jsonfile = require('jsonfile');
 
 const file = 'data.json';
+var commandType = process.argv[2];
+var  task = process.argv[3];
 
 var addToList = (task)=>{
  jsonfile.readFile(file, (err, obj) => {
@@ -11,4 +13,8 @@ var addToList = (task)=>{
     console.log(err);
   });
 });
+}
+
+if(commandType === "add" && task!== null){
+    addToList(task);
 }
