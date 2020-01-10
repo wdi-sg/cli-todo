7a1362fb227975062f3970965016fab08587170c
Robert Kolsek <kolsek.income@gmail.com>
2020-01-10 23:00:03 +0800

    Further one "node index.js done 2"

--- index.js
+++ index.js
@@ -4,20 +4,36 @@ const file = 'data.json'
 
 jsonfile.readFile(file, (err, obj) => {
 
-  console.log(obj);
   if (!obj.todoItems){
     obj.todoItems = []
   }
 
-  if (process.argv[2] === "add"){
-    obj.todoItems.push(`${obj.todoItems.length+1}. [ ] - ${process.argv[3]}`)
-    console.log(obj.todoItems)
+  if (!obj.tempStorage){
+    obj.tempStorage = []
   }
-  // if (process.argv[2] === "done"){
-  //   let element = obj.todoItems[process.argv[3]-1]
-  //   element = `${parseInt(process.argv[3])}. [X] - ${element}`
-  //   console.log(obj.todoItems)
-  // }
+
+
+  if (process.argv[2] === "add"){
+    obj.tempStorage.push(process.argv[3])
+    obj.todoItems.push(`${obj.todoItems.length+1}. [ ] - ${process.argv[3]}`)
+    
+    for (let i = 0; i < obj.todoItems.length; i++){
+      console.log(`${obj.todoItems[i]}`)
+
+    }
+  }
+
+  if (process.argv[2] === "done"){
+    let arrayItem = process.argv[3]-1
+    obj.todoItems[arrayItem] = `${process.argv[3]}. [X] - ${obj.tempStorage[arrayItem]}`
+
+    for (let i = 0; i < obj.todoItems.length; i++){
+      console.log(`${obj.todoItems[i]}`)
+    }
+
+  }
+
+  //WORK ON ^^^^ step
 
   jsonfile.writeFile(file, obj, (err) => {
     console.log(err)
