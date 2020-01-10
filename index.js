dec8e2654095a3af1a88d11d7ad1fc56062f798c
Stuart Myers <stuart.myers@gmail.com>
2020-01-10 16:44:52 +0800

    Can add items and mark as done

--- index.js
+++ index.js
@@ -11,14 +11,24 @@ const file = 'data.json'
 
 var toDoItems = [];
 
+class ToDoListItem {
+    constructor(item, isDone = false) {
+        this.item = item;
+        this.isDone = isDone;
+    }
+}
+
 const whatCommand = () => {
     switch (commandType) {
         case 'add':
-            addToDoListItem();
+            addToDoListItem(commandArg);
             break;
         case 'show':
             break;
         case 'display':
+            break;
+        case 'done':
+            markedDone(commandArg);
             break;
         default:
             console.log('Not a valid command');
@@ -45,20 +55,32 @@ const onFileRead = (err, obj) => {
 
     jsonfile.writeFile(file, obj, (err) => {
         if (err) {
-          console.log('Error writing file:');
-          console.log(err);
+            console.log('Error writing file:');
+            console.log(err);
         }
     });
 };
 
-const addToDoListItem = () => {
-    console.log('adding' + commandArg);
-    toDoItems.push(commandArg);
+const addToDoListItem = (inputArg) => {
+    console.log('adding' + inputArg);
+    let itemToPush = new ToDoListItem(inputArg, false)
+    toDoItems.push(itemToPush);
+}
+
+const markedDone = (inputNo) => {
+    inputNo = parseInt(inputNo);
+    if (inputNo <= 0 || inputNo > toDoItems.length || !inputNo) {
+        console.log('Error please put in a valid number');
+        return;
+    }
+    var indexNo = inputNo - 1;
+    toDoItems[indexNo].isDone = true;
 }
 
 const listToDoListItems = () => {
     for (let i = 0; i < toDoItems.length; i++) {
-        console.log(toDoItems[i]);
+        let markedAsDone = toDoItems[i].isDone ? "X" : " ";
+        console.log(`${i+1}. [${markedAsDone}] - ${toDoItems[i].item}`);
     }
 }
 
