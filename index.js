feb67f18d00b04c772281a05a8be198d859341d4
Robert Kolsek <kolsek.income@gmail.com>
2020-01-11 00:05:23 +0800

    refactored code

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const file = 'data.json'
 
 jsonfile.readFile(file, (err, obj) => {
 
+  let display = () => {
+    //loop through todoItems for display
+    for (let i = 0; i < obj.todoItems.length; i++){
+      console.log(`${i+1}. ${obj.todoItems[i]}`)
+    }
+  }
+
   if (!obj.todoItems){
     obj.todoItems = []
   }
@@ -11,36 +18,41 @@ jsonfile.readFile(file, (err, obj) => {
     obj.tempStorage = []
   }
 
-  let display = () => {
-    //loop through todoItems for display
-    for (let i = 0; i < obj.todoItems.length; i++){
-      console.log(`${i+1}. ${obj.todoItems[i]}`)
-    }
-  }
   let command = process.argv[2]
+  let input = process.argv[3]
 
   if (command === "add"){
     //create date
     let date = new Date()
     //push input and date into temp storage
-    obj.tempStorage.push({input: process.argv[3], created: `created at: ${date.toString()}`})
+    obj.tempStorage.push({input: input, created: `created at: ${date.toString()}`})
+
+    let dateValue = obj.tempStorage[obj.todoItems.length].created
     //push todo with string text into todoItems
-    obj.todoItems.push(`[ ] - ${process.argv[3]} - ${obj.tempStorage[obj.todoItems.length].created}`)
+    obj.todoItems.push(`[ ] - ${input} - ${dateValue}`)
 
     display()
   }
 
   if (command === "done"){
+    //set update date to todo
     let date = new Date()
-    let arrayItem = process.argv[3]-1
     obj.tempStorage[arrayItem].updated = `updated at: ${date.toString()}`
-    //set todoItem string to X
-    obj.todoItems[arrayItem] = `[X] - ${obj.tempStorage[arrayItem].input} - ${obj.tempStorage[arrayItem].created} - ${obj.tempStorage[arrayItem].updated}`
+
+    //variables for better readability
+    let arrayItem = input-1
+    let todo = obj.tempStorage[arrayItem].input
+    let createdDate = obj.tempStorage[arrayItem].created
+    let updatedDate = obj.tempStorage[arrayItem].updated
+
+    //set todoItem string to X and add updatedDate
+    obj.todoItems[arrayItem] = `[X] - ${todo} - ${createdDate} - ${updatedDate}`
     display()
   }
 
   if (command === "remove"){
-    let arrayItem = process.argv[3]-1
+    let arrayItem = input-1
+    //remove array item from todo and temp array
     obj.todoItems.splice(arrayItem, 1)
     obj.tempStorage.splice(arrayItem, 1)
     display()
