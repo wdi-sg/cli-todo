f733e2c17075afed210dff1e32aee4f3c33de9c8
eunicelok <eunicelp511@gmail.com>
2020-01-10 17:25:59 +0800

    Can add individual item into the empty array, yet to do 'show' onwards

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-console.log("works!!", process.argv[2]);
+// console.log("works!!", process.argv[2]);
 
-var commandType = process.argv[2];
+// var commandType = process.argv[2];
 
-console.log("Your command was: "+commandType);
+// console.log("Your command was: "+commandType);
 
 const jsonfile = require('jsonfile');
 
@@ -10,10 +10,31 @@ const file = 'data.json'
 
 jsonfile.readFile(file, (err, obj) => {
 
+    if (process.argv[2] === "add") {
+        obj["todoItems"].push(process.argv[3])
+    }
   console.log(obj);
-  obj["helloworld"] = "monkey";
 
   jsonfile.writeFile(file, obj, (err) => {
     console.log(err)
   });
 });
+
+
+
+//Create a commandline todo list app that you can use from your terminal and that will keep track of things you need to do.
+
+// let counter = 0;
+
+// if (process.argv[2] === "add") {
+//     for (var i = 0; i < list.length; i++) {
+//     let smth = i + "." + "[ ]" + " - " + process.argv[3] + " " + process.argv[4] + " " + process.argv[5];//how to let it add process.argv[3] onwards unlimitedly?
+//     counter++;
+//     file.value.push(smth);
+//     console.log(smth);
+//     }
+// }
+
+// if (process.argv[2] === "show") {
+//     console.log(list);
+// }
