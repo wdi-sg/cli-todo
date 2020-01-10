04d05feca54b10df90382aa9a7fec3af55e8d0c7
Jess <jessicatanhc@gmail.com>
2020-01-10 16:39:18 +0800

    Add to list function

--- todo.js
+++ todo.js
@@ -1,3 +1,14 @@
 const jsonfile = require('jsonfile');
 
 const file = 'data.json';
+
+var addToList = (task)=>{
+ jsonfile.readFile(file, (err, obj) => {
+
+  console.log(obj);
+  obj["todoItems"].push(task);
+  jsonfile.writeFile(file, obj, (err) => {
+    console.log(err);
+  });
+});
+}
