25524d1ff6cb3c9d1fbc3756e670f3b94a0d5714
rachellesg <rachelle.yeo@outlook.com>
2020-01-11 11:24:49 +0800

    refactored code, done (done) further

--- todo.js
+++ todo.js
@@ -1,8 +1,13 @@
-console.log("works!!", process.argv[2], process.argv[3]);
+//console.log("works!!", process.argv[2]);
 
 var commandType = process.argv[2];
 
 //console.log("Your command was: "+commandType);
+
+// {
+//   "todoItems": [
+//   ]
+// }
 
 const jsonfile = require('jsonfile');
 
@@ -12,19 +17,49 @@ jsonfile.readFile(file, (err, obj) => {
 
     // console.log(obj);
     // obj["helloworld"] = "monkey";
-    var emptyBox = "[ ] - ";
-    var checkedBox = "[X] - ";
-    var listNo = (obj.todoItems.length + 1) + ". ";
-    var chore = process.argv[3];
+
+    var choreDone = false;
+    var box;
+
+    // set boolean true or false for done-ness
+
+    // don't have to store in this format
+    // only need to display in this format
+    var todoList = obj.todoItems.length;
+    var todoListChore = obj.todoItemsChore.length;
+    let chore = process.argv[3];
+
+    function checkDoneChores() {
+        if (choreDone === true) {
+            box = " [X] - ";
+        } else if (choreDone === false) {
+            box = " [ ] - ";
+        }
+    }
 
     if (commandType === "add") {
-        obj.todoItems.push(listNo + emptyBox + chore)
+        choreDone === false;
+        obj.todoItems.push(chore);
+        obj.todoItemsChore.push(choreDone);
     } else if (commandType === "show") {
-        console.log(obj.todoItems);
+        for (i = 0; i < todoList; i++) {
+            checkDoneChores();
+            var listNo = parseInt(parseInt([i]) + 1) + ". ";
+            if (obj.todoItemsChore[i] === true) {
+                box = " [X] - ";
+            } else if (obj.todoItemsChore[i] = true === false) {
+                box = " [ ] - ";
+            }
+            console.log(listNo + box + obj.todoItems[i]);
+                // +
+        }
     } else if (commandType === "done") {
-        // mark empty as checked
-        // var doneItem = process.argv[3];
-        // obj.todoItems[(doneItem-1)] = doneItem + checkedBox + chore;
+        //mark empty as checked
+        var listNo = parseInt(process.argv[3]-1);
+        obj.todoItemsChore[listNo] = true;
+        box = " [X] - ";
+        console.log(chore + box + obj.todoItems[listNo]);
+        //console.log(listNo + box + obj.todoItems);
     } else if (commandType === "delete") {
         obj.todoItems.pop()
     }
