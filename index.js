a7633ab88427e546f4ace486186477a4795738d0
Angel Ferreros <AngelFerreros@tjs-macbook-pro.lan>
2020-01-11 15:17:36 +0800

    Added logic to mark item as done

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log("works!!");
+console.log('works!!');
 
 // Implement the deliverables in the order shown above.
 // Add to the list. (It starts empty)
@@ -6,26 +6,48 @@ console.log("works!!");
 
 // Hint: you MUST add a data structure into the json file: the array of todo items you want to track.
 
+
+//if process.argv[2] == add - push to array
+//else if  process.argv[2] == done - create [x] string
 const jsonfile = require('jsonfile');
 
 const file = 'data.json';
-// add item input by user into array
-    // starts at number 1 and increment by 1 each time
+
+var counter = 0; // to track count of items in array
+
+
 jsonfile.readFile(file, (err, obj) => {
     console.log(obj);
-    //generate number and brackets
-    var counter = 0;
-    for (var i = 0 ; i <= obj["todoItems"].length ; i++) {
-        counter = counter + 1;
+//generate number and brackets
+    for (var i = 0 ; i <= obj['todoItems'].length ; i++) {
+    counter = counter + 1;
     }
-        let brackets = `${counter}. [ ]`;
+    let openTaskNum = `${counter}. [ ]`;
 
-        obj["todoItems"].push(`${brackets} - ${process.argv[3]}`);
 
+    if (process.argv[2] == 'add'){
+        let task = process.argv[3];
+        console.log(task);
+        obj['todoItems'].push(`${openTaskNum} - ${task}`);
         console.log(obj);
+    }
+    else if (process.argv[2] == 'done'){
+        let taskNumber = process.argv[3];
+        console.log(taskNumber);
+        let taskIndex = taskNumber-1;
+        let item = obj['todoItems'][taskIndex];
+        let itemArray = item.split(" "); //splits item into array
+        console.log(itemArray);
+        itemArray.splice(2,0,'x');
+        item = itemArray.join(' ');
+        obj['todoItems'].splice(taskNumber-1,1,item);
+        console.log(obj);
+
+    }
 
   jsonfile.writeFile(file, obj, (err) => {
     console.log(err);
     console.log(obj);
   });
+
 });
