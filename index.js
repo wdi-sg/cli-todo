a48aad736e1e9626a77de54b98d25f06d8af86e0
Angel Ferreros <AngelFerreros@tjs-macbook-pro.lan>
2020-01-11 10:49:32 +0800

    First part done - add tasks in todoItems

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
-console.log("works!!", process.argv[2]);
+console.log("works!!");
 
-var commandType = process.argv[2];
+// Implement the deliverables in the order shown above.
+// Add to the list. (It starts empty)
+// Show all the items in the list (Once you have something inside it)
 
-console.log("Your command was: "+commandType);
+// Hint: you MUST add a data structure into the json file: the array of todo items you want to track.
 
 const jsonfile = require('jsonfile');
 
-const file = 'data.json'
-
+const file = 'data.json';
+// add item input by user into array
+    // starts at number 1 and increment by 1 each time
 jsonfile.readFile(file, (err, obj) => {
+    console.log(obj);
+    //generate number and brackets
+    var counter = 0;
+    for (var i = 0 ; i <= obj["todoItems"].length ; i++) {
+        counter = counter + 1;
+    }
+        let brackets = `${counter}. [ ]`;
 
-  console.log(obj);
-  obj["helloworld"] = "monkey";
+        obj["todoItems"].push(`${brackets} - ${process.argv[3]}`);
+
+        console.log(obj);
 
   jsonfile.writeFile(file, obj, (err) => {
-    console.log(err)
+    console.log(err);
+    console.log(obj);
   });
 });
