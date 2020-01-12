40279c5ba43217b21f9f23c94b69e214b69e9884
joycemap <57893220+joycemap@users.noreply.github.com>
2020-01-12 22:30:30 +0800

    Halfway through check done

--- todo.js
+++ todo.js
@@ -4,15 +4,26 @@ const file = 'data.json'
 
 jsonfile.readFile(file, (err, obj) => {
     let todoItems = obj.todoItems;
-    console.log(todoItems);
-    if (process.argv[2] === "add") {
-       todoItems.push(process.argv[3])
-    }
-    for (let i = 0; i < todoItems.length; i++) {
-        console.log(i + 1 + ". [ ] - " + todoItems[i])
-        }
+    //console.log(todoItems);
+   //  if (process.argv[2] === "add") {
+   //     todoItems.push(process.argv[3])
+   //  }
+    
+    
+   //  for (let i = 0; i < todoItems.length; i++) {
+   //      console.log((i + 1) + ". [] - " + todoItems[i])
+   //      }
+        for (let i = 0; i < todoItems.length; i++) {
+           let parsedIdx = parseInt(process.argv[3])
+         if ((process.argv[2] === "done") && (parsedIdx === i-1)){
+            let doneItem = todoItems[i-1].replace(". [] -", ". [X] -");
+            console.log(todoItems.splice(parsedIdx,0,doneItem));
+                }
+       
+         }
  jsonfile.writeFile(file, obj, (err) => {
     // console.log(err)
      
   });
 });
+
