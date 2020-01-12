4335a2c81a1fdb443d70891479695770cbe0d2fe
Robert Kolsek <kolsek.income@gmail.com>
2020-01-12 11:41:19 +0800

    Added Ascii-text. Last Further done.

--- index.js
+++ index.js
@@ -1,64 +1,97 @@
 const jsonfile = require('jsonfile');
-
+const figlet = require('figlet')
 const file = 'data.json'
 
-jsonfile.readFile(file, (err, obj) => {
+//ASCII Art
+figlet.text('To-do List', {
+  font: 'Larry 3D',
+  horizontalLayout: 'default',
+  verticalLayout: 'default'
+}, function (err, data) {
+  if (err) {
+    console.log('Something went wrong...');
+    console.dir(err);
+    return;
+  }
+  console.log(data);
 
-  let display = () => {
-    //loop through todoItems for display
-    for (let i = 0; i < obj.todoItems.length; i++){
-      console.log(`${i+1}. ${obj.todoItems[i]}`)
+
+  //ACTUAL PROGRAM
+  jsonfile.readFile(file, (err, obj) => {
+
+    if (err){
+      return console.log(err)
     }
-  }
+    const display = () => {
+      //loop through todoItems for display
+      for (let i = 0; i < obj.todoItems.length; i++) {
+        console.log(`${i+1}. ${obj.todoItems[i]}`)
+      }
+    }
+    //create arrays if not existant
+    if (!obj.todoItems) {
+      obj.todoItems = []
+    }
+    if (!obj.tempStorage) {
+      obj.tempStorage = []
+    }
 
-  if (!obj.todoItems){
-    obj.todoItems = []
-  }
-  if (!obj.tempStorage){
-    obj.tempStorage = []
-  }
+    let command = process.argv[2]
+    let input = process.argv[3]
 
-  let command = process.argv[2]
-  let input = process.argv[3]
+    if (command === "add") {
+      //create date
+      let date = new Date()
+      //push input and date into temp storage
+      obj.tempStorage.push({
+        input: input,
+        created: `created at: ${date.toString()}`
+      })
 
-  if (command === "add"){
-    //create date
-    let date = new Date()
-    //push input and date into temp storage
-    obj.tempStorage.push({input: input, created: `created at: ${date.toString()}`})
+      //variable for better readability
+      let dateValue = obj.tempStorage[obj.todoItems.length].created
+      //push todo with string text into todoItems
+      obj.todoItems.push(`[ ] - ${input} - ${dateValue}`)
 
-    let dateValue = obj.tempStorage[obj.todoItems.length].created
-    //push todo with string text into todoItems
-    obj.todoItems.push(`[ ] - ${input} - ${dateValue}`)
+      display()
+    }
 
-    display()
-  }
+    if (command === "done") {
+      //set update date to todo
+      let date = new Date()
 
-  if (command === "done"){
-    //set update date to todo
-    let date = new Date()
-    obj.tempStorage[arrayItem].updated = `updated at: ${date.toString()}`
+      //variables for better readability
+      let arrayItem = input - 1
+      let todo = obj.tempStorage[arrayItem].input
+      let createdDate = obj.tempStorage[arrayItem].created
+      
 
-    //variables for better readability
-    let arrayItem = input-1
-    let todo = obj.tempStorage[arrayItem].input
-    let createdDate = obj.tempStorage[arrayItem].created
-    let updatedDate = obj.tempStorage[arrayItem].updated
+      //create updated key and create variable for it
+      obj.tempStorage[arrayItem].updated = `updated at: ${date}`
+      let updatedDate = obj.tempStorage[arrayItem].updated
 
-    //set todoItem string to X and add updatedDate
-    obj.todoItems[arrayItem] = `[X] - ${todo} - ${createdDate} - ${updatedDate}`
-    display()
-  }
+      //set todoItem string to X and add updatedDate
+      obj.todoItems[arrayItem] = `[X] - ${todo} - ${createdDate} - ${updatedDate}`
 
-  if (command === "remove"){
-    let arrayItem = input-1
-    //remove array item from todo and temp array
-    obj.todoItems.splice(arrayItem, 1)
-    obj.tempStorage.splice(arrayItem, 1)
-    display()
-  }
+      display()
+    }
 
-  jsonfile.writeFile(file, obj, (err) => {
-    console.log(err)
+    if (command === "remove") {
+      let arrayItem = input - 1
+      //remove array item from todo and temp array
+      obj.todoItems.splice(arrayItem, 1)
+      obj.tempStorage.splice(arrayItem, 1)
+      display()
+    }
+
+    jsonfile.writeFile(file, obj, (err) => {
+      if (err) {
+        return console.log(err)
+      }
+
+    });
   });
+
+
+
 });
