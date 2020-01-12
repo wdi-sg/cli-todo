1432d0c57aed9a6b6c30df625580a36c8c0d13fe
SamTanFL <samsite_tan@hotmail.com>
2020-01-12 09:56:16 +0800

    added "done" features

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let commandType = process.argv[2];
 let commandString = process.argv[3];
+let praseCString = parseInt(process.argv[3]);
 const jsonfile = require('jsonfile');
 const file = 'data.json'
 
@@ -29,15 +30,22 @@ switch (commandType) {
                     console.log(i + 1 + ". [ x ] - " + obj.toDoItems[i]);
                 } //closing bracket for if and else
             }//closing brackets for For loop
-            jsonfile.writeFile(file, obj, (err) => {
-                if (err != null) {
-                    console.log(err);
-                } // closing bracket for if
-            }) // closing bracket for writeFile
         }) //closing bracket for readFile
     break;
     case "done":
-
+        jsonfile.readFile(file, (err, obj) => {
+            if (obj.toDoItems[(praseCString - 1)] !== undefined) {
+                obj.done[(praseCString - 1)] = true;
+                console.log(commandString + ". '" + obj.toDoItems[(praseCString - 1)] + "' is done.");
+            } else {
+                console.log("Invalid Input");
+            }
+                jsonfile.writeFile(file, obj, (err) => {
+                    if (err != null) {
+                        console.log(err);
+                    } // closing bracket for "if"
+                }); //closing bracket for "writeFile"
+            }); //closing bracket for "readFile"
     break;
     default:
         console.log("Please type a recognizable command. Eg. 'add', 'show' or 'done'");
