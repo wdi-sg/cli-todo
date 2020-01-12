bcd6ba08684667292964c4d860d4d5e72d8e1bbf
SamTanFL <samsite_tan@hotmail.com>
2020-01-12 09:26:16 +0800

    added "show" function

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ switch (commandType) {
     case "add":
         jsonfile.readFile(file, (err, obj) => {
             obj.toDoItems.push(commandString);
+            obj.done.push(false);
             jsonfile.writeFile(file, obj, (err) => {
                 if (err != null) {
                     console.log(err);
@@ -15,17 +16,26 @@ switch (commandType) {
         }); //closing bracket for "readFile"
     break;
     case "show":
-        json.readFile(file, (err,obj) => {
+        jsonfile.readFile(file, (err,obj) => {
+            for (var i = 0; i < obj.toDoItems.length; i++) {
+                if (obj.done[i] == false) {
+                    console.log(i + 1 + ". [   ] - "+ obj.toDoItems[i]);
+                } else {
+                    console.log(i + 1 + ". [ x ] - " + obj.toDoItems[i]);
+                } //closing bracket for if and else
+            }//closing brackets for For loop
             jsonfile.writeFile(file, obj, (err) => {
                 if (err != null) {
                     console.log(err);
-                } //closing bracket for if
+                } // closing bracket for if
             }) // closing bracket for writeFile
         }) //closing bracket for readFile
     break;
     case "done":
 
     break;
+    default:
+        console.log("Please type a recognizable command. Eg. 'add', 'show' or 'done'");
 }
 
 
