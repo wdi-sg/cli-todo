e89820144e0d77b218c26331f58d79a7c1859678
SafrazHakamali <safrazhakamali@gmail.com>
2020-01-12 13:48:27 +0800

    completed add to list, done tagging, created at, delete and updated at.

--- index.js
+++ index.js
@@ -1,19 +1,58 @@
-console.log("works!!", process.argv[2]);
-
 var commandType = process.argv[2];
+var entry = "";
+if (process.argv[3]) {
+	entry=process.argv[3];
+};
+var jsonfile = require('jsonfile');
+var file = 'data.json';
+var content = {"todoItems":[]};
 
-console.log("Your command was: "+commandType);
+var displayList = function(obj) {
+	for (i=0; i<obj["todoItems"].length; i++) {
+			console.log(parseInt(i+1)+". ["+obj["completion"][i]+"] - "+obj["todoItems"][i] + " created_at @ "+obj["created_at"][i]+obj["updated_at"][i]);	};
+};
 
-const jsonfile = require('jsonfile');
+if (commandType=="add") {
+	jsonfile.readFile(file, (err, obj) => {
+		if (Object.keys(obj).length == 0) {
+			obj.todoItems=[];
+			obj.completion=[];
+			obj.created_at=[];
+			obj.updated_at=[];
+		};
+		obj["todoItems"].push(entry);
+		obj["completion"].push(" ");
+		obj["created_at"].push(Date());
+		obj["updated_at"].push(" ");
+		jsonfile.writeFile(file, obj, (err) => {
+		});
+		displayList(obj);
+	});
+};
 
-const file = 'data.json'
+if (commandType=="show") {
+	jsonfile.readFile(file, (err, obj) => {
+		displayList(obj);
+	});
+};
 
-jsonfile.readFile(file, (err, obj) => {
+if (commandType=="done") {
+	jsonfile.readFile(file, (err, obj) => {
+		obj["completion"][parseInt(entry)-1]="x";
+		obj["updated_at"][parseInt(entry)-1]=" & completed at "+Date();
+		jsonfile.writeFile(file, obj, (err) => {
+		});
+		displayList(obj);
+	});
+};
 
-  console.log(obj);
-  obj["helloworld"] = "monkey";
-
-  jsonfile.writeFile(file, obj, (err) => {
-    console.log(err)
-  });
-});
+if (commandType=="delete") {
+	jsonfile.readFile(file, (err, obj) => {
+		obj["todoItems"].splice(parseInt(entry)-1,1);
+		obj["completion"].splice(parseInt(entry)-1,1);
+		obj["created_at"].splice(parseInt(entry)-1,1);
+		jsonfile.writeFile(file, obj, (err) => {
+		});
+		displayList(obj);
+	});
+};
