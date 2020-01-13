4bff827dd260400d1a130b3aa444128006942378
joycemap <57893220+joycemap@users.noreply.github.com>
2020-01-13 08:55:56 +0800

    Included splice function

--- todo.js
+++ todo.js
@@ -4,11 +4,12 @@ const file = 'data.json'
 
 jsonfile.readFile(file, (err, obj) => {
     let todoItems = obj.todoItems;
+    let prefix = todoItems[i-1] + ". [] -"
+    console.log(prefix + todoItems)
     //console.log(todoItems);
-   //  if (process.argv[2] === "add") {
-   //     todoItems.push(process.argv[3])
-   //  }
-    
+    //if (process.argv[2] === "add") {
+    //  todoItems.push(process.argv[3])
+   //}
     
    //  for (let i = 0; i < todoItems.length; i++) {
    //      console.log((i + 1) + ". [] - " + todoItems[i])
@@ -16,8 +17,8 @@ jsonfile.readFile(file, (err, obj) => {
         for (let i = 0; i < todoItems.length; i++) {
            let parsedIdx = parseInt(process.argv[3])
          if ((process.argv[2] === "done") && (parsedIdx === i-1)){
-            let doneItem = todoItems[i-1].replace(". [] -", ". [X] -");
-            console.log(todoItems.splice(parsedIdx,0,doneItem));
+            let doneItem = todoItems[parsedIdx].replace(". [] -", ". [X] -");
+            console.log((prefix + todoItems) + todoItems.splice(parsedIdx,0,doneItem));
                 }
        
          }
