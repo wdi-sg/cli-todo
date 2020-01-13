f42ff8fc9c1a0c54a6358e523987b7f770347c6d
rohai <rohai.sul@gmail.com>
2020-01-13 09:28:31 +0800

    part 1 only

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 console.log("works!!", process.argv[2]);
 
-var commandType = process.argv[2];
 
+var commandType = process.argv[2];
 console.log("Your command was: "+commandType);
 
 const jsonfile = require('jsonfile');
-
 const file = 'data.json'
+
 
 jsonfile.readFile(file, (err, obj) => {
 
+    if (process.argv[2] === "add") {
+        obj["todoItems"].push(process.argv[3])
+    }
+
+    for (let i = 0; i < obj["todoItems"].length; i++) {
+    console.log(i + 1 +". [ ] - " + obj["todoItems"][i]);
+    }
+
   console.log(obj);
-  obj["helloworld"] = "monkey";
+
 
   jsonfile.writeFile(file, obj, (err) => {
     console.log(err)
