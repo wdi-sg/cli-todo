91c675a8b1e045d3e443ea54f849de7d5a4091ac
“hweechieh” <hweechieh@gmail.com>
2020-01-13 12:45:39 +0800

    Level One complete

--- index.js
+++ index.js
@@ -22,36 +22,35 @@ jsonfile.readFile(file, (err, obj) => {
         todoTask.name = todoInput;
         todoTask.status = false;
 
-
         obj["todoItems"].push(todoTask);
 
+        for (let i = 0; i < obj.todoItems.length; i++) {
+            console.log(obj.todoItems[i].task +". [ ] - " + obj.todoItems[i].name);
 
-        for(let i = 0; i<obj.todoItems.length; i++){
-            if(obj.todoItems.status === false){
-                console.log(obj.todoItems[i].task +". [ ] - " + obj.todoItems[i].name);
-            }
-            if(obj.todoItems.status === true){
+            if (obj.todoItems.status === true) {
                 console.log(obj.todoItems[i].task +". [x] - " + obj.todoItems[i].name);
+            }
+            if (thirdWord === obj.todoItems[i].name) {
+                console.log("Task has already been recorded, please try again.");
             }
         }
     }
 
-            // if (thirdWord === obj.todoItems[i].name) {
-            //     console.log("Task has already been recorded, please try again.");
-            // }
 
 
-    // if (thirdWord === "show"){
-
-    //     for(let i = 0; i<obj.todoItems.length; i++){
-    //         console.log(obj.todoItems[i].task +". [ ] - " + obj.todoItems[i].name);
+    // for(let i = 0; i<obj.todoItems.length; i++){
+    //         if(obj.todoItems.status === false){
+    //             console.log(obj.todoItems[i].task +". [ ] - " + obj.todoItems[i].name);
+    //         }
+    //         if(obj.todoItems.status === true){
+    //             console.log(obj.todoItems[i].task +". [x] - " + obj.todoItems[i].name);
+    //         }
     //     }
-    // }
-
 
 
   jsonfile.writeFile(file, obj, (err) => {
-    console.log("logged")
+    console.log(err)
+    // console.log(obj)
   });
 
 });
