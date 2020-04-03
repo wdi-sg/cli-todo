85cda333ffbf2f61b65bcf0fe920ef5467f45596
mdazharaw <darkaz62@hotmail.com>
2020-04-03 22:43:45 +0800

    Added mark as done. Part 2 done.

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 // console.log("works!!", process.argv[2]);
 
 var commandType = process.argv[2];
-var inputString = function(){
+var inputString = function () {
   var stringArr = process.argv;
-  var remove = stringArr.splice(0,3);
+  var remove = stringArr.splice(0, 3);
   // console.log(remove);
   var outString = "";
-  for (id in stringArr){
+  for (id in stringArr) {
     outString += stringArr[id];
     outString += " ";
   }
   return outString;
 }
+
+var inputNum = function () {
+  var num = parseInt(process.argv[3]);
+  return num;
+}
+
+//   } else if ((!isNaN(process.argv[3]) && process.argv.length==4)){
+//     var outNum = 0;
+//     outNum = parseInt(process.argv[3]);
+//     console.log(`Num ${outNum}`);
+//     return outNum;
+//   }
+
+// }
 
 // console.log("Your command was: "+commandType);
 
@@ -26,10 +40,13 @@ jsonfile.readFile(file, (err, obj) => {
   // console.log(obj);
   switch (commandType) {
     case "show":
-      show(obj.todoItems);
+      show(obj);
       break;
     case "add":
-      add(obj.todoItems);
+      add(obj);
+      break;
+    case "done":
+      done(obj);
       break;
     default:
       break;
@@ -43,13 +60,17 @@ jsonfile.readFile(file, (err, obj) => {
   });
 });
 
-function show(list) {
-  if (list.length == 0) {
+function show(object) {
+  if (object.todoItems.length == 0) {
     console.log("List is empty...");
-  } else if (list.length > 0) {
+  } else if (object.todoItems.length > 0) {
     var listOut = "";
-    for (id in list){
-      listOut += `${parseInt(id)+1}. [ ] - ${list[id]}\n`;
+    for (id in object.todoItems) {
+      if (object.doneItems.includes(object.todoItems[id])) {
+        listOut += `${parseInt(id) + 1}. [X] - ${object.todoItems[id]}\n`;
+      } else {
+        listOut += `${parseInt(id) + 1}. [ ] - ${object.todoItems[id]}\n`;
+      }
     }
     listOut = listOut.trim();
     console.log(listOut);
@@ -57,9 +78,17 @@ function show(list) {
 
 }
 
-function add(list) {
-  var input = inputString().trim();
-  list.push(input);
-  show(list);
-
+function add(object) {
+  var input = inputString();
+  input = input.trim();
+  object.todoItems.push(input);
+  show(object);
 }
+
+function done(object) {
+  var input = inputNum();
+  var doneItem = object.todoItems[input - 1];
+  object.doneItems.push(doneItem);
+  show(object);
+}
+
