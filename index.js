49e872add2e6b5507ad78f54bd08b1816dc39412
BOBBY <BOBBYKWONGZW@GMAIL.COM>
2020-04-03 20:12:39 +0800

    Script able to mark to do item as done with "X"

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const file = 'data.json'
 if(process.argv[2] === "add"){
     jsonfile.readFile(file, (err, obj) => {
 
-      obj["toDoItems"].push(process.argv[3]);
+      obj["toDoItems"].push(obj["toDoItems"].length + ". [ ] - " + process.argv[3]);
 
       if(err){
         console.log(err);
@@ -17,4 +17,27 @@ if(process.argv[2] === "add"){
         console.log(error)
       });
     });
+}
+
+if(process.argv[2] === "done"){
+    jsonfile.readFile(file, (err, obj) => {
+
+        // Mark X in the bracket of item that is done
+        const item = obj["toDoItems"][process.argv[3]];
+        itemSplit = item.split(' ')
+        itemSplit.splice(2, 0, 'X');
+        console.log(itemSplit);
+
+        // Join back array into full string
+        newObj = itemSplit.join(' ');
+
+
+        // Change obj to reflect new object
+        obj["toDoItems"][process.argv[3]] = newObj;
+
+        jsonfile.writeFile(file, obj, (error) => {
+            console.log(obj);
+            console.log(error);
+        })
+    })
 }
