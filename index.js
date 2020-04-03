7e52eea296a6227ebba06275c742186048b34b20
Ianshalom <you@example.comianshalom92@gmail.comgit config --global user.email you@example.comianshalom92@gmail.com>
2020-04-04 00:27:18 +0800

    1st commit

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
-console.log("works!!", process.argv[2]);
-
-var commandType = process.argv[2];
-
-console.log("Your command was: "+commandType);
-
 const jsonfile = require('jsonfile');
-
 const file = 'data.json'
+
+const processArgv = process.argv;
+
+
 
 jsonfile.readFile(file, (err, obj) => {
 
-  console.log(obj);
-  obj["helloworld"] = "monkey";
+const todoList = obj["todoItems"];
+console.log(todoList);
+let sqBrackets = "[ ]";
+let dot = ". ";
+let dash = " - ";
+
+let num = todoList.length + 1;
+
+        if (process.argv[2] === 'add') {
+
+                todoList.push(num + dot + sqBrackets + dash + process.argv[3]);
+        } else if (processArgv[2] === 'done') {
+            todoList[processArgv[3] - 1] = todoList[processArgv[3] - 1].replace("[ ]", "[X]");
+        }
+
+    console.log(obj);
+
+
 
   jsonfile.writeFile(file, obj, (err) => {
     console.log(err)
