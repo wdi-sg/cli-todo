b348f94c3ac63af0f4ae31f2d29c3f7f20ee644c
kokchuantan <kokchuan1996@gmail.com>
2020-04-03 23:32:18 +0800

    added mark as done function

--- todo.js
+++ todo.js
@@ -7,12 +7,23 @@ console.log("Your command was: " + commandType);
 const jsonfile = require('jsonfile');
 
 const file = 'data.json'
+var userInput1 = process.argv[3];
+var userInput2 = process.argv[4];
 
 const editStuff = (err, obj) => {
     if (commandType == 'add') {
-        console.log(obj);
+        let box = '- [ ]';
+        let count = obj.toDoItems.length + 1;
+        obj.toDoItems.push(`${count}: ${userInput1} ${userInput2} ${box}`);
     } else if (commandType == 'done') {
-        console.log(obj);
+        let box = '- [X]';
+        let choice = parseInt(userInput1)-1;
+        var change = obj.toDoItems[choice].split('-');
+        change[1] = box;
+        var completed = change.join('');
+        obj.toDoItems[choice] = completed
+    } else if (commandType == 'show') {
+        console.log(obj)
     } else {
         console.log('error')
     }
