1f0674514eda473004c481edf708303cfc86dada
Rachelik <rachel.ik.87@gmail.com>
2020-04-04 20:08:52 +0800

    first commit

--- todo.js
+++ todo.js
@@ -1,12 +1,11 @@
 console.log("works!!", process.argv[2]);
 
 var commandType = process.argv[2];
-
 console.log("Your command was: "+commandType);
 
 const jsonfile = require('jsonfile');
-
 const file = 'data.json'
+
 
 jsonfile.readFile(file, (err, obj) => {
 
@@ -16,4 +15,5 @@ jsonfile.readFile(file, (err, obj) => {
   jsonfile.writeFile(file, obj, (err) => {
     console.log(err)
   });
+
 });
