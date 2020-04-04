52a591c57417669b48d0f35bab94e4f6d2f7e5cf
Ianshalom <you@example.comianshalom92@gmail.comgit config --global user.email you@example.comianshalom92@gmail.com>
2020-04-04 12:25:29 +0800

    2nd commit

--- index.js
+++ index.js
@@ -8,21 +8,38 @@ const processArgv = process.argv;
 jsonfile.readFile(file, (err, obj) => {
 
 const todoList = obj["todoItems"];
-console.log(todoList);
 let sqBrackets = "[ ]";
 let dot = ". ";
 let dash = " - ";
+let num = 0;
 
-let num = todoList.length + 1;
 
         if (process.argv[2] === 'add') {
+            let todo = {};
+            todo["task"] = process.argv[3];
+            let date = new Date()
+            todo["created_at"] = date.toISOString();
+            todo["isDone"] = '[ ]';
+            todo["updated_at"] = '';
+            todoList.push(todo);
 
-                todoList.push(num + dot + sqBrackets + dash + process.argv[3]);
         } else if (processArgv[2] === 'done') {
-            todoList[processArgv[3] - 1] = todoList[processArgv[3] - 1].replace("[ ]", "[X]");
+            todoList[processArgv[3] - 1].isDone = "[X]";
+            let currentDate = new Date();
+            todoList[processArgv[3] - 1].updated_at = currentDate.toISOString();
+        } else if (processArgv[2] === 'del') {
+            let itemToRemove = process.argv[3];
+            todoList.splice((itemToRemove - 1), 1);
+
         }
 
-    console.log(obj);
+
+        for(let i = 0; i < todoList.length; i++) {
+            num++;
+            console.log(num + '. ' + todoList[i].isDone + ' - ' + todoList[i].task + ', ' + todoList[i].created_at);
+        }
+
+        console.log(obj);
 
 
 
