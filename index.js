72fc6677930c0a0f42b8eeceeece71c9b56d9055
chelsejw <chelsejw@gmail.com>
2020-04-04 16:31:16 +0800

    completed part 1

--- index.js
+++ index.js
@@ -1,19 +1,36 @@
-console.log("works!!", process.argv[2]);
-
-var commandType = process.argv[2];
-
-console.log("Your command was: "+commandType);
-
+const input = process.argv.slice(2)
+const command = input[0];
 const jsonfile = require('jsonfile');
-
 const file = 'data.json'
+
+//Function to console.log items in an array.
+const logArray = (array) => {
+  array.forEach((item) => console.log(item));
+}
 
 jsonfile.readFile(file, (err, obj) => {
 
-  console.log(obj);
-  obj["helloworld"] = "monkey";
+    const toDoList = obj["todoItems"]
 
-  jsonfile.writeFile(file, obj, (err) => {
-    console.log(err)
-  });
+//TO ADD STUFF INTO THE LIST.
+  if (command === "add") {
+      let newItem = input.slice(1).join(" ");
+      toDoList.push(`${(toDoList.length+1)}. [ ] - ${newItem}`);
+      jsonfile.writeFile(file, obj, (err) => {
+          if (!err) {
+            console.log(`Item added successfully! Updated list:`);
+            logArray(toDoList);
+          } else {
+            console.log(`Sorry, there was an error with adding to the list.`);
+            console.log(err);
+          }
+      });
+  }
+
+//TO SEE YOUR LIST.
+
+  if (command === "show") {
+    logArray(toDoList)
+  }
+  
 });
