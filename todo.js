bf89d65614436e81c9d0807828d0089c7f8bd452
Rachelik <rachel.ik.87@gmail.com>
2020-04-04 22:15:09 +0800

    Add numbered todoItems, show and clear.

--- todo.js
+++ todo.js
@@ -1,19 +1,51 @@
-console.log("works!!", process.argv[2]);
+// console.log("works!!", process.argv[2]);
 
-var commandType = process.argv[2];
-console.log("Your command was: "+commandType);
+// var commandType = process.argv[2];
+// console.log("Your command was: "+commandType);
 
+var nodeArgv = process.argv;
 const jsonfile = require('jsonfile');
-const file = 'data.json'
+const file = 'data.json';
 
+// var key = "todoItems";
 
-jsonfile.readFile(file, (err, obj) => {
+var listKey = "todoItems";
+if (nodeArgv[2] === "add") {
+    jsonfile.readFile(file, (err, obj) => {
 
-  console.log(obj);
-  obj["helloworld"] = "monkey";
+        if(obj[listKey] === undefined) {
+            obj[listKey] = [];
+        };
 
-  jsonfile.writeFile(file, obj, (err) => {
-    console.log(err)
-  });
+        var items = nodeArgv[3];
 
-});
+        for (var i = 0; i <= obj["todoItems"].length; i++) {
+            var itemsNum = i+1;
+        };
+
+        obj["todoItems"].push(itemsNum+" [ ] - " +items);
+        console.log("Item updated")
+
+        jsonfile.writeFile(file, obj, (err) => {
+            console.log(err)
+        });
+    });
+} else if (nodeArgv[2] === "show") {
+
+    jsonfile.readFile(file, (err, obj) => {
+        if (obj[listKey].length === 0) {
+            console.log("To do list is empty.")
+        }
+        for (var i = 0; i < obj["todoItems"].length; i++){
+            console.log(obj["todoItems"][i]);
+        };
+    })
+} else if (nodeArgv[2] === "clear") {
+    jsonfile.readFile(file, (err, obj) => {
+        obj["todoItems"] = [];
+
+        jsonfile.writeFile(file, obj, (err) => {
+            console.log(err);
+        });
+    });
+};
