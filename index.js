0cef8bcdfdf2df7880d1547dbc5503282baf2f0c
travisenquiry <travisenquiry@gmail.com>
2020-04-04 17:18:34 +0800

    completed further 4

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ if(process.argv[2] === "add"){
   let addObject = {
     "task": task,
     "isDone": "[ ]",
-    "created_at": date.toString()
+    "created_at": date.toString(),
+    "updated_at": "-"
   }
 
   jsonfile.readFile(file, (err, obj) => {
@@ -27,15 +28,17 @@ if(process.argv[2] === "add"){
 if(process.argv[2] === "show"){
   jsonfile.readFile(file, (err, obj) => {
     for(let i = 0; i < obj["toDoItems"].length; i++){
-      let line = (i + 1) + ". " + obj["toDoItems"][i].isDone + " -" + obj["toDoItems"][i].task + ", Created at: " + obj["toDoItems"][i].created_at;
+      let line = (i + 1) + ". " + obj["toDoItems"][i].isDone + " -" + obj["toDoItems"][i].task + ", Created at: " + obj["toDoItems"][i].created_at + ", Updated at: " + obj["toDoItems"][i].updated_at;
       console.log(line);
     }
   });
 }
 
 if(process.argv[2] === "done"){
+  let date = Date();
   jsonfile.readFile(file, (err, obj) => {
     obj["toDoItems"][process.argv[3] - 1].isDone = "[x]";
+    obj["toDoItems"][process.argv[3] - 1].updated_at = date.toString();
 
     jsonfile.writeFile(file, obj, (err) => {
       console.log("Error detected: " + err);
