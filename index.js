2210dbe8ac2e249b5610e6d2266c0c7f0f53c53c
wickker <choydianchun@gmail.com>
2020-04-04 11:56:39 +0800

    mvp edited

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const dataFile = "data.json";
 let inputArr = process.argv;
 
 const printList = (err, obj) => {
-  for (let i = 2; i < inputArr.length; i++) {
-    obj["todoItems"].push(obj["todoItems"].length + 1 + ". [ ] - " + inputArr[i]);
+  if (inputArr[2] === "add") {
+    for (let i = 3; i < inputArr.length; i++) {
+      obj["todoItems"].push(obj["todoItems"].length + 1 + ". [ ] - " + inputArr[i]);
+    }
   }
 
   for (let z = 0; z < obj["todoItems"].length; z++) {
