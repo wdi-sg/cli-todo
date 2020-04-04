996dd0dbf3ecf2b6fc14ad432a0369a19175b872
wickker <choydianchun@gmail.com>
2020-04-04 16:03:11 +0800

    changed items to object

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const jsonFile = require("jsonfile");
 
 const dataFile = "data.json";
+const TODO_ITEMS = "todoItems";
 
 let inputArr = process.argv;
 
@@ -10,25 +11,30 @@ const printList = (err, obj) => {
       let dateObj = new Date();
       let mth = dateObj.getMonth() + 1;
       let dateTdy = dateObj.getDate() + "-" + mth + "-" + dateObj.getFullYear();
-      obj["todoItems"].push(obj["todoItems"].length + 1 + ". [ ] - " + dateTdy + " - " + inputArr[i]);
+      // obj[TODO_ITEMS].push(obj[TODO_ITEMS].length + 1 + ". [ ] - " + dateTdy + " - " + inputArr[i]);
+      let todoObj = {
+        done: "[ ]",
+        date: dateTdy,
+        item: inputArr[i]
+      };
+      obj[TODO_ITEMS].push(todoObj);
     }
   }
   else if (inputArr[2] === "done") {
     let index = inputArr[3] - 1;
-    let newStr = obj["todoItems"][index].replace("[ ]", "[x]");
-    obj["todoItems"][index] = newStr;
+    obj[TODO_ITEMS][index].done = "[x]";
   }
   else if (inputArr[2] === "delete") {
     let index = inputArr[3] - 1;
-    obj["todoItems"].splice(index, 1);
+    obj[TODO_ITEMS].splice(index, 1);
   }
 
-  for (let z = 0; z < obj["todoItems"].length; z++) {
-    console.log(obj["todoItems"][z]);
+  for (let z = 0; z < obj[TODO_ITEMS].length; z++) {
+    console.log(`${z+1}. ${obj[TODO_ITEMS][z].done} - ${obj[TODO_ITEMS][z].date} - ${obj[TODO_ITEMS][z].item}`);
   }
 
   jsonFile.writeFile(dataFile, obj, (err) => {
-    console.log(err);
+    if (err) throw err;
   });
 };
 
