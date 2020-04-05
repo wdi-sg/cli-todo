bf36835caee785f51fdcba92a9f0d281ad59874c
BOBBY <BOBBYKWONGZW@GMAIL.COM>
2020-04-05 20:06:20 +0800

    Completed app

--- index.js
+++ index.js
@@ -3,62 +3,63 @@ const jsonfile = require('jsonfile');
 
 const file = 'data.json'
 
-if (process.argv[2] === "read") {
-    jsonfile.readFile(file, (err, obj) => {
-        console.log(obj);
+jsonfile.readFile(file, (err, obj) => {
+    let itemDetails = {
+        item : "",
+        created_at : "",
+        updated_at : ""
+    }
+
+    if (process.argv[2] === "read") {
+        console.log(
+        `
+ ____  _____  ____    __    ____  ___  ____
+(  _ \\(  _  )(  _ \\  (  )  (_  _)/ __)(_  _)
+ ) _ < )(_)(  ) _ <   )(__  _)(_ \\__ \\  )(
+(____/(_____)(____/  (____)(____)(___/ (__)
+
+        `
+        );
+        let i =0;
+        obj["toDoItems"].forEach(el => {
+            console.log(`
+${i}. ${el.item}
+created_at: ${el.created_at}
+updated_at: ${el.updated_at}
+            `);
+            i++
+        })
         console.log(err);
-    })
-}
+    }
 
-if(process.argv[2] === "add"){
-    jsonfile.readFile(file, (err, obj) => {
+    else if (process.argv[2] === "add") {
+        itemDetails.item = '[ ] - ' + process.argv[3];
+        itemDetails.created_at = Date();
 
-      obj["toDoItems"].push(obj["toDoItems"].length + ". [ ] - " + process.argv[3] + " | created_at: " + Date());
+        obj["toDoItems"].push(itemDetails);
+    }
 
-      if(err){
-        console.log(err);
-      }
-
-      jsonfile.writeFile(file, obj, (error) => {
-        console.log(obj);
-        console.log(error)
-      });
-    });
-}
-
-if(process.argv[2] === "done"){
-    jsonfile.readFile(file, (err, obj) => {
-
+    else if (process.argv[2] === "done") {
         // Mark X in the bracket of item that is done
-        const item = obj["toDoItems"][process.argv[3]];
-        itemSplit = item.split(' ')
-        itemSplit.splice(2, 0, 'X');
-        itemSplit.push(" | updated_at: " + Date());
+        let targetItem = obj["toDoItems"][process.argv[3]];
+        let itemSplit = targetItem.item.split(' ')
+        itemSplit.splice(1, 0, 'X');
 
         // Join back array into full string
-        newObj = itemSplit.join(' ');
+        targetItem.item = itemSplit.join(' ');
 
+        // Add date into update_at property
+        targetItem.updated_at = Date();
 
         // Change obj to reflect new object
-        obj["toDoItems"][process.argv[3]] = newObj;
+        obj["toDoItems"][process.argv[3]] = targetItem;
+    }
 
-        jsonfile.writeFile(file, obj, (error) => {
-            console.log(obj);
-            console.log(error);
-        })
-    })
-}
-
-if (process.argv[2] === 'del'){
-    jsonfile.readFile(file, (err, obj) => {
+    else if (process.argv[2] === 'del') {
         obj['toDoItems'].splice(process.argv[3], 1);
+    }
 
-        console.log(err);
-
-        jsonfile.writeFile(file, obj, (error) => {
-            console.log(obj);
-            console.log(error);
-        })
+    jsonfile.writeFile(file, obj, (error) => {
+        console.log(error);
     })
-
-}
+})
