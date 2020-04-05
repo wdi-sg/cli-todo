85d3c4b1bbe6f3371a3bc6a73599303c697b8f31
vigbit <vigneshb@live.com>
2020-04-06 07:31:58 +0800

    added and shown

--- index.js
+++ index.js
@@ -1,19 +1,45 @@
-console.log("works!!", process.argv[2]);
 
-var commandType = process.argv[2];
+let command = process.argv[2];
+let taskItem = process.argv[3];
 
-console.log("Your command was: "+commandType);
+let unmarked = " ";
+let marked = "x";
 
 const jsonfile = require('jsonfile');
 
-const file = 'data.json'
+const fileName = 'data.json'
 
-jsonfile.readFile(file, (err, obj) => {
 
-  console.log(obj);
-  obj["helloworld"] = "monkey";
+const addData = (error, toDoList) => {
+console.log(toDoList);
+//adding task items to the list
+    if ( command === "add"){
+      // let itemsCount = toDoList['todoItems'].push(taskItem);
+      // console.log("the item add is " + itemsCount);
+//create an output format
+      let itemsCount = toDoList['todoItems'].length + 1
+      let formattedData = itemsCount + "." + " [" + unmarked + "]" + " - " + taskItem;
+      console.log("the output is " + formattedData);
+      let addedItems = toDoList['todoItems'].push(formattedData);
+      console.log(addedItems);
+      console.log(toDoList['todoItems']);
+      //console.log(toDoList['todoItems'][0]);
+    }
+    if ( command === "show"){
+      for ( let i = 0; i < toDoList['todoItems'].length; i++){
+        console.log(toDoList['todoItems'][i]);
+      }
+    }
 
-  jsonfile.writeFile(file, obj, (err) => {
-    console.log(err)
-  });
-});
+
+jsonfile.writeFile(fileName, toDoList)
+
+}
+
+
+jsonfile.readFile(fileName, addData)
+console.log("read File")
+
+
+
+
