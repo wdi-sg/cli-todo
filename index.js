ee5b04b1709c85fc1e0a7c1d6bf75f4c53823662
jordanswp <sohweepin@gmail.com>
2020-04-06 12:03:18 +0800

    first part done

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
-console.log("works!!", process.argv[2]);
-
-var commandType = process.argv[2];
-
-console.log("Your command was: "+commandType);
+// console.log("added: ", process.argv[2], process.argv[3]);
 
 const jsonfile = require('jsonfile');
 
 const file = 'data.json'
 
-jsonfile.readFile(file, (err, obj) => {
+// var toDoList = {"todoItems:[]"};
+
+var commandType = process.argv[2];
+
+
+if (commandType === "add") {
+        jsonfile.readFile(file, (err, obj) => {
 
   console.log(obj);
-  obj["helloworld"] = "monkey";
+  // obj[process.argv[3]] = process.argv[4]
+
+if (obj["todoItems"] === undefined){
+    obj["todoItems"] = []
+}
+
+obj["todoItems"].push(process.argv[3])
+
 
   jsonfile.writeFile(file, obj, (err) => {
     console.log(err)
   });
 });
+} else if (commandType === "show") {
+    jsonfile.readFile(file, (err, obj) => {
+    for (i = 0; i < obj["todoItems"].length; i++){
+        console.log ((i + 1) + "." + obj["todoItems"][i]);
+    }
+});
+}
