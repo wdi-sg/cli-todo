64e93fbcc69e17b1482e0f81891d7a2785c66352
YusofGotboudine <yusof95@hotmail.com>
2020-04-06 10:46:06 +0800

    MVP with further attempt

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
-console.log("works!!", process.argv[2]);
+console.log("NODEEEEE");
 
 var commandType = process.argv[2];
+var commandItem = process.argv[3];
+var selectedInput = process.argv[4];
 
-console.log("Your command was: "+commandType);
+console.log("Your command was: " + commandType);
 
 const jsonfile = require('jsonfile');
 
-const file = 'data.json'
+const file = 'data.json';
 
 jsonfile.readFile(file, (err, obj) => {
 
-  console.log(obj);
-  obj["helloworld"] = "monkey";
+  if (process.argv[2] === "add") {
+    var newInputNumber = obj["toDoList"].length + 1;
+    var newInput = newInputNumber + ". [ ] - " + commandItem;
+    obj["toDoList"].push(newInput);
+  } else if (process.argv[2] === "show") {
+    for (let i = 0; i < obj["toDoList"].length; i++) {
+      console.log(obj["toDoList"][i]);
+    }
+    console.log(obj);
+    // } else if (process.argv[2] === "done") {
+    //   var completedTask = selectedInput - 1;
 
-  jsonfile.writeFile(file, obj, (err) => {
-    console.log(err)
-  });
-});
+    // }
+    jsonfile.writeFile(file, obj, (err) => { console.log(err); })
+  })
