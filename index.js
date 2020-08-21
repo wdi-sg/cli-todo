648d4266afe34561aa7df18eb5c1c8fc05359f47
chewjta <chewjta@gmail.com>
2020-08-22 04:35:50 +0800

    deliverable with first furthers done.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-console.log("works!!", process.argv[2]);
+console.log("works!!", process.argv);
 
 var commandType = process.argv[2];
+
+var task = process.argv[3];
 
 console.log("Your command was: "+commandType);
 
@@ -8,10 +10,21 @@ const jsonfile = require('jsonfile');
 
 const file = 'data.json'
 
-jsonfile.readFile(file, (err, obj) => {
+const notDone = " [ ] - ";
+const done = " [X] - ";
 
-  console.log(obj);
-  obj["helloworld"] = "monkey";
+jsonfile.readFile(file, (err, obj) => {
+    if(commandType === "add"){
+    let listNo = obj["todoItems"].length+1;
+obj["todoItems"].push(`${listNo}.${notDone}${task}`)
+  console.log(obj)
+} if(commandType === "done"){
+    let listNo = process.argv[3];
+    // console.log(obj["todoItems"][listNo-1]);
+    let doneTask = obj["todoItems"][listNo-1].split(" ").slice(4).join(' ');
+    obj["todoItems"][listNo-1] = `${listNo}.${done}${doneTask}`
+    console.log(obj);
+}
 
   jsonfile.writeFile(file, obj, (err) => {
     console.log(err)
