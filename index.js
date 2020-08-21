a4414b7264776599404595e199a1864c6bd40b8a
Aurelia Lim <aureliadotlim@gmail.com>
2020-08-22 02:11:41 +0800

    initial commit

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
-console.log("works!!", process.argv[2]);
-
-var commandType = process.argv[2];
-
-console.log("Your command was: "+commandType);
-
 const jsonfile = require('jsonfile');
-
 const file = 'data.json'
 
-jsonfile.readFile(file, (err, obj) => {
+const ENV = process.argv[0];
+const PATH = process.argv[1];
+const operation = process.argv[2];
+const listItem = process.argv[3];
 
-  console.log(obj);
-  obj["helloworld"] = "monkey";
+//status is either [x] or [ ]
+let status;
 
-  jsonfile.writeFile(file, obj, (err) => {
-    console.log(err)
-  });
-});
+let toDoList = file.toDoList.length + 1;
+let listNum = listNum + ".";
+
+
+if(operation=== 'add'){
+  console.log(add (listItem))
+}
+
+//helper functions
+
+//function to add things to do list
+
+// everytime a user adds something, push it into file.toDoItems array so you can use .length property to track number of items in the list
+
