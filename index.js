bb4a3c03a9cdf94e5318e62c962b7d247afab785
exhlim <exhlimm@gmail.com>
2020-08-23 20:46:01 +0800

    sei-24-Eugene

--- index.js
+++ index.js
@@ -1,19 +1,36 @@
-console.log("works!!", process.argv[2]);
-
-var commandType = process.argv[2];
-
-console.log("Your command was: "+commandType);
-
-const jsonfile = require('jsonfile');
-
-const file = 'data.json'
-
-jsonfile.readFile(file, (err, obj) => {
-
-  console.log(obj);
-  obj["helloworld"] = "monkey";
-
-  jsonfile.writeFile(file, obj, (err) => {
-    console.log(err)
-  });
-});
+var formPhrase = require("font-ascii").default;
+let argv = process.argv.splice(2);
+const jsonfile = require("jsonfile")
+const filename = "data.json";
+jsonfile.readFile(filename, (err, obj) => {
+    switch(argv[0].toLowerCase()){
+    case "add": {
+        let toDoArr = obj.toDoList;
+        let toDoIndex = obj.toDoIndex;
+        obj.toDoList.push("[ ] - " + argv[1] + " Added on " + ( new Date() ).toLocaleDateString().split("/"));
+        jsonfile.writeFile(filename, obj, (err) => {})
+        formPhrase("Added", { typeface: "StarWars", color: "yellow" });; break;
+    }
+    case "show": {
+        let i = 1;
+        formPhrase("To do List", { typeface: "StarWars", color: "white" });
+        obj.toDoList.forEach(element => {
+            console.log(`${i}. ${element}`);
+            i++;}); break; }
+    case "done": {
+        formPhrase("Done", { typeface: "StarWars", color: "green" });
+        let index = parseInt(argv[1]) - 1;
+        if((/["updated"]/gi).test(obj.toDoList[index])){break;}
+        let updateIndex = obj.toDoList[index];
+        let array = updateIndex.split("");
+        array[1] = "X";
+        obj.toDoList[index] = array.join("") + " Updated on " + ( new Date() ).toLocaleDateString().split("/");
+        jsonfile.writeFile(filename, obj, (err) => {}); break;
+    }; break;
+    case "remove": {
+        formPhrase("Removed", { typeface: "StarWars", color: "red" });
+        let index = parseInt(argv[1] - 1);
+        obj.toDoList.splice(index, 1);
+        jsonfile.writeFile(filename, obj, (err) => {}); } break;
+    }
+})
